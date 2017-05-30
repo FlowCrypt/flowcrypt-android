d152e3e33b75cc47a55ef0f7d8df18a18a9d24eb
DenBond7 <DenBond7@gmail.com>
2017-05-30 15:16:34 +0300

    Added a logic for the feedback screen. |#22

--- FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/settings/FeedbackActivity.java
+++ FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/settings/FeedbackActivity.java
@@ -1,12 +1,27 @@
 package com.flowcrypt.email.ui.activity.settings;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
+import com.flowcrypt.email.BuildConfig;
 import com.flowcrypt.email.R;
+import com.flowcrypt.email.api.retrofit.BaseResponse;
+import com.flowcrypt.email.api.retrofit.request.PostHelpFeedbackRequest;
+import com.flowcrypt.email.api.retrofit.request.model.PostHelpFeedbackModel;
+import com.flowcrypt.email.api.retrofit.response.PostHelpFeedbackResponse;
 import com.flowcrypt.email.ui.activity.base.BaseBackStackAuthenticationActivity;
+import com.flowcrypt.email.ui.loader.ApiServiceAsyncTaskLoader;
+import com.flowcrypt.email.util.GeneralUtil;
+import com.flowcrypt.email.util.UIUtil;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
 /**
@@ -19,16 +34,40 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
  *         E-mail: DenBond7@gmail.com
  */
 
-public class FeedbackActivity extends BaseBackStackAuthenticationActivity {
+public class FeedbackActivity extends BaseBackStackAuthenticationActivity implements
+        LoaderManager.LoaderCallbacks<BaseResponse> {
+    private static final String KEY_IS_MESSAGE_SENT = BuildConfig.APPLICATION_ID + "" +
+            ".KEY_IS_MESSAGE_SENT";
+
+    private View progressBar;
+    private View layoutInput;
+    private View layoutContent;
+    private EditText editTextUserMessage;
+
+    private String email;
+    private boolean isMessageSent;
 
     @Override
     public View getRootView() {
-        return null;
+        return layoutContent;
     }
 
     @Override
     public void handleSignInResult(GoogleSignInResult googleSignInResult, boolean isOnStartCall) {
-
+        if (googleSignInResult != null) {
+            if (googleSignInResult.isSuccess()) {
+                GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+                if (googleSignInAccount != null) {
+                    this.email = googleSignInAccount.getEmail();
+                } else {
+                    finish();
+                }
+            } else {
+                finish();
+            }
+        } else {
+            finish();
+        }
     }
 
     @Override
@@ -39,12 +78,125 @@ public class FeedbackActivity extends BaseBackStackAuthenticationActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            this.isMessageSent = savedInstanceState.getBoolean(KEY_IS_MESSAGE_SENT);
+        }
 
+        initViews();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putBoolean(KEY_IS_MESSAGE_SENT, isMessageSent);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_feedback, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuActionSend:
+                if (!isMessageSent) {
+                    if (isInformationValid()) {
+                        if (GeneralUtil.isInternetConnectionAvailable(this)) {
+                            UIUtil.hideSoftInput(this, editTextUserMessage);
+                            getSupportLoaderManager().restartLoader(R.id
+                                            .loader_id_post_help_feedback,
+                                    null, this);
+                        } else {
+                            UIUtil.showInfoSnackbar(getRootView(), getString(R.string
+                                    .internet_connection_is_not_available));
+                        }
+                    }
+                } else {
+                    UIUtil.showInfoSnackbar(getRootView(), getString(R.string
+                            .you_already_sent_this_message));
+                }
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public Loader<BaseResponse> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case R.id.loader_id_post_help_feedback:
+                UIUtil.showProgress(this, true, progressBar, layoutInput);
+
+                return new ApiServiceAsyncTaskLoader(getApplicationContext(),
+                        new PostHelpFeedbackRequest(new PostHelpFeedbackModel(email,
+                                editTextUserMessage.getText().toString())));
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<BaseResponse> loader, BaseResponse data) {
+        switch (loader.getId()) {
+            case R.id.loader_id_post_help_feedback:
+                UIUtil.showProgress(this, false, progressBar, layoutInput);
+
+                if (data != null) {
+                    if (data.getResponseModel() != null) {
+                        PostHelpFeedbackResponse postHelpFeedbackResponse =
+                                (PostHelpFeedbackResponse) data.getResponseModel();
+                        if (postHelpFeedbackResponse.isSent()) {
+                            this.isMessageSent = true;
+                            UIUtil.showSnackbar(getRootView(),
+                                    postHelpFeedbackResponse.getText(),
+                                    getString(R.string.back),
+                                    new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            finish();
+                                        }
+                                    });
+                        } else {
+                            UIUtil.showInfoSnackbar(getRootView(), postHelpFeedbackResponse
+                                    .getText());
+                        }
+
+                    } else if (data.getException() != null) {
+                        UIUtil.showInfoSnackbar(getRootView(), data.getException().getMessage());
+                    } else {
+                        UIUtil.showInfoSnackbar(getRootView(), getString(R.string.unknown_error));
+                    }
+                } else {
+                    UIUtil.showInfoSnackbar(getRootView(), getString(R.string.unknown_error));
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<BaseResponse> loader) {
+
+    }
+
+    private void initViews() {
+        editTextUserMessage = (EditText) findViewById(R.id.editTextUserMessage);
+        progressBar = findViewById(R.id.progressBar);
+        layoutInput = findViewById(R.id.layoutInput);
+        layoutContent = findViewById(R.id.layoutContent);
+    }
+
+    private boolean isInformationValid() {
+        if (TextUtils.isEmpty(editTextUserMessage.getText().toString())) {
+            UIUtil.showInfoSnackbar(editTextUserMessage,
+                    getString(R.string.your_message_must_be_non_empty));
+            return false;
+        } else {
+            return true;
+        }
     }
 }
