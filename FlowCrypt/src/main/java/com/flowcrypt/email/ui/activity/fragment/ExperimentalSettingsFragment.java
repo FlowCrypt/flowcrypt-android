bd2da1a3deb74d521835b1e9066e4166acb597ea
DenBond7 <DenBond7@gmail.com>
2017-09-29 22:55:36 +0300

    Added "Settings page: Experimental" | Close #111.

---
+++ FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/ExperimentalSettingsFragment.java
@@ -0,0 +1,25 @@
+/*
+ * Business Source License 1.0 © 2017 FlowCrypt Limited (tom@cryptup.org).
+ * Use limitations apply. See https://github.com/FlowCrypt/flowcrypt-android/blob/master/LICENSE
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.activity.fragment;
+
+import android.os.Bundle;
+
+import com.flowcrypt.email.R;
+import com.flowcrypt.email.ui.activity.fragment.base.BasePreferenceFragment;
+
+/**
+ * @author DenBond7
+ *         Date: 29.09.2017.
+ *         Time: 22:46.
+ *         E-mail: DenBond7@gmail.com
+ */
+public class ExperimentalSettingsFragment extends BasePreferenceFragment {
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences_experimental_settings);
+    }
+}
