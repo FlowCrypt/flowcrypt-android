30eec099c4ccfb976702681acba7602bd57368dc
DenBond7 <DenBond7@gmail.com>
2017-12-14 12:53:51 +0200

    Added JsTest.| #166

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/js/JsTest.java
@@ -0,0 +1,54 @@
+/*
+ * Business Source License 1.0 © 2017 FlowCrypt Limited (human@flowcrypt.com).
+ * Use limitations apply. See https://github.com/FlowCrypt/flowcrypt-android/blob/master/LICENSE
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.js;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 13.12.2017
+ *         Time: 15:01
+ *         E-mail: DenBond7@gmail.com
+ */
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JsTest {
+    private static final String TAG = JsTest.class.getSimpleName();
+    private Js js;
+    private StorageConnectorInterface storageConnectorInterface;
+
+    @Before
+    public void setUp() throws Exception {
+        long startInitTime = System.currentTimeMillis();
+        Log.d(TAG, "Js init time = " + (System.currentTimeMillis() - startInitTime));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void method_1_prepareStorageConnectorInterface() throws Exception {
+        this.storageConnectorInterface = new SampleStorageConnector(InstrumentationRegistry.getTargetContext());
+    }
+
+    @Test
+    public void method_2_prepareJs() throws Exception {
+        this.js = new Js(InstrumentationRegistry.getTargetContext(), storageConnectorInterface);
+    }
+}
