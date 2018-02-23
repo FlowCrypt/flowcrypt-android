dcc077be15d79667d22f746a07e79e7b03bf3b9c
DenBond7 <DenBond7@gmail.com>
2018-02-23 10:24:42 +0200

    Added AttesterSettingsActivityTest.

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/AttesterSettingsActivityTest.java
@@ -0,0 +1,46 @@
+/*
+ * © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.activity;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.flowcrypt.email.R;
+import com.flowcrypt.email.rules.AddAccountDaoToDatabaseRule;
+import com.flowcrypt.email.rules.ClearAppSettingsRule;
+import com.flowcrypt.email.ui.activity.settings.AttesterSettingsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.flowcrypt.email.base.BaseTest.matchEmptyList;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 23.02.2018
+ *         Time: 10:11
+ *         E-mail: DenBond7@gmail.com
+ */
+
+public class AttesterSettingsActivityTest {
+    @Rule
+    public TestRule ruleChain = RuleChain
+            .outerRule(new ClearAppSettingsRule())
+            .around(new AddAccountDaoToDatabaseRule())
+            .around(new ActivityTestRule<>(AttesterSettingsActivity.class));
+
+    @Test
+    public void testIsKeysExistsOnAttester() {
+        onView(withId(R.id.listViewKeys)).check(matches(not(matchEmptyList()))).check(matches(isDisplayed()));
+        onView(withId(R.id.emptyView)).check(matches(not(isDisplayed())));
+    }
+}
