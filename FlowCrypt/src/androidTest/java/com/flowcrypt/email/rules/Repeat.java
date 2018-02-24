14a00f7ca2ac3dee97285edcdbac4ec26b818a33
DenBond7 <DenBond7@gmail.com>
2018-02-24 18:15:34 +0200

    Added RepeatedRule.

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/rules/Repeat.java
@@ -0,0 +1,26 @@
+/*
+ * © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.rules;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 24.02.2018
+ *         Time: 17:29
+ *         E-mail: DenBond7@gmail.com
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD, ANNOTATION_TYPE})
+public @interface Repeat {
+    int value() default 1;
+}
---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/rules/RepeatedRule.java
@@ -0,0 +1,48 @@
+/*
+ * © 2016-2018 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.rules;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 24.02.2018
+ *         Time: 17:28
+ *         E-mail: DenBond7@gmail.com
+ */
+
+public class RepeatedRule implements TestRule {
+
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        Statement result = statement;
+        Repeat repeat = description.getAnnotation(Repeat.class);
+        if (repeat != null) {
+            int times = repeat.value();
+            result = new RepeatStatement(statement, times);
+        }
+        return result;
+    }
+
+    private static class RepeatStatement extends Statement {
+        private final Statement statement;
+        private final int repeat;
+
+        public RepeatStatement(Statement statement, int repeat) {
+            this.statement = statement;
+            this.repeat = repeat;
+        }
+
+        @Override
+        public void evaluate() throws Throwable {
+            for (int i = 0; i < repeat; i++) {
+                statement.evaluate();
+            }
+        }
+    }
+}
