598afd6eb2507f7acaa0c13680f6db3da28f3679
DenBond7 <DenBond7@gmail.com>
2018-08-08 14:51:31 +0300

    Added showing a hint if a current user doesn't have any related keys.| #252

--- FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/preferences/SecuritySettingsFragment.java
+++ FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/preferences/SecuritySettingsFragment.java
@@ -10,23 +10,29 @@ import android.support.v7.preference.Preference;
 
 import com.flowcrypt.email.Constants;
 import com.flowcrypt.email.R;
+import com.flowcrypt.email.database.dao.source.AccountDao;
 import com.flowcrypt.email.database.dao.source.AccountDaoSource;
+import com.flowcrypt.email.database.dao.source.UserIdEmailsKeysDaoSource;
 import com.flowcrypt.email.ui.activity.ChangePassPhraseActivity;
 import com.flowcrypt.email.ui.activity.fragment.base.BasePreferenceFragment;
+import com.flowcrypt.email.util.UIUtil;
 
 /**
  * This fragment contains actions which related to Security options.
  *
  * @author DenBond7
- * Date: 08.08.2018.
- * Time: 10:47.
- * E-mail: DenBond7@gmail.com
+ *         Date: 08.08.2018.
+ *         Time: 10:47.
+ *         E-mail: DenBond7@gmail.com
  */
 public class SecuritySettingsFragment extends BasePreferenceFragment implements Preference.OnPreferenceClickListener {
+    private AccountDao accountDao;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences_security_settings);
+
+        accountDao = new AccountDaoSource().getActiveAccountInformation(getContext());
 
         Preference preferenceChangePassPhrase = findPreference(Constants.PREFERENCES_KEY_SECURITY_CHANGE_PASS_PHRASE);
         if (preferenceChangePassPhrase != null) {
@@ -38,8 +44,12 @@ public class SecuritySettingsFragment extends BasePreferenceFragment implements 
     public boolean onPreferenceClick(Preference preference) {
         switch (preference.getKey()) {
             case Constants.PREFERENCES_KEY_SECURITY_CHANGE_PASS_PHRASE:
-                startActivity(ChangePassPhraseActivity.newIntent(getContext(), new AccountDaoSource()
-                        .getActiveAccountInformation(getContext())));
+                if (new UserIdEmailsKeysDaoSource().getLongIdsByEmail(getContext(), accountDao.getEmail()).isEmpty()) {
+                    UIUtil.showInfoSnackbar(getView(), getString(R.string.account_has_no_associated_keys, getString
+                            (R.string.support_email)));
+                } else {
+                    startActivity(ChangePassPhraseActivity.newIntent(getContext(), accountDao));
+                }
                 return true;
 
             default:
