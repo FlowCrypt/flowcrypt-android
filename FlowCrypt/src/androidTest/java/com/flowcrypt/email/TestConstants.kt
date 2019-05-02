6c4d571dcaa916981d4850e862d03e491064686c
DenBond7 <DenBond7@gmail.com>
2019-05-02 12:25:51 +0300

    Changed a structure of TestConstants

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/TestConstants.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/TestConstants.kt
@@ -11,16 +11,18 @@ package com.flowcrypt.email
  * Time: 15:15
  * E-mail: DenBond7@gmail.com
  */
-object TestConstants {
-  const val IMAP = "IMAP"
-  const val SMTP = "SMTP"
+class TestConstants {
+  companion object {
+    const val IMAP = "IMAP"
+    const val SMTP = "SMTP"
 
-  const val COMMERCIAL_AT_SYMBOL = '@'
+    const val COMMERCIAL_AT_SYMBOL = '@'
 
-  const val RECIPIENT_WITH_PUBLIC_KEY_ON_ATTESTER = "test@denbond7.com"
-  const val RECIPIENT_WITHOUT_PUBLIC_KEY_ON_ATTESTER = "not_attester_user@denbond7.com"
+    const val RECIPIENT_WITH_PUBLIC_KEY_ON_ATTESTER = "test@denbond7.com"
+    const val RECIPIENT_WITHOUT_PUBLIC_KEY_ON_ATTESTER = "not_attester_user@denbond7.com"
 
-  const val DEFAULT_STRONG_PASSWORD = "My super strong password 2018"
-  const val DEFAULT_SECOND_STRONG_PASSWORD = "My super strong passphrase 2019"
-  const val DEFAULT_PASSWORD = "android"
+    const val DEFAULT_STRONG_PASSWORD = "My super strong password 2018"
+    const val DEFAULT_SECOND_STRONG_PASSWORD = "My super strong passphrase 2019"
+    const val DEFAULT_PASSWORD = "android"
+  }
 }
