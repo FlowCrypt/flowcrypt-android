10576b82e7dfefb93c9162f1089fa4c60d12c064
DenBond7 <DenBond7@gmail.com>
2019-05-03 18:58:05 +0300

    Transformed AddNewAccountActivityTest to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/AddNewAccountActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/AddNewAccountActivityTest.kt
@@ -3,39 +3,36 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import android.app.Activity;
-import android.app.Instrumentation;
-
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.rules.AddAccountToDatabaseRule;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.isInternal
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import org.hamcrest.Matchers.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
  * @author Denis Bondarenko
@@ -44,38 +41,41 @@ import static org.hamcrest.Matchers.not;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class AddNewAccountActivityTest extends BaseTest {
-  private ActivityTestRule activityTestRule = new IntentsTestRule<>(AddNewAccountActivity.class);
+@RunWith(AndroidJUnit4::class)
+class AddNewAccountActivityTest : BaseTest() {
+  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(AddNewAccountActivity::class.java)
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(new AddAccountToDatabaseRule())
-      .around(activityTestRule);
-
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return activityTestRule;
-  }
+  @get:Rule
+  val ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule())
+      .around(activityTestRule)
 
   @Before
-  public void stubAllExternalIntents() {
+  fun stubAllExternalIntents() {
     // All external Intents will be blocked.
-    intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    intending(not<Intent>(isInternal())).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
   }
 
   @Test
-  public void testUseOtherEmailProviders() {
-    onView(withId(R.id.buttonOtherEmailProvider)).check(matches(isDisplayed())).perform(click());
-    onView(withText(R.string.adding_new_account)).check(matches(isDisplayed()));
-    onView(withId(R.id.editTextEmail)).check(matches(isDisplayed()));
+  fun testUseOtherEmailProviders() {
+    onView(withId(R.id.buttonOtherEmailProvider))
+        .check(matches(isDisplayed()))
+        .perform(click())
+
+    onView(withText(R.string.adding_new_account))
+        .check(matches(isDisplayed()))
+
+    onView(withId(R.id.editTextEmail))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testUseGmail() {
-    onView(withId(R.id.buttonSignInWithGmail)).check(matches(isDisplayed())).perform(click());
+  fun testUseGmail() {
+    onView(withId(R.id.buttonSignInWithGmail))
+        .check(matches(isDisplayed()))
+        .perform(click())
     //check that the Google Sign-in screen is displayed
-    intended(toPackage("com.google.android.gms"));
+    intended(toPackage("com.google.android.gms"))
   }
 }
