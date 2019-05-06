af9ada7f9d80a759010ebc4470931f0537f15853
DenBond7 <DenBond7@gmail.com>
2019-05-06 14:06:07 +0300

    Transformed BasePassphraseActivityTest and extended classes to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/CreatePrivateKeyActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/CreatePrivateKeyActivityTest.kt
@@ -3,37 +3,31 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import android.content.Context;
-import android.content.Intent;
-
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-import com.flowcrypt.email.ui.activity.base.BasePassphraseActivityTest;
-import com.flowcrypt.email.util.AccountDaoManager;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.ui.activity.base.BasePassphraseActivityTest
+import com.flowcrypt.email.util.AccountDaoManager
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
- * A test for {@link CreatePrivateKeyActivity}
+ * A test for [CreatePrivateKeyActivity]
  *
  * @author Denis Bondarenko
  * Date: 15.01.2018
@@ -41,41 +35,38 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class CreatePrivateKeyActivityTest extends BasePassphraseActivityTest {
+@RunWith(AndroidJUnit4::class)
+class CreatePrivateKeyActivityTest : BasePassphraseActivityTest() {
 
-  private ActivityTestRule activityTestRule =
-      new ActivityTestRule<CreatePrivateKeyActivity>(CreatePrivateKeyActivity.class) {
-        @Override
-        protected Intent getActivityIntent() {
-          Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-          Intent result = new Intent(targetContext, CreatePrivateKeyActivity.class);
-          result.putExtra(CreatePrivateKeyActivity.KEY_EXTRA_ACCOUNT_DAO,
-              AccountDaoManager.getDefaultAccountDao());
-          return result;
+  override val activityTestRule: ActivityTestRule<*>? =
+      object : ActivityTestRule<CreatePrivateKeyActivity>(CreatePrivateKeyActivity::class.java) {
+        override fun getActivityIntent(): Intent {
+          val result = Intent(getTargetContext(), CreatePrivateKeyActivity::class.java)
+          result.putExtra(CreatePrivateKeyActivity.KEY_EXTRA_ACCOUNT_DAO, AccountDaoManager.getDefaultAccountDao())
+          return result
         }
-      };
+      }
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(activityTestRule);
-
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return activityTestRule;
-  }
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(activityTestRule)
 
   @Test
-  public void testUseCorrectPassPhrase() {
-    onView(withId(R.id.editTextKeyPassword)).check(matches(isDisplayed())).perform(replaceText(PERFECT_PASSWORD),
-        closeSoftKeyboard());
-    onView(withId(R.id.buttonSetPassPhrase)).check(matches(isDisplayed())).perform(click());
-
-    onView(withId(R.id.editTextKeyPasswordSecond)).check(matches(isDisplayed())).perform(replaceText(PERFECT_PASSWORD),
-        closeSoftKeyboard());
-    onView(withId(R.id.buttonConfirmPassPhrases)).check(matches(isDisplayed())).perform(click());
-
-    onView(withId(R.id.buttonSuccess)).check(matches(isDisplayed()));
+  fun testUseCorrectPassPhrase() {
+    onView(withId(R.id.editTextKeyPassword))
+        .check(matches(isDisplayed()))
+        .perform(replaceText(PERFECT_PASSWORD), closeSoftKeyboard())
+    onView(withId(R.id.buttonSetPassPhrase))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.editTextKeyPasswordSecond))
+        .check(matches(isDisplayed()))
+        .perform(replaceText(PERFECT_PASSWORD), closeSoftKeyboard())
+    onView(withId(R.id.buttonConfirmPassPhrases))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.buttonSuccess))
+        .check(matches(isDisplayed()))
   }
 }
--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BasePassphraseActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BasePassphraseActivityTest.kt
@@ -3,33 +3,26 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity.base;
+package com.flowcrypt.email.ui.activity.base
 
-import android.app.Activity;
-
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.isEmptyString;
-import static org.hamcrest.Matchers.startsWith;
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import org.hamcrest.Matchers.isEmptyString
+import org.hamcrest.Matchers.startsWith
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
 /**
  * @author Denis Bondarenko
@@ -37,149 +30,172 @@ import static org.hamcrest.Matchers.startsWith;
  * Time: 3:44 PM
  * E-mail: DenBond7@gmail.com
  */
-public abstract class BasePassphraseActivityTest extends BaseTest {
-  protected static final String WEAK_PASSWORD = "weak";
-  protected static final String POOR_PASSWORD = "weak, perfect, great";
-  protected static final String REASONABLE_PASSWORD = "weak, poor, reasonable";
-  protected static final String GOOD_PASSWORD = "weak, poor, good,";
-  protected static final String GREAT_PASSWORD = "weak, poor, great, good";
-  protected static final String PERFECT_PASSWORD = "unconventional blueberry unlike any other";
+abstract class BasePassphraseActivityTest : BaseTest() {
 
   @Before
-  public void init() {
-    ActivityTestRule activityTestRule = getActivityTestRule();
-    if (activityTestRule != null) {
-      Activity activity = activityTestRule.getActivity();
-      if (activity instanceof BasePassPhraseManagerActivity) {
-        IdlingRegistry.getInstance().register(
-            ((BasePassPhraseManagerActivity) activity).getIdlingForPassphraseChecking());
-      }
+  fun registerIdlingForPassphraseChecking() {
+    val activity = activityTestRule?.activity ?: return
+    if (activity is BasePassPhraseManagerActivity) {
+      IdlingRegistry.getInstance().register(activity.idlingForPassphraseChecking)
     }
   }
 
   @After
-  public void unregisterDecryptionIdling() {
-    ActivityTestRule activityTestRule = getActivityTestRule();
-    if (activityTestRule != null) {
-      Activity activity = activityTestRule.getActivity();
-      if (activity instanceof BasePassPhraseManagerActivity) {
-        IdlingRegistry.getInstance().unregister(
-            ((BasePassPhraseManagerActivity) activity).getIdlingForPassphraseChecking());
-      }
+  fun unregisterIdlingForPassphraseChecking() {
+    val activity = activityTestRule?.activity ?: return
+    if (activity is BasePassPhraseManagerActivity) {
+      IdlingRegistry.getInstance().unregister(activity.idlingForPassphraseChecking)
     }
   }
 
   @Test
-  public void testShowDialogWithPasswordRecommendation() {
-    onView(withId(R.id.imageButtonShowPasswordHint)).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.webView)).check(matches(isDisplayed()));
-    onView(withId(R.id.buttonOk)).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.textViewFirstPasswordCheckTitle)).check(matches(isDisplayed()));
+  fun testShowDialogWithPasswordRecommendation() {
+    onView(withId(R.id.imageButtonShowPasswordHint))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.webView))
+        .check(matches(isDisplayed()))
+    onView(withId(R.id.buttonOk))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.textViewFirstPasswordCheckTitle))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testEmptyFirstPassPhrase() {
-    closeSoftKeyboard();
-    onView(withId(R.id.buttonSetPassPhrase)).check(matches(isDisplayed())).perform(click());
+  fun testEmptyFirstPassPhrase() {
+    closeSoftKeyboard()
+    onView(withId(R.id.buttonSetPassPhrase))
+        .check(matches(isDisplayed()))
+        .perform(click())
 
-    checkIsNonEmptyHintShown();
+    checkIsNonEmptyHintShown()
   }
 
   @Test
-  public void testEmptySecondPassPhrase() {
-    testShowRepeatingPassPhraseScreen();
-    onView(withId(R.id.buttonConfirmPassPhrases)).perform(click());
+  fun testEmptySecondPassPhrase() {
+    testShowRepeatingPassPhraseScreen()
+    onView(withId(R.id.buttonConfirmPassPhrases))
+        .perform(click())
 
-    checkIsNonEmptyHintShown();
+    checkIsNonEmptyHintShown()
   }
 
   @Test
-  public void testShowRepeatingPassPhraseScreen() {
-    onView(withId(R.id.editTextKeyPassword)).perform(scrollTo(), replaceText(PERFECT_PASSWORD), closeSoftKeyboard());
-    onView(withId(R.id.buttonSetPassPhrase)).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.buttonConfirmPassPhrases)).check(matches(isDisplayed()));
+  fun testShowRepeatingPassPhraseScreen() {
+    onView(withId(R.id.editTextKeyPassword))
+        .perform(scrollTo(), replaceText(PERFECT_PASSWORD), closeSoftKeyboard())
+    onView(withId(R.id.buttonSetPassPhrase))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.buttonConfirmPassPhrases))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testShowMsgMismatchOfPassPhrase() {
-    testShowRepeatingPassPhraseScreen();
+  fun testShowMsgMismatchOfPassPhrase() {
+    testShowRepeatingPassPhraseScreen()
 
-    onView(withId(R.id.editTextKeyPasswordSecond)).check(matches(isDisplayed()))
-        .perform(scrollTo(), replaceText("some text"), closeSoftKeyboard());
-    onView(withId(R.id.buttonConfirmPassPhrases)).perform(click());
-
-    onView(withText(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string
-        .pass_phrases_do_not_match)))
-        .check(matches(isDisplayed()));
-    onView(withId(com.google.android.material.R.id.snackbar_action)).check(matches(isDisplayed())).perform(click());
+    onView(withId(R.id.editTextKeyPasswordSecond))
+        .check(matches(isDisplayed()))
+        .perform(scrollTo(), replaceText("some text"), closeSoftKeyboard())
+    onView(withId(R.id.buttonConfirmPassPhrases))
+        .perform(click())
+    onView(withText(getResString(R.string.pass_phrases_do_not_match)))
+        .check(matches(isDisplayed()))
+    onView(withId(com.google.android.material.R.id.snackbar_action))
+        .check(matches(isDisplayed())).perform(click())
   }
 
   @Test
-  public void testGoToUseAnotherPassPhrase() {
-    testShowRepeatingPassPhraseScreen();
+  fun testGoToUseAnotherPassPhrase() {
+    testShowRepeatingPassPhraseScreen()
 
-    onView(withId(R.id.buttonUseAnotherPassPhrase)).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.imageButtonShowPasswordHint)).check(matches(isDisplayed()));
+    onView(withId(R.id.buttonUseAnotherPassPhrase))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.imageButtonShowPasswordHint))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testCheckEraseOfRepeatingPassPhrase() {
-    testShowRepeatingPassPhraseScreen();
+  fun testCheckEraseOfRepeatingPassPhrase() {
+    testShowRepeatingPassPhraseScreen()
 
-    onView(withId(R.id.editTextKeyPasswordSecond)).check(
-        matches(isDisplayed())).perform(replaceText(PERFECT_PASSWORD), closeSoftKeyboard());
-    onView(withId(R.id.buttonUseAnotherPassPhrase)).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.buttonSetPassPhrase)).check(matches(isDisplayed()));
+    onView(withId(R.id.editTextKeyPasswordSecond))
+        .check(matches(isDisplayed()))
+        .perform(replaceText(PERFECT_PASSWORD), closeSoftKeyboard())
+    onView(withId(R.id.buttonUseAnotherPassPhrase))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.buttonSetPassPhrase))
+        .check(matches(isDisplayed()))
 
-    testShowRepeatingPassPhraseScreen();
-    onView(withId(R.id.editTextKeyPasswordSecond)).check(
-        matches(isDisplayed())).check(matches(withText(isEmptyString())));
+    testShowRepeatingPassPhraseScreen()
+    onView(withId(R.id.editTextKeyPasswordSecond))
+        .check(matches(isDisplayed())).check(matches(withText(isEmptyString())))
   }
 
   @Test
-  public void testChangingQualityOfPassPhrase() {
-    String[] passPhrases = {WEAK_PASSWORD, POOR_PASSWORD, REASONABLE_PASSWORD,
-        GOOD_PASSWORD, GREAT_PASSWORD, PERFECT_PASSWORD};
+  fun testChangingQualityOfPassPhrase() {
+    val passPhrases = arrayOf(WEAK_PASSWORD, POOR_PASSWORD, REASONABLE_PASSWORD, GOOD_PASSWORD,
+        GREAT_PASSWORD, PERFECT_PASSWORD)
 
-    String[] degreeOfReliabilityOfPassPhrase = {
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_weak),
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_poor),
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_reasonable),
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_good),
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_great),
-        InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.password_quality_perfect),
-    };
+    val degreeOfReliabilityOfPassPhrase = arrayOf(getResString(R.string.password_quality_weak),
+        getResString(R.string.password_quality_poor),
+        getResString(R.string.password_quality_reasonable),
+        getResString(R.string.password_quality_good),
+        getResString(R.string.password_quality_great),
+        getResString(R.string.password_quality_perfect))
 
-    for (int i = 0; i < passPhrases.length; i++) {
-      onView(withId(R.id.editTextKeyPassword)).check(matches(isDisplayed())).perform(replaceText(passPhrases[i]));
-      onView(withId(R.id.textViewPasswordQualityInfo)).check(matches(withText(
-          startsWith(degreeOfReliabilityOfPassPhrase[i].toUpperCase()))));
-      onView(withId(R.id.editTextKeyPassword)).check(matches(isDisplayed())).perform(clearText());
+    for (i in passPhrases.indices) {
+      onView(withId(R.id.editTextKeyPassword))
+          .check(matches(isDisplayed()))
+          .perform(replaceText(passPhrases[i]))
+      onView(withId(R.id.textViewPasswordQualityInfo))
+          .check(matches(withText(startsWith(degreeOfReliabilityOfPassPhrase[i].toUpperCase()))))
+      onView(withId(R.id.editTextKeyPassword))
+          .check(matches(isDisplayed()))
+          .perform(clearText())
     }
   }
 
   @Test
-  public void testShowDialogAboutBadPassPhrase() {
-    String[] badPassPhrases = {WEAK_PASSWORD, POOR_PASSWORD};
+  fun testShowDialogAboutBadPassPhrase() {
+    val badPassPhrases = arrayOf(WEAK_PASSWORD, POOR_PASSWORD)
 
-    for (String passPhrase : badPassPhrases) {
-      onView(withId(R.id.editTextKeyPassword)).check(matches(isDisplayed())).perform(replaceText(passPhrase),
-          closeSoftKeyboard());
-      onView(withId(R.id.buttonSetPassPhrase)).check(matches(isDisplayed())).perform(click());
-
-      onView(withText(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string
-          .select_stronger_pass_phrase)))
-          .check(matches(isDisplayed()));
-      onView(withId(android.R.id.button1)).check(matches(isDisplayed())).perform(click());
-      onView(withId(R.id.editTextKeyPassword)).check(matches(isDisplayed())).perform(clearText());
+    for (passPhrase in badPassPhrases) {
+      onView(withId(R.id.editTextKeyPassword))
+          .check(matches(isDisplayed()))
+          .perform(replaceText(passPhrase), closeSoftKeyboard())
+      onView(withId(R.id.buttonSetPassPhrase))
+          .check(matches(isDisplayed()))
+          .perform(click())
+      onView(withText(getResString(R.string.select_stronger_pass_phrase)))
+          .check(matches(isDisplayed()))
+      onView(withId(android.R.id.button1))
+          .check(matches(isDisplayed()))
+          .perform(click())
+      onView(withId(R.id.editTextKeyPassword))
+          .check(matches(isDisplayed()))
+          .perform(clearText())
     }
   }
 
-  protected void checkIsNonEmptyHintShown() {
-    onView(withText(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string
-        .passphrase_must_be_non_empty)))
-        .check(matches(isDisplayed()));
-    onView(withId(com.google.android.material.R.id.snackbar_action)).check(matches(isDisplayed()))
-        .perform(click());
+  protected fun checkIsNonEmptyHintShown() {
+    onView(withText(getResString(R.string.passphrase_must_be_non_empty)))
+        .check(matches(isDisplayed()))
+    onView(withId(com.google.android.material.R.id.snackbar_action))
+        .check(matches(isDisplayed()))
+        .perform(click())
+  }
+
+  companion object {
+    internal const val WEAK_PASSWORD = "weak"
+    internal const val POOR_PASSWORD = "weak, perfect, great"
+    internal const val REASONABLE_PASSWORD = "weak, poor, reasonable"
+    internal const val GOOD_PASSWORD = "weak, poor, good,"
+    internal const val GREAT_PASSWORD = "weak, poor, great, good"
+    internal const val PERFECT_PASSWORD = "unconventional blueberry unlike any other"
   }
 }
