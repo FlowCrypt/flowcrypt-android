fc6430277f4c50a84fced5055855bef31f818666
DenBond7 <DenBond7@gmail.com>
2019-05-06 16:30:31 +0300

    Transformed ContactsSettingsActivityTest to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/ContactsSettingsActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/ContactsSettingsActivityTest.kt
@@ -3,38 +3,36 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.database.dao.source.ContactsDaoSource;
-import com.flowcrypt.email.model.PgpContact;
-import com.flowcrypt.email.rules.AddAccountToDatabaseRule;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-import com.flowcrypt.email.ui.activity.settings.ContactsSettingsActivity;
-
-import org.junit.AfterClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.flowcrypt.email.matchers.CustomMatchers.withEmptyListView;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.not;
+import android.view.View
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.database.dao.source.ContactsDaoSource
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withEmptyListView
+import com.flowcrypt.email.model.PgpContact
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.ui.activity.settings.ContactsSettingsActivity
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.not
+import org.junit.AfterClass
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
  * @author Denis Bondarenko
@@ -43,66 +41,66 @@ import static org.hamcrest.Matchers.not;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class ContactsSettingsActivityTest extends BaseTest {
-  private static final String[] EMAILS = new String[]{
-      "contact_0@denbond7.com",
-      "contact_1@denbond7.com",
-      "contact_2@denbond7.com",
-      "contact_3@denbond7.com"};
+@RunWith(AndroidJUnit4::class)
+class ContactsSettingsActivityTest : BaseTest() {
 
-  private ActivityTestRule activityTestRule = new ActivityTestRule<>(ContactsSettingsActivity.class);
+  override val activityTestRule: ActivityTestRule<*>? = ActivityTestRule(ContactsSettingsActivity::class.java)
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(new AddAccountToDatabaseRule())
-      .around(activityTestRule);
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule())
+      .around(activityTestRule)
 
-  @AfterClass
-  public static void clearContactsFromDatabase() {
-    ContactsDaoSource contactsDaoSource = new ContactsDaoSource();
-    for (String email : EMAILS) {
-      contactsDaoSource.deletePgpContact(InstrumentationRegistry.getInstrumentation().getTargetContext(), email);
-    }
-  }
-
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return activityTestRule;
+  @Test
+  fun testShowHelpScreen() {
+    testHelpScreen()
   }
 
   @Test
-  public void testShowHelpScreen() {
-    testHelpScreen();
+  fun testEmptyList() {
+    onView(withId(R.id.listViewContacts))
+        .check(matches(withEmptyListView())).check(matches(not<View>(isDisplayed())))
+    onView(withId(R.id.emptyView))
+        .check(matches(isDisplayed())).check(matches(withText(R.string.no_results)))
   }
 
   @Test
-  public void testEmptyList() {
-    onView(withId(R.id.listViewContacts)).check(matches(withEmptyListView())).check(matches(not(isDisplayed())));
-    onView(withId(R.id.emptyView)).check(matches(isDisplayed())).check(matches(withText(R.string.no_results)));
-  }
-
-  @Test
-  public void testDeleteContacts() {
-    addContactsToDatabase();
-    for (String ignored : EMAILS) {
+  fun testDeleteContacts() {
+    addContactsToDatabase()
+    for (ignored in EMAILS) {
       onData(anything())
           .inAdapterView(withId(R.id.listViewContacts))
           .onChildView(withId(R.id.imageButtonDeleteContact))
           .atPosition(0)
-          .perform(click());
+          .perform(click())
     }
-    testEmptyList();
-    clearContactsFromDatabase();
+    testEmptyList()
+    clearContactsFromDatabase()
   }
 
-  private void addContactsToDatabase() {
-    ContactsDaoSource contactsDaoSource = new ContactsDaoSource();
-    for (String email : EMAILS) {
-      PgpContact pgpContact = new PgpContact(email, null, "", true, null,
-          false, null, null, null, 0);
-      contactsDaoSource.addRow(InstrumentationRegistry.getInstrumentation().getTargetContext(), pgpContact);
+  private fun addContactsToDatabase() {
+    val contactsDaoSource = ContactsDaoSource()
+    for (email in EMAILS) {
+      val pgpContact = PgpContact(email, null, "", true, null,
+          false, null, null, null, 0)
+      contactsDaoSource.addRow(getTargetContext(), pgpContact)
+    }
+  }
+
+  companion object {
+    private val EMAILS = arrayOf(
+        "contact_0@denbond7.com",
+        "contact_1@denbond7.com",
+        "contact_2@denbond7.com",
+        "contact_3@denbond7.com")
+
+    @AfterClass
+    fun clearContactsFromDatabase() {
+      val contactsDaoSource = ContactsDaoSource()
+      for (email in EMAILS) {
+        contactsDaoSource.deletePgpContact(InstrumentationRegistry.getInstrumentation().targetContext, email)
+      }
     }
   }
 }
