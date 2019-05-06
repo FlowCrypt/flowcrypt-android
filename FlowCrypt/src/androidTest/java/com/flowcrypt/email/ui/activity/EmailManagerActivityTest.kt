d511355f27e23067a10dc2c33b2f9e11e8397d08
DenBond7 <DenBond7@gmail.com>
2019-05-06 17:17:03 +0300

    Transformed BaseEmailListActivityTest and extended classes to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/EmailManagerActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/EmailManagerActivityTest.kt
@@ -3,67 +3,59 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.api.email.LocalFolder;
-import com.flowcrypt.email.database.dao.source.AccountDao;
-import com.flowcrypt.email.database.dao.source.AccountDaoSource;
-import com.flowcrypt.email.rules.AddAccountToDatabaseRule;
-import com.flowcrypt.email.rules.AddLabelsToDatabaseRule;
-import com.flowcrypt.email.rules.AddMessageToDatabaseRule;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-import com.flowcrypt.email.ui.activity.base.BaseEmailListActivityTest;
-import com.flowcrypt.email.ui.activity.settings.SettingsActivity;
-import com.flowcrypt.email.util.AccountDaoManager;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.IdlingResource;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.action.ViewActions.swipeDown;
-import static androidx.test.espresso.action.ViewActions.swipeUp;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.DrawerActions.open;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.flowcrypt.email.matchers.CustomMatchers.withEmptyListView;
-import static com.flowcrypt.email.matchers.CustomMatchers.withToolBarText;
-import static com.flowcrypt.email.viewaction.CustomViewActions.navigateToItemWithName;
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.not;
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.ComponentName
+import android.content.Intent
+import android.view.View
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.swipeDown
+import androidx.test.espresso.action.ViewActions.swipeUp
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions.open
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.api.email.LocalFolder
+import com.flowcrypt.email.database.dao.source.AccountDaoSource
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withEmptyListView
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withToolBarText
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddLabelsToDatabaseRule
+import com.flowcrypt.email.rules.AddMessageToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.ui.activity.base.BaseEmailListActivityTest
+import com.flowcrypt.email.ui.activity.settings.SettingsActivity
+import com.flowcrypt.email.util.AccountDaoManager
+import com.flowcrypt.email.viewaction.CustomViewActions.Companion.navigateToItemWithName
+import org.hamcrest.CoreMatchers.anyOf
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import java.util.*
 
 /**
  * @author Denis Bondarenko
@@ -72,157 +64,185 @@ import static org.hamcrest.Matchers.not;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class EmailManagerActivityTest extends BaseEmailListActivityTest {
-  private static final List<LocalFolder> LOCAL_FOLDERS;
-  private static final LocalFolder INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT =
-      new LocalFolder("INBOX", "INBOX", 0, new String[]{"\\HasNoChildren"}, false);
+@RunWith(AndroidJUnit4::class)
+class EmailManagerActivityTest : BaseEmailListActivityTest() {
 
-  static {
-    LOCAL_FOLDERS = new ArrayList<>();
-    LOCAL_FOLDERS.add(INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT);
-    LOCAL_FOLDERS.add(new LocalFolder("Drafts", "Drafts", 0, new String[]{"\\HasNoChildren", "\\Drafts"}, false));
-    LOCAL_FOLDERS.add(new LocalFolder("Sent", "Sent", 0, new String[]{"\\HasNoChildren", "\\Sent"}, false));
-    LOCAL_FOLDERS.add(new LocalFolder("Junk", "Junk", 0, new String[]{"\\HasNoChildren", "\\Junk"}, false));
-  }
+  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(EmailManagerActivity::class.java)
 
-  private AccountDao userWithoutLetters = AccountDaoManager.getAccountDao("user_without_letters.json");
-  private AccountDao userWithMoreThan21LettersAccount = AccountDaoManager.getUserWitMoreThan21Letters();
-  private IntentsTestRule intentsTestRule = new IntentsTestRule<>(EmailManagerActivity.class);
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(new AddAccountToDatabaseRule(userWithoutLetters))
-      .around(new AddAccountToDatabaseRule(userWithMoreThan21LettersAccount))
-      .around(new AddLabelsToDatabaseRule(userWithMoreThan21LettersAccount, LOCAL_FOLDERS))
-      .around(new AddMessageToDatabaseRule(userWithMoreThan21LettersAccount,
-          INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT))
-      .around(intentsTestRule);
+  private val userWithoutLetters = AccountDaoManager.getAccountDao("user_without_letters.json")
+  private val userWithMoreThan21LettersAccount = AccountDaoManager.getUserWitMoreThan21Letters()
 
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return intentsTestRule;
-  }
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule(userWithoutLetters))
+      .around(AddAccountToDatabaseRule(userWithMoreThan21LettersAccount))
+      .around(AddLabelsToDatabaseRule(userWithMoreThan21LettersAccount, LOCAL_FOLDERS))
+      .around(AddMessageToDatabaseRule(userWithMoreThan21LettersAccount, INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT))
+      .around(activityTestRule)
 
   @Before
-  public void registerIdlingResource() {
-    IdlingRegistry.getInstance().register(((EmailManagerActivity) intentsTestRule.getActivity())
-        .getMsgsCountingIdlingResource());
-    IdlingRegistry.getInstance().register(((EmailManagerActivity) intentsTestRule.getActivity())
-        .getCountingIdlingResourceForLabel());
+  fun registerIdlingResource() {
+    IdlingRegistry.getInstance().register((activityTestRule?.activity as EmailManagerActivity).msgsCountingIdlingResource)
+    IdlingRegistry.getInstance().register((activityTestRule.activity as EmailManagerActivity).countingIdlingResourceForLabel)
   }
 
   @After
-  public void unregisterIdlingResource() {
-    for (IdlingResource idlingResource : IdlingRegistry.getInstance().getResources()) {
-      IdlingRegistry.getInstance().unregister(idlingResource);
+  fun unregisterIdlingResource() {
+    for (idlingResource in IdlingRegistry.getInstance().resources) {
+      IdlingRegistry.getInstance().unregister(idlingResource)
     }
   }
 
   @Test
-  public void testComposeFloatButton() {
-    onView(withId(R.id.floatActionButtonCompose)).check(matches(isDisplayed())).perform(click());
-    intended(hasComponent(CreateMessageActivity.class.getName()));
-    onView(allOf(withText(R.string.compose), withParent(withId(R.id.toolbar)))).check(matches(isDisplayed()));
+  fun testComposeFloatButton() {
+    onView(withId(R.id.floatActionButtonCompose))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    intended(hasComponent(CreateMessageActivity::class.java.name))
+    onView(allOf<View>(withText(R.string.compose), withParent(withId(R.id.toolbar))))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testRunMsgDetailsActivity() {
-    testRunMsgDetailsActivity(0);
+  fun testRunMsgDetailsActivity() {
+    testRunMsgDetailsActivity(0)
   }
 
   @Test
-  public void testForceLoadMsgs() {
+  fun testForceLoadMsgs() {
     onData(anything())
         .inAdapterView(withId(R.id.listViewMessages))
         .atPosition(0)
-        .perform(scrollTo());
-    onView(withId(R.id.listViewMessages)).check(matches(isDisplayed())).perform(swipeDown());
-    onView(withId(R.id.listViewMessages)).check(matches(not(withEmptyListView()))).check(matches(isDisplayed()));
+        .perform(scrollTo())
+    onView(withId(R.id.listViewMessages))
+        .check(matches(isDisplayed()))
+        .perform(swipeDown())
+    onView(withId(R.id.listViewMessages))
+        .check(matches(not<View>(withEmptyListView())))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testOpenAndSwipeNavigationView() {
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.navigationView)).perform(swipeUp());
+  fun testOpenAndSwipeNavigationView() {
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.navigationView))
+        .perform(swipeUp())
   }
 
   @Test
-  public void testShowSplashActivityAfterLogout() {
-    clickLogOut();
-    clickLogOut();
-    intended(hasComponent(SignInActivity.class.getName()));
+  fun testShowSplashActivityAfterLogout() {
+    clickLogOut()
+    clickLogOut()
+    intended(hasComponent(SignInActivity::class.java.name))
   }
 
   @Test
-  public void testClickLogOutIfMoreAccounts() {
-    clickLogOut();
-    onView(withId(R.id.floatActionButtonCompose)).check(matches(isDisplayed()));
+  fun testClickLogOutIfMoreAccounts() {
+    clickLogOut()
+    onView(withId(R.id.floatActionButtonCompose))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testGoToSettingsActivity() {
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.navigationView)).perform(swipeUp());
-    onView(withText(R.string.action_settings)).check(matches(isDisplayed())).perform(click());
-    intended(hasComponent(SettingsActivity.class.getName()));
+  fun testGoToSettingsActivity() {
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.navigationView))
+        .perform(swipeUp())
+    onView(withText(R.string.action_settings))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    intended(hasComponent(SettingsActivity::class.java.name))
   }
 
   @Test
-  public void testSwitchLabels() {
-    String menuItem = "Sent";
-    onView(withId(R.id.toolbar)).check(matches(anyOf(
+  fun testSwitchLabels() {
+    val menuItem = "Sent"
+    onView(withId(R.id.toolbar)).check(matches(anyOf<View>(
         withToolBarText("INBOX"),
-        withToolBarText(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R
-            .string.loading)))));
+        withToolBarText(InstrumentationRegistry.getInstrumentation().targetContext.getString(R
+            .string.loading)))))
 
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.navigationView)).perform(navigateToItemWithName(menuItem));
-    onView(withId(R.id.toolbar)).check(matches(withToolBarText(menuItem)));
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.navigationView))
+        .perform(navigateToItemWithName(menuItem))
+    onView(withId(R.id.toolbar))
+        .check(matches(withToolBarText(menuItem)))
   }
 
   @Test
-  public void testAddNewAccount() {
-    Context targetContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-    AccountDao account = AccountDaoManager.getDefaultAccountDao();
-    Intent result = new Intent();
-    result.putExtra(AddNewAccountActivity.KEY_EXTRA_NEW_ACCOUNT, account);
-    intending(hasComponent(new ComponentName(targetContext, AddNewAccountActivity.class)))
-        .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, result));
+  fun testAddNewAccount() {
+    val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+    val account = AccountDaoManager.getDefaultAccountDao()
+    val result = Intent()
+    result.putExtra(AddNewAccountActivity.KEY_EXTRA_NEW_ACCOUNT, account)
+    intending(hasComponent(ComponentName(targetContext, AddNewAccountActivity::class.java)))
+        .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, result))
 
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.layoutUserDetails)).check(matches(isDisplayed())).perform(click(), click());
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.layoutUserDetails))
+        .check(matches(isDisplayed()))
+        .perform(click(), click())
 
     try {
-      AccountDaoSource accountDaoSource = new AccountDaoSource();
-      accountDaoSource.addRow(targetContext, account.getAuthCreds());
-      accountDaoSource.setActiveAccount(targetContext, account.getEmail());
-    } catch (Exception e) {
-      e.printStackTrace();
+      val accountDaoSource = AccountDaoSource()
+      accountDaoSource.addRow(targetContext, account.authCreds)
+      accountDaoSource.setActiveAccount(targetContext, account.email)
+    } catch (e: Exception) {
+      e.printStackTrace()
     }
 
-    onView(withId(R.id.viewIdAddNewAccount)).check(matches(isDisplayed())).perform(click());
+    onView(withId(R.id.viewIdAddNewAccount))
+        .check(matches(isDisplayed())).perform(click())
 
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.textViewActiveUserEmail)).check(matches(isDisplayed()))
-        .check(matches(withText(account.getEmail())));
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.textViewActiveUserEmail))
+        .check(matches(isDisplayed())).check(matches(withText(account.email)))
   }
 
   @Test
-  public void testChooseAnotherAccount() {
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.textViewActiveUserEmail)).check(matches(isDisplayed())).check(matches(withText
-        (userWithMoreThan21LettersAccount.getEmail())));
-    onView(withId(R.id.layoutUserDetails)).check(matches(isDisplayed())).perform(click(), click());
-    onView(withText(userWithoutLetters.getEmail())).check(matches(isDisplayed())).perform(click());
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.textViewActiveUserEmail)).check(matches(isDisplayed())).check(
-        matches(withText(userWithoutLetters.getEmail())));
+  fun testChooseAnotherAccount() {
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.textViewActiveUserEmail))
+        .check(matches(isDisplayed())).check(matches(withText(userWithMoreThan21LettersAccount.email)))
+    onView(withId(R.id.layoutUserDetails))
+        .check(matches(isDisplayed()))
+        .perform(click(), click())
+    onView(withText(userWithoutLetters.email))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.textViewActiveUserEmail))
+        .check(matches(isDisplayed())).check(matches(withText(userWithoutLetters.email)))
   }
 
-  private void clickLogOut() {
-    onView(withId(R.id.drawer_layout)).perform(open());
-    onView(withId(R.id.navigationView)).perform(swipeUp());
-    onView(withText(R.string.log_out)).check(matches(isDisplayed())).perform(click());
+  private fun clickLogOut() {
+    onView(withId(R.id.drawer_layout))
+        .perform(open())
+    onView(withId(R.id.navigationView))
+        .perform(swipeUp())
+    onView(withText(R.string.log_out)
+    ).check(matches(isDisplayed()))
+        .perform(click())
+  }
+
+  companion object {
+    private val LOCAL_FOLDERS: MutableList<LocalFolder>
+    private val INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT = LocalFolder("INBOX", "INBOX", 0, arrayOf("\\HasNoChildren"), false)
+
+    init {
+      LOCAL_FOLDERS = ArrayList()
+      LOCAL_FOLDERS.add(INBOX_USER_WITH_MORE_THAN_21_LETTERS_ACCOUNT)
+      LOCAL_FOLDERS.add(LocalFolder("Drafts", "Drafts", 0, arrayOf("\\HasNoChildren", "\\Drafts"), false))
+      LOCAL_FOLDERS.add(LocalFolder("Sent", "Sent", 0, arrayOf("\\HasNoChildren", "\\Sent"), false))
+      LOCAL_FOLDERS.add(LocalFolder("Junk", "Junk", 0, arrayOf("\\HasNoChildren", "\\Junk"), false))
+    }
   }
 }
--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BaseEmailListActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BaseEmailListActivityTest.kt
@@ -3,25 +3,25 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity.base;
+package com.flowcrypt.email.ui.activity.base
 
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.ui.activity.MessageDetailsActivity;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.flowcrypt.email.matchers.CustomMatchers.withListViewItemCount;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.isEmptyString;
-import static org.hamcrest.Matchers.not;
+import android.view.View
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withListViewItemCount
+import com.flowcrypt.email.ui.activity.MessageDetailsActivity
+import org.hamcrest.Matchers.anything
+import org.hamcrest.Matchers.isEmptyString
+import org.hamcrest.Matchers.not
 
 /**
  * @author Denis Bondarenko
@@ -29,24 +29,27 @@ import static org.hamcrest.Matchers.not;
  * Time: 11:11
  * E-mail: DenBond7@gmail.com
  */
-public abstract class BaseEmailListActivityTest extends BaseTest {
+abstract class BaseEmailListActivityTest : BaseTest() {
 
-  protected void testDownloadAllMsgs(int messageCount) {
-    onView(withId(R.id.emptyView)).check(matches(not(isDisplayed())));
+  protected fun testDownloadAllMsgs(messageCount: Int) {
+    onView(withId(R.id.emptyView))
+        .check(matches(not<View>(isDisplayed())))
     // size of list = number of the letters in the mail + 1 footer.
-    onView(withId(R.id.listViewMessages)).check(matches(withListViewItemCount(messageCount))).check(matches(isDisplayed()));
+    onView(withId(R.id.listViewMessages))
+        .check(matches(withListViewItemCount(messageCount))).check(matches(isDisplayed()))
   }
 
-  protected void testRunMsgDetailsActivity(int position) {
+  protected fun testRunMsgDetailsActivity(position: Int) {
     onData(anything())
         .inAdapterView(withId(R.id.listViewMessages))
         .atPosition(position)
-        .perform(click());
-    intended(hasComponent(MessageDetailsActivity.class.getName()));
-    onView(withId(R.id.textViewSenderAddress)).check(matches(isDisplayed()))
-        .check(matches(withText(not(isEmptyString()))));
-    onView(withId(R.id.textViewSubject)).check(matches(isDisplayed()))
-        .check(matches(withText(not(isEmptyString()))));
-    onView(withId(R.id.textViewDate)).check(matches(isDisplayed())).check(matches(withText(not(isEmptyString()))));
+        .perform(click())
+    intended(hasComponent(MessageDetailsActivity::class.java.name))
+    onView(withId(R.id.textViewSenderAddress))
+        .check(matches(isDisplayed())).check(matches(withText(not(isEmptyString()))))
+    onView(withId(R.id.textViewSubject))
+        .check(matches(isDisplayed())).check(matches(withText(not(isEmptyString()))))
+    onView(withId(R.id.textViewDate))
+        .check(matches(isDisplayed())).check(matches(withText(not(isEmptyString()))))
   }
 }
