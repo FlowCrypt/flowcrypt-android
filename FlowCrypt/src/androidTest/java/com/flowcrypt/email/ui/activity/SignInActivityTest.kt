75a28475f95e2b4af05786d11eb8402d9fd54e9d
DenBond7 <DenBond7@gmail.com>
2019-05-07 15:31:06 +0300

    Transformed SignInActivityTest to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/SignInActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/SignInActivityTest.kt
@@ -3,43 +3,41 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import android.app.Activity;
-import android.app.Instrumentation;
-
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.not;
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.isInternal
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
- * A test for {@link SignInActivity}
+ * A test for [SignInActivity]
  *
  * @author Denis Bondarenko
  * Date: 13.02.2018
@@ -47,55 +45,63 @@ import static org.hamcrest.Matchers.not;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class SignInActivityTest extends BaseTest {
-  private IntentsTestRule intentsTestRule = new IntentsTestRule<>(SignInActivity.class);
+@RunWith(AndroidJUnit4::class)
+class SignInActivityTest : BaseTest() {
+  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(SignInActivity::class.java)
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(intentsTestRule);
-
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return intentsTestRule;
-  }
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(activityTestRule)
 
   @Before
-  public void stubAllExternalIntents() {
+  fun stubAllExternalIntents() {
     // All external Intents will be blocked.
-    intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    intending(not<Intent>(isInternal())).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
   }
 
   @Test
-  public void testUseOtherEmailProviders() {
-    onView(withId(R.id.buttonOtherEmailProvider)).check(matches(isDisplayed())).perform(click());
-    onView(allOf(withText(R.string.adding_new_account), withParent(withId(R.id.toolbar)))).check(matches
-        (isDisplayed()));
+  fun testUseOtherEmailProviders() {
+    onView(withId(R.id.buttonOtherEmailProvider))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(allOf<View>(withText(R.string.adding_new_account), withParent(withId(R.id.toolbar))))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testUseGmail() {
-    onView(withId(R.id.buttonSignInWithGmail)).check(matches(isDisplayed())).perform(click());
+  fun testUseGmail() {
+    onView(withId(R.id.buttonSignInWithGmail))
+        .check(matches(isDisplayed()))
+        .perform(click())
     //check that the Google Sign-in screen displayed
-    intended(toPackage("com.google.android.gms"));
+    intended(toPackage("com.google.android.gms"))
   }
 
   @Test
-  public void testShowPrivacyScreen() {
-    onView(withId(R.id.buttonPrivacy)).check(matches(isDisplayed())).perform(click());
-    onView(allOf(withText(R.string.privacy), withParent(withId(R.id.toolbar)))).check(matches(isDisplayed()));
+  fun testShowPrivacyScreen() {
+    onView(withId(R.id.buttonPrivacy))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(allOf<View>(withText(R.string.privacy), withParent(withId(R.id.toolbar))))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testShowTermsScreen() {
-    onView(withId(R.id.buttonTerms)).check(matches(isDisplayed())).perform(click());
-    onView(allOf(withText(R.string.terms), withParent(withId(R.id.toolbar)))).check(matches(isDisplayed()));
+  fun testShowTermsScreen() {
+    onView(withId(R.id.buttonTerms))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(allOf<View>(withText(R.string.terms), withParent(withId(R.id.toolbar))))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testShowSecurityScreen() {
-    onView(withId(R.id.buttonSecurity)).check(matches(isDisplayed())).perform(click());
-    onView(allOf(withText(R.string.security), withParent(withId(R.id.toolbar)))).check(matches(isDisplayed()));
+  fun testShowSecurityScreen() {
+    onView(withId(R.id.buttonSecurity))
+        .check(matches(isDisplayed()))
+        .perform(click())
+    onView(allOf<View>(withText(R.string.security), withParent(withId(R.id.toolbar))))
+        .check(matches(isDisplayed()))
   }
 }
