5efc6413a198dd5ca7211685fde0d8943a2ee66e
DenBond7 <DenBond7@gmail.com>
2019-05-07 14:43:42 +0300

    Transformed SearchBackupsInEmailActivityTest to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/SearchBackupsInEmailActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/SearchBackupsInEmailActivityTest.kt
@@ -3,37 +3,35 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.rules.AddAccountToDatabaseRule;
-import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-import com.flowcrypt.email.ui.activity.settings.SearchBackupsInEmailActivity;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.not;
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.ui.activity.settings.SearchBackupsInEmailActivity
+import org.hamcrest.Matchers.not
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
  * @author Denis Bondarenko
@@ -42,74 +40,81 @@ import static org.hamcrest.Matchers.not;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class SearchBackupsInEmailActivityTest extends BaseTest {
+@RunWith(AndroidJUnit4::class)
+class SearchBackupsInEmailActivityTest : BaseTest() {
 
-  private IntentsTestRule activityTestRule = new IntentsTestRule<>(SearchBackupsInEmailActivity.class);
+  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(SearchBackupsInEmailActivity::class.java)
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(new AddAccountToDatabaseRule())
-      .around(new AddPrivateKeyToDatabaseRule())
-      .around(activityTestRule);
-
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return activityTestRule;
-  }
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule())
+      .around(AddPrivateKeyToDatabaseRule())
+      .around(activityTestRule)
 
   @Before
-  public void registerNodeIdling() {
-    IdlingRegistry.getInstance().register(((SearchBackupsInEmailActivity) activityTestRule.getActivity())
-        .getCountingIdlingResource());
+  override fun registerNodeIdling() {
+    val activity = activityTestRule?.activity ?: return
+    if (activity is SearchBackupsInEmailActivity) {
+      IdlingRegistry.getInstance().register(activity.countingIdlingResource)
+    }
   }
 
   @After
-  public void unregisterNodeIdling() {
-    IdlingRegistry.getInstance().unregister(((SearchBackupsInEmailActivity) activityTestRule.getActivity())
-        .getCountingIdlingResource());
+  override fun unregisterNodeIdling() {
+    val activity = activityTestRule?.activity ?: return
+    if (activity is SearchBackupsInEmailActivity) {
+      IdlingRegistry.getInstance().unregister(activity.countingIdlingResource)
+    }
   }
 
   @Test
-  public void testShowHelpScreen() {
-    testHelpScreen();
+  fun testShowHelpScreen() {
+    testHelpScreen()
   }
 
   @Test
-  public void testIsBackupFound() {
-    onView(withId(R.id.buttonSeeMoreBackupOptions)).check(matches(isDisplayed()));
-    onView(withId(R.id.textViewBackupFound)).check(matches(isDisplayed()));
+  fun testIsBackupFound() {
+    onView(withId(R.id.buttonSeeMoreBackupOptions))
+        .check(matches(isDisplayed()))
+    onView(withId(R.id.textViewBackupFound))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testShowBackupOptions() {
-    testIsBackupFound();
-    onView(withId(R.id.buttonSeeMoreBackupOptions)).perform(click());
-    onView(withId(R.id.buttonBackupAction)).check(matches(isDisplayed()));
+  fun testShowBackupOptions() {
+    testIsBackupFound()
+    onView(withId(R.id.buttonSeeMoreBackupOptions))
+        .perform(click())
+    onView(withId(R.id.buttonBackupAction))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testSelectEmailForSavingBackup() {
-    testShowBackupOptions();
-    onView(withId(R.id.radioButtonEmail)).check(matches(isDisplayed()))
-        .perform(click()).check(matches(isChecked()));
-    onView(withId(R.id.textViewOptionsHint)).check(matches(isDisplayed()))
-        .check(matches(withText(R.string.backup_as_email_hint)));
-    onView(withId(R.id.buttonBackupAction)).check(matches(withText(R.string.backup_as_email)));
-    onView(withId(R.id.radioButtonDownload)).check(matches(isDisplayed()))
-        .check(matches(not(isChecked())));
+  fun testSelectEmailForSavingBackup() {
+    testShowBackupOptions()
+    onView(withId(R.id.radioButtonEmail))
+        .check(matches(isDisplayed()))
+        .perform(click()).check(matches(isChecked()))
+    onView(withId(R.id.textViewOptionsHint))
+        .check(matches(isDisplayed())).check(matches(withText(R.string.backup_as_email_hint)))
+    onView(withId(R.id.buttonBackupAction))
+        .check(matches(withText(R.string.backup_as_email)))
+    onView(withId(R.id.radioButtonDownload))
+        .check(matches(isDisplayed())).check(matches(not<View>(isChecked())))
   }
 
   @Test
-  public void testSelectDownloadToFileForSavingBackup() {
-    testShowBackupOptions();
-    onView(withId(R.id.radioButtonDownload)).check(matches(isDisplayed()))
-        .perform(click()).check(matches(isChecked()));
-    onView(withId(R.id.textViewOptionsHint)).check(matches(isDisplayed()))
-        .check(matches(withText(R.string.backup_as_download_hint)));
-    onView(withId(R.id.buttonBackupAction)).check(matches(withText(R.string.backup_as_a_file)));
-    onView(withId(R.id.radioButtonEmail)).check(matches(isDisplayed()))
-        .check(matches(not(isChecked())));
+  fun testSelectDownloadToFileForSavingBackup() {
+    testShowBackupOptions()
+    onView(withId(R.id.radioButtonDownload))
+        .check(matches(isDisplayed()))
+        .perform(click()).check(matches(isChecked()))
+    onView(withId(R.id.textViewOptionsHint))
+        .check(matches(isDisplayed())).check(matches(withText(R.string.backup_as_download_hint)))
+    onView(withId(R.id.buttonBackupAction))
+        .check(matches(withText(R.string.backup_as_a_file)))
+    onView(withId(R.id.radioButtonEmail))
+        .check(matches(isDisplayed())).check(matches(not<View>(isChecked())))
   }
 }
