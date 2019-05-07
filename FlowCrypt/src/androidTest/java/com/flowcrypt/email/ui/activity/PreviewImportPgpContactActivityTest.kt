83fb6026e56d0c895fe377a3c554bfba485a8407
DenBond7 <DenBond7@gmail.com>
2019-05-07 14:36:41 +0300

    Transformed PreviewImportPgpContactActivityTest to use Kotlin.| #535

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/PreviewImportPgpContactActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/PreviewImportPgpContactActivityTest.kt
@@ -3,36 +3,33 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity;
+package com.flowcrypt.email.ui.activity
 
-import com.flowcrypt.email.R;
-import com.flowcrypt.email.assertions.RecyclerViewItemCountAssertion;
-import com.flowcrypt.email.base.BaseTest;
-import com.flowcrypt.email.database.dao.source.ContactsDaoSource;
-import com.flowcrypt.email.model.PgpContact;
-import com.flowcrypt.email.rules.AddAccountToDatabaseRule;
-import com.flowcrypt.email.rules.ClearAppSettingsRule;
-import com.flowcrypt.email.util.PrivateKeysManager;
-import com.flowcrypt.email.util.TestGeneralUtil;
-import com.flowcrypt.email.viewaction.ClickOnViewInRecyclerViewItem;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-
-import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.flowcrypt.email.R
+import com.flowcrypt.email.assertions.RecyclerViewItemCountAssertion
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.database.dao.source.ContactsDaoSource
+import com.flowcrypt.email.model.PgpContact
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.util.PrivateKeysManager
+import com.flowcrypt.email.util.TestGeneralUtil
+import com.flowcrypt.email.viewaction.ClickOnViewInRecyclerViewItem
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
  * @author Denis Bondarenko
@@ -41,84 +38,78 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
  * E-mail: DenBond7@gmail.com
  */
 @LargeTest
-@RunWith(AndroidJUnit4.class)
-public class PreviewImportPgpContactActivityTest extends BaseTest {
-  private ActivityTestRule activityTestRule =
-      new ActivityTestRule<PreviewImportPgpContactActivity>(PreviewImportPgpContactActivity.class, false, false) {
-      };
+@RunWith(AndroidJUnit4::class)
+class PreviewImportPgpContactActivityTest : BaseTest() {
+  override val activityTestRule: ActivityTestRule<*>? =
+      ActivityTestRule<PreviewImportPgpContactActivity>(PreviewImportPgpContactActivity::class.java, false, false)
 
-  @Rule
-  public TestRule ruleChain = RuleChain
-      .outerRule(new ClearAppSettingsRule())
-      .around(new AddAccountToDatabaseRule())
-      .around(activityTestRule);
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule())
+      .around(activityTestRule)
 
-  @Override
-  public ActivityTestRule getActivityTestRule() {
-    return activityTestRule;
+  private val singlePublicKeyForUnsavedContact: String
+    get() = PrivateKeysManager.getNodeKeyDetailsFromAssets("node/default@denbond7.com_fisrtKey_pub.json")
+        .publicKey
+
+  private val tenPubKeys: String =
+      TestGeneralUtil.readFileFromAssetsAsString(getContext(), "pgp/pub_keys_2048_bits_10.asc")
+
+  @Test
+  fun testShowHelpScreen() {
+    activityTestRule?.launchActivity(
+        PreviewImportPgpContactActivity.newIntent(getTargetContext(), singlePublicKeyForUnsavedContact))
+    testHelpScreen()
   }
 
   @Test
-  public void testShowHelpScreen() {
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), getSinglePublicKeyForUnsavedContact()));
-    testHelpScreen();
+  fun testIsDisplayedSingleItem() {
+    val pgpContact = PgpContact("default@denbond7.com", null,
+        singlePublicKeyForUnsavedContact, true, null, false, null, null, null, 0)
+    ContactsDaoSource().addRow(getTargetContext(), pgpContact)
+    activityTestRule?.launchActivity(
+        PreviewImportPgpContactActivity.newIntent(getTargetContext(), singlePublicKeyForUnsavedContact))
+    onView(withId(R.id.recyclerViewContacts))
+        .check(RecyclerViewItemCountAssertion(1))
+    onView(withText(getResString(R.string.template_message_part_public_key_owner, "default@denbond7.com")))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testIsDisplayedSingleItem() {
-    PgpContact pgpContact = new PgpContact("default@denbond7.com", null,
-        getSinglePublicKeyForUnsavedContact(), true, null, false, null, null, null, 0);
-    new ContactsDaoSource().addRow(InstrumentationRegistry.getInstrumentation().getTargetContext(), pgpContact);
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), getSinglePublicKeyForUnsavedContact()));
-    onView(withId(R.id.recyclerViewContacts)).check(new RecyclerViewItemCountAssertion(1));
-    onView(withText(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(
-        R.string.template_message_part_public_key_owner, "default@denbond7.com")))
-        .check(matches(isDisplayed()));
+  fun testIsDisplayedLabelAlreadyImported() {
+    activityTestRule?.launchActivity(
+        PreviewImportPgpContactActivity.newIntent(getTargetContext(), singlePublicKeyForUnsavedContact))
+    onView(withId(R.id.recyclerViewContacts))
+        .check(RecyclerViewItemCountAssertion(1))
   }
 
   @Test
-  public void testIsDisplayedLabelAlreadyImported() {
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), getSinglePublicKeyForUnsavedContact()));
-    onView(withId(R.id.recyclerViewContacts)).check(new RecyclerViewItemCountAssertion(1));
+  fun testSaveButtonForSingleContact() {
+    activityTestRule?.launchActivity(
+        PreviewImportPgpContactActivity.newIntent(getTargetContext(), singlePublicKeyForUnsavedContact))
+    onView(withId(R.id.recyclerViewContacts))
+        .check(RecyclerViewItemCountAssertion(1))
+    onView(withId(R.id.recyclerViewContacts))
+        .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
+            ClickOnViewInRecyclerViewItem(R.id.buttonSaveContact)))
+    checkIsToastDisplayed(activityTestRule?.activity, getResString(R.string.contact_successfully_saved))
   }
 
   @Test
-  public void testSaveButtonForSingleContact() {
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), getSinglePublicKeyForUnsavedContact()));
-    onView(withId(R.id.recyclerViewContacts)).check(new RecyclerViewItemCountAssertion(1));
-    onView(withId(R.id.recyclerViewContacts)).perform(RecyclerViewActions.actionOnItemAtPosition(0,
-        new ClickOnViewInRecyclerViewItem(R.id.buttonSaveContact)));
-    checkIsToastDisplayed(activityTestRule.getActivity(), getResString(R.string.contact_successfully_saved));
+  fun testIsImportAllButtonDisplayed() {
+    activityTestRule?.launchActivity(PreviewImportPgpContactActivity.newIntent(getTargetContext(), tenPubKeys))
+    onView(withId(R.id.buttonImportAll))
+        .check(matches(isDisplayed()))
   }
 
   @Test
-  public void testIsImportAllButtonDisplayed() {
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), get10PublicKeysForUnsavedContacts()));
-    onView(withId(R.id.buttonImportAll)).check(matches(isDisplayed()));
-  }
+  fun testLoadLotOfContacts() {
+    val countOfKeys = 10
 
-  @Test
-  public void testLoadLotOfContacts() {
-    int countOfKeys = 10;
-
-    activityTestRule.launchActivity(PreviewImportPgpContactActivity.newIntent(
-        InstrumentationRegistry.getInstrumentation().getTargetContext(), get10PublicKeysForUnsavedContacts()));
-    onView(withId(R.id.recyclerViewContacts)).check(new RecyclerViewItemCountAssertion(countOfKeys))
-        .perform(RecyclerViewActions.scrollToPosition(countOfKeys - 1));
-  }
-
-  private String getSinglePublicKeyForUnsavedContact() {
-    return PrivateKeysManager.getNodeKeyDetailsFromAssets("node/default@denbond7.com_fisrtKey_pub.json")
-        .getPublicKey();
-  }
-
-  private String get10PublicKeysForUnsavedContacts() {
-    return TestGeneralUtil.readFileFromAssetsAsString(InstrumentationRegistry.getInstrumentation().getContext(),
-        "pgp/pub_keys_2048_bits_10.asc");
+    activityTestRule?.launchActivity(PreviewImportPgpContactActivity.newIntent(getTargetContext(), tenPubKeys))
+    onView(withId(R.id.recyclerViewContacts))
+        .check(RecyclerViewItemCountAssertion(countOfKeys))
+        .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(countOfKeys - 1))
   }
 }
