abc82c69cfe5dfb8bc6c04ac9ef3c86ff1f632cf
DenBond7 <DenBond7@gmail.com>
2019-05-16 10:00:49 +0300

    Renamed some classes which implement Parcelable to use the Kotlin extension. Removed Parcelable implementation from some classes. Refactored code. | #553

---
+++ FlowCrypt/src/main/java/com/flowcrypt/email/api/retrofit/response/model/LookUpPublicKeyInfo.kt
@@ -0,0 +1,86 @@
+/*
+ * © 2016-2019 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.api.retrofit.response.model
+
+import android.os.Parcel
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/**
+ * This POJO class describes information about a public key from the
+ * API <code> https://flowcrypt.com/attester/lookup/</code>
+ *
+ * @author Denis Bondarenko
+ * Date: 05.05.2018
+ * Time: 14:04
+ * E-mail: DenBond7@gmail.com
+ */
+class LookUpPublicKeyInfo implements Parcelable {
+  public static final Creator < LookUpPublicKeyInfo > CREATOR = new Creator<LookUpPublicKeyInfo> {
+    @Override
+    public LookUpPublicKeyInfo createFromParcel(Parcel source) {
+      return new LookUpPublicKeyInfo (source)
+    }
+
+    @Override
+    public LookUpPublicKeyInfo[] newArray(int size) {
+      return new LookUpPublicKeyInfo [size]
+    }
+  }
+
+  @SerializedName("longid")
+  @Expose
+  private String longId
+
+  @SerializedName("pubkey")
+  @Expose
+  private String pubKey
+
+  @Expose
+  private String query
+
+  @Expose
+  private ArrayList < String > attests
+
+  public LookUpPublicKeyInfo() {
+  }
+
+  protected LookUpPublicKeyInfo(Parcel in) {
+    this.longId = in.readString()
+    this.pubKey = in.readString()
+    this.query = in.readString()
+    this.attests = in.createStringArrayList()
+  }
+
+  @Override
+  public int describeContents {
+    return 0
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(this.longId)
+    dest.writeString(this.pubKey)
+    dest.writeString(this.query)
+    dest.writeStringList(this.attests)
+  }
+
+  public String getLongId {
+    return longId
+  }
+
+  public String getPubKey {
+    return pubKey
+  }
+
+  public String getQuery {
+    return query
+  }
+
+  public ArrayList<String> getAttests() {
+    return attests
+  }
+}
