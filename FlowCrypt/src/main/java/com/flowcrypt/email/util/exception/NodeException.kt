97838af42ae9470c231e48397c2d17d13096dc03
DenBond7 <DenBond7@gmail.com>
2019-06-20 16:53:54 +0300

    Fixed NodeException message after migration to Kotlin.| Close #566

--- FlowCrypt/src/main/java/com/flowcrypt/email/util/exception/NodeException.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/util/exception/NodeException.kt
@@ -16,13 +16,11 @@ import com.flowcrypt.email.api.retrofit.response.model.node.Error
  * E-mail: DenBond7@gmail.com
  */
 open class NodeException(val nodeError: Error?) : Exception() {
-  override val message: String = {
-    val builder = StringBuilder()
-    if (nodeError != null) {
-      builder.append("Error :").append(nodeError.msg).append("\n")
-      builder.append("Stack :").append(nodeError.stack).append("\n")
-      builder.append("Type :").append(nodeError.type)
-    }
-    builder
-  }.toString()
+
+  override val message: String = if (nodeError != null) {
+    StringBuilder()
+        .append("Error :").append(nodeError.msg).append("\n")
+        .append("Stack :").append(nodeError.stack).append("\n")
+        .append("Type :").append(nodeError.type).toString()
+  } else ""
 }
