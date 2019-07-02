9f3f8b464b5d84e5d4b13419e1ab8ed31fbfec9e
Tom J <tom@holub.me>
2019-07-02 12:06:29 +0000

    fix pre email formatting #619

--- FlowCrypt/src/main/assets/js/flowcrypt-android.js
+++ FlowCrypt/src/main/assets/js/flowcrypt-android.js
@@ -69028,8 +69028,9 @@ exports.fmtContentBlock = contentBlocks => {
     <head>
       <meta name="viewport" content="width=device-width" />
       <style>
-        img { display: inline !important; height: auto !important; max-width: 100% !important; }
         body { word-wrap: break-word; word-break: break-word; hyphens: auto; margin-left: 0px; padding-left: 0px; }
+        body img { display: inline !important; height: auto !important; max-width: 100% !important; }
+        body pre { white-space: pre-wrap !important; }
       </style>
     </head>
     <body>${msgContentAsHtml}</body>
