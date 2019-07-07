f00128517829b3750c94abfa3406479fd42c76b0
Tom J <tom@holub.me>
2019-07-07 14:01:25 +0000

    try without while

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -14,7 +14,7 @@ else
     wget https://dl.google.com/android/repository/$SDK_ARCHIVE -P ~/Android/ && unzip -qq ~/Android/$SDK_ARCHIVE -d $ANDROID_SDK_ROOT && rm ~/Android/$SDK_ARCHIVE
     export PATH="$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools:$PATH"
     echo "yes" | sdkmanager --licenses > /dev/null
-    (while sleep 3; do echo "y"; done) | sdkmanager "build-tools;26.0.1" "platforms;android-24" "extras;google;m2repository" "extras;android;m2repository" "platform-tools" "emulator" "system-images;android-24;google_apis;armeabi-v7a"
+    ( sleep 5; echo "y" ) | sdkmanager "build-tools;26.0.1" "platforms;android-24" "extras;google;m2repository" "extras;android;m2repository" "platform-tools" "emulator" "system-images;android-24;google_apis;armeabi-v7a"
     echo -ne '\n' | avdmanager -v create avd -n semaphore-android-dev -k "system-images;android-24;google_apis;armeabi-v7a" --tag "google_apis" --abi "armeabi-v7a"
 fi
 
