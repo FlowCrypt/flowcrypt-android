5022149d458bb86e6413a44515d78eff84fd2942
Tom J <tom@holub.me>
2019-07-07 15:06:53 +0000

    refactor ci install #635

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -11,8 +11,15 @@ else
     mkdir ~/.android
     touch ~/.android/repositories.cfg
     mkdir -p $ANDROID_SDK_ROOT
-    wget https://dl.google.com/android/repository/$SDK_ARCHIVE -P ~/Android/ && unzip -qq ~/Android/$SDK_ARCHIVE -d $ANDROID_SDK_ROOT && rm ~/Android/$SDK_ARCHIVE
+
+    # download, unpack and remove sdk archive
+    wget https://dl.google.com/android/repository/$SDK_ARCHIVE
+    unzip -qq $SDK_ARCHIVE -d $ANDROID_SDK_ROOT
+    rm $SDK_ARCHIVE
+
     export PATH="$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/tools/bin:$ANDROID_SDK_ROOT/platform-tools:$PATH"
+
+    # install sdkmanager deps
     echo "yes" | sdkmanager --licenses > /dev/null
     ( sleep 5; echo "y" ) | sdkmanager "build-tools;26.0.1" "platforms;android-24" "extras;google;m2repository" "extras;android;m2repository" "platform-tools" "emulator" "system-images;android-24;google_apis;armeabi-v7a"
     echo -ne '\n' | avdmanager -v create avd -n semaphore-android-dev -k "system-images;android-24;google_apis;armeabi-v7a" --tag "google_apis" --abi "armeabi-v7a"
