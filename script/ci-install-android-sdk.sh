db3a7b982e5e6be4f1f37f306b97458b950ec36c
Tom J <tom@holub.me>
2019-07-12 14:13:04 +0000

    install emulator and other stuff

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -26,8 +26,8 @@ else
     ( sleep 5; echo "y" ) | sdkmanager "build-tools;29.0.0" "platforms;android-28"
 
     # runtime / unused deps
-    # ( sleep 5; echo "y" ) | sdkmanager "extras;google;m2repository" "extras;android;m2repository" "platform-tools" "emulator" "system-images;android-24;google_apis;x86_64"
-    # echo -ne '\n' | avdmanager -v create avd --name ci-test-nexus4-x86-64-api24 --package "system-images;android-24;google_apis;x86_64" --device 'Nexus 4' --abi 'google_apis/x86_64'
+    ( sleep 5; echo "y" ) | sdkmanager "extras;google;m2repository" "extras;android;m2repository" "platform-tools" "emulator" "system-images;android-24;google_apis;x86_64"
+    echo -ne '\n' | avdmanager -v create avd --name ci-test-nexus4-x86-64-api24 --package "system-images;android-24;google_apis;x86_64" --device 'Nexus 4' --abi 'google_apis/x86_64'
 fi
 
 sdkmanager --list
