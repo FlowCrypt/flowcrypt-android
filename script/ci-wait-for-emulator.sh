428079a16e25be9fd8cc15b747855e03a68391ae
Tom J <tom@holub.me>
2019-07-12 17:05:28 +0000

    count all seconds as err

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -11,13 +11,13 @@ timeout_in_sec=90
 until [[ "$bootanim" =~ "stopped" ]]; do
   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
   if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" ]]; then
-    let "failcounter += 1"
     echo "Waiting for emulator to start"
     if [[ $failcounter -gt timeout_in_sec ]]; then
       echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
       exit 1
     fi
   fi
+  let "failcounter += 1"
   sleep 1
 done
 
