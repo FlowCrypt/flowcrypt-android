9c2b445088e6a123d147dfcc545232b094deddc0
DenBond7 <DenBond7@gmail.com>
2019-07-27 11:48:11 +0300

    Added ci-disable-emulator-animations.sh. Modified semaphore.yml.| #667

---
+++ script/ci-disable-emulator-animations.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+~/Android/Sdk/platform-tools/adb shell settings put global window_animation_scale 0.0
+~/Android/Sdk/platform-tools/adb shell settings put global transition_animation_scale 0.0
+~/Android/Sdk/platform-tools/adb shell settings put global animator_duration_scale 0.0
