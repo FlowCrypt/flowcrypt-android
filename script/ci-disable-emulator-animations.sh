7483336ebc363cafda7773a71b5b758b6e251c93
DenBond7 <DenBond7@gmail.com>
2019-07-27 14:38:01 +0300

    Added debug code.| #667

--- script/ci-disable-emulator-animations.sh
+++ script/ci-disable-emulator-animations.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+~/Android/Sdk/platform-tools/adb shell service list
 ~/Android/Sdk/platform-tools/adb shell settings put global window_animation_scale 0.0
 ~/Android/Sdk/platform-tools/adb shell settings put global transition_animation_scale 0.0
 ~/Android/Sdk/platform-tools/adb shell settings put global animator_duration_scale 0.0
