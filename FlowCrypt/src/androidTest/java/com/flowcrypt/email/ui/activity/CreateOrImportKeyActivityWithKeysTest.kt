3bbe66d0d1d4cfe2e3f9d0f9f222faaeb889f6e2
DenBond7 <DenBond7@gmail.com>
2019-11-02 10:27:22 +0200

    Added a test for "NO_PRV_CREATE" option. Refactored code.| #755

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/CreateOrImportKeyActivityWithKeysTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/CreateOrImportKeyActivityWithKeysTest.kt
@@ -12,24 +12,23 @@ import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.ActivityResultMatchers.hasResultCode
+import androidx.test.espresso.contrib.ActivityResultMatchers
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.flowcrypt.email.R
-import com.flowcrypt.email.base.BaseTest
 import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
 import com.flowcrypt.email.rules.ClearAppSettingsRule
-import com.flowcrypt.email.ui.activity.base.BaseImportKeyActivity
+import com.flowcrypt.email.ui.activity.base.BaseCreateOrImportKeyActivityTest
 import com.flowcrypt.email.ui.activity.base.BasePassPhraseManagerActivity
 import com.flowcrypt.email.util.AccountDaoManager
-import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.allOf
+import org.junit.Assert.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -44,7 +43,7 @@ import org.junit.runner.RunWith
  */
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class CreateOrImportKeyActivityWithKeysTest : BaseTest() {
+class CreateOrImportKeyActivityWithKeysTest : BaseCreateOrImportKeyActivityTest() {
 
   override val activityTestRule =
       object : IntentsTestRule<CreateOrImportKeyActivity>(CreateOrImportKeyActivity::class.java) {
@@ -65,39 +64,9 @@ class CreateOrImportKeyActivityWithKeysTest : BaseTest() {
         hasExtraWithKey(BasePassPhraseManagerActivity.KEY_EXTRA_ACCOUNT_DAO)))
         .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
     onView(withId(R.id.buttonCreateNewKey))
-        .check(matches(isDisplayed()))
+        .check(matches(ViewMatchers.isDisplayed()))
         .perform(click())
-    assertThat(activityTestRule.activityResult, hasResultCode(Activity.RESULT_OK))
-  }
-
-  @Test
-  fun testClickOnButtonImportMyKey() {
-    intending(allOf(hasComponent(ComponentName(getTargetContext(), ImportPrivateKeyActivity::class.java)),
-        hasExtraWithKey(BaseImportKeyActivity.KEY_EXTRA_IS_SYNC_ENABLE),
-        hasExtraWithKey(BaseImportKeyActivity.KEY_EXTRA_TITLE),
-        hasExtraWithKey(BaseImportKeyActivity.KEY_EXTRA_PRIVATE_KEY_IMPORT_MODEL_FROM_CLIPBOARD),
-        hasExtraWithKey(BaseImportKeyActivity.KEY_EXTRA_IS_THROW_ERROR_IF_DUPLICATE_FOUND)))
-        .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
-    onView(withId(R.id.buttonImportMyKey))
-        .check(matches(isDisplayed()))
-        .perform(click())
-    assertThat(activityTestRule.activityResult, hasResultCode(Activity.RESULT_OK))
-  }
-
-  @Test
-  fun testClickOnButtonSelectAnotherAccount() {
-    onView(withId(R.id.buttonSelectAnotherAccount))
-        .check(matches(isDisplayed()))
-        .perform(click())
-    assertThat(activityTestRule.activityResult,
-        hasResultCode(CreateOrImportKeyActivity.RESULT_CODE_USE_ANOTHER_ACCOUNT))
-  }
-
-  @Test
-  fun testClickOnButtonSkipSetup() {
-    onView(withId(R.id.buttonSkipSetup))
-        .check(matches(isDisplayed()))
-        .perform(click())
-    assertThat(activityTestRule.activityResult, hasResultCode(Activity.RESULT_OK))
+    assertThat(activityTestRule.activityResult, ActivityResultMatchers
+        .hasResultCode(Activity.RESULT_OK))
   }
 }
---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/enterprise/CreateOrImportKeyActivityEnterpriseTest.kt
@@ -0,0 +1,59 @@
+/*
+ * © 2016-2019 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.activity.enterprise
+
+import android.content.Intent
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.flowcrypt.email.DoesNotNeedMailserverEnterprise
+import com.flowcrypt.email.R
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.ui.activity.CreateOrImportKeyActivity
+import com.flowcrypt.email.ui.activity.base.BaseCreateOrImportKeyActivityTest
+import com.flowcrypt.email.util.AccountDaoManager
+import org.hamcrest.CoreMatchers.not
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 11/2/19
+ *         Time: 10:07 AM
+ *         E-mail: DenBond7@gmail.com
+ */
+@LargeTest
+@DoesNotNeedMailserverEnterprise
+@RunWith(AndroidJUnit4::class)
+class CreateOrImportKeyActivityEnterpriseTest : BaseCreateOrImportKeyActivityTest() {
+  override val activityTestRule =
+      object : IntentsTestRule<CreateOrImportKeyActivity>(CreateOrImportKeyActivity::class.java) {
+        override fun getActivityIntent(): Intent {
+          return CreateOrImportKeyActivity.newIntent(getTargetContext(), AccountDaoManager
+              .getAccountDao("enterprise_account_no_prv_create.json"), true)
+        }
+      }
+
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+      .outerRule(ClearAppSettingsRule())
+      .around(AddPrivateKeyToDatabaseRule())
+      .around(activityTestRule)
+
+  @Test
+  fun testCreateNewKeyNotExist() {
+    onView(withId(R.id.buttonCreateNewKey))
+        .check(matches(not(isDisplayed())))
+  }
+}
