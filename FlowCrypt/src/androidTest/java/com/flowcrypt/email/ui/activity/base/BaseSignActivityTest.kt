c5f205f7c30c4aa60ffb176a53c1f35770f784ab
DenBond7 <DenBond7@gmail.com>
2019-11-08 16:06:13 +0200

    Added tests for the `Add email account` screen. Modified SignInActivityEnterpriseTest. Refactored code.| Close #761

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BaseSignActivityTest.kt
@@ -0,0 +1,67 @@
+/*
+ * © 2016-2019 FlowCrypt Limited. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.activity.base
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.util.google.GoogleApiClientHelper
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 11/8/19
+ *         Time: 3:39 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+abstract class BaseSignActivityTest : BaseTest() {
+
+  protected fun setupAndClickSignInButton(signInAccountJson: String) {
+    val intent = Intent()
+    intent.putExtra("googleSignInAccount", GoogleSignInAccount.zaa(signInAccountJson))
+
+    val signInIntent = GoogleSignIn.getClient(activityTestRule?.activity!!,
+        GoogleApiClientHelper.generateGoogleSignInOptions()).signInIntent
+
+    Intents.intending(IntentMatchers.hasComponent(signInIntent.component))
+        .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, intent))
+
+    Espresso.onView(ViewMatchers.withId(R.id.buttonSignInWithGmail))
+        .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        .perform(ViewActions.click())
+  }
+
+  protected fun genMockGoogleSignInAccountJson(email: String): String {
+    return "{\n" +
+        "   \"id\":\"111111111111111111111\",\n" +
+        "   \"tokenId\":\"some_token\",\n" +
+        "   \"email\":\"" + email + "\",\n" +
+        "   \"displayName\":\"Test\",\n" +
+        "   \"givenName\":\"Test\",\n" +
+        "   \"familyName\":\"Test\",\n" +
+        "   \"photoUrl\":\"https:\\/\\/example.com\",\n" +
+        "   \"expirationTime\":1572537339,\n" +
+        "   \"obfuscatedIdentifier\":\"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\",\n" +
+        "   \"grantedScopes\":[\n" +
+        "      \"email\",\n" +
+        "      \"https:\\/\\/mail.google.com\\/\",\n" +
+        "      \"https:\\/\\/www.googleapis.com\\/auth\\/userinfo.email\",\n" +
+        "      \"https:\\/\\/www.googleapis.com\\/auth\\/userinfo.profile\",\n" +
+        "      \"openid\",\n" +
+        "      \"profile\"\n" +
+        "   ]\n" +
+        "}"
+  }
+}
--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/enterprise/AddNewAccountActivityEnterpriseTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/enterprise/AddNewAccountActivityEnterpriseTest.kt
@@ -22,12 +22,13 @@ import com.flowcrypt.email.api.retrofit.ApiHelper
 import com.flowcrypt.email.api.retrofit.request.model.LoginModel
 import com.flowcrypt.email.api.retrofit.response.api.DomainRulesResponse
 import com.flowcrypt.email.api.retrofit.response.api.LoginResponse
-import com.flowcrypt.email.api.retrofit.response.base.ApiError
 import com.flowcrypt.email.api.retrofit.response.model.DomainRules
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
 import com.flowcrypt.email.rules.ClearAppSettingsRule
 import com.flowcrypt.email.rules.FlowCryptMockWebServerRule
+import com.flowcrypt.email.rules.StubAllExternalIntentsRule
+import com.flowcrypt.email.ui.activity.AddNewAccountActivity
 import com.flowcrypt.email.ui.activity.CreateOrImportKeyActivity
-import com.flowcrypt.email.ui.activity.SignInActivity
 import com.flowcrypt.email.ui.activity.base.BaseSignActivityTest
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
@@ -43,44 +44,22 @@ import java.io.InputStreamReader
 
 /**
  * @author Denis Bondarenko
- *         Date: 10/31/19
- *         Time: 3:08 PM
+ *         Date: 11/8/19
+ *         Time: 3:30 PM
  *         E-mail: DenBond7@gmail.com
  */
 @DoesNotNeedMailserverEnterprise
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class SignInActivityEnterpriseTest : BaseSignActivityTest() {
-  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(SignInActivity::class.java)
+class AddNewAccountActivityEnterpriseTest : BaseSignActivityTest() {
+  override val activityTestRule: ActivityTestRule<*>? = IntentsTestRule(AddNewAccountActivity::class.java)
 
   @get:Rule
-  var ruleChain: TestRule = RuleChain
+  val ruleChain: TestRule = RuleChain
       .outerRule(ClearAppSettingsRule())
+      .around(AddAccountToDatabaseRule())
       .around(activityTestRule)
-
-  @Test
-  fun testErrorLogin() {
-    setupAndClickSignInButton(genMockGoogleSignInAccountJson(EMAIL_LOGIN_ERROR))
-    isToastDisplayed(activityTestRule?.activity, LOGIN_API_ERROR_RESPONSE.apiError?.msg!!)
-  }
-
-  @Test
-  fun testSuccessLoginNotVerified() {
-    setupAndClickSignInButton(genMockGoogleSignInAccountJson(EMAIL_LOGIN_NOT_VERIFIED))
-    isToastDisplayed(activityTestRule?.activity, getResString(R.string.user_not_verified))
-  }
-
-  @Test
-  fun testSuccessLoginNotRegisteredNotVerified() {
-    setupAndClickSignInButton(genMockGoogleSignInAccountJson(EMAIL_LOGIN_NOT_REGISTERED_NOT_VERIFIED))
-    isToastDisplayed(activityTestRule?.activity, getResString(R.string.user_not_registered_not_verified))
-  }
-
-  @Test
-  fun testErrorGetDomainRules() {
-    setupAndClickSignInButton(genMockGoogleSignInAccountJson(EMAIL_DOMAIN_RULES_ERROR))
-    isToastDisplayed(activityTestRule?.activity, DOMAIN_RULES_ERROR_RESPONSE.apiError?.msg!!)
-  }
+      .around(StubAllExternalIntentsRule())
 
   @Test
   fun testNoPrvCreateRule() {
@@ -95,16 +74,6 @@ class SignInActivityEnterpriseTest : BaseSignActivityTest() {
 
   companion object {
     const val EMAIL_WITH_NO_PRV_CREATE_RULE = "no_prv_create@example.com"
-    const val EMAIL_LOGIN_ERROR = "login_error@example.com"
-    const val EMAIL_LOGIN_NOT_VERIFIED = "login_not_verified@example.com"
-    const val EMAIL_LOGIN_NOT_REGISTERED_NOT_VERIFIED = "login_not_registered_not_verified@example.com"
-    const val EMAIL_DOMAIN_RULES_ERROR = "domain_rules_error@example.com"
-
-    val LOGIN_API_ERROR_RESPONSE = LoginResponse(ApiError(400, "Something wrong happened.",
-        "api input: missing key: token"), null, null)
-
-    val DOMAIN_RULES_ERROR_RESPONSE = DomainRulesResponse(ApiError(401,
-        "Not logged in or unknown account", "auth"), null)
 
     @get:ClassRule
     @JvmStatic
@@ -116,18 +85,6 @@ class SignInActivityEnterpriseTest : BaseSignActivityTest() {
 
         if (request?.path.equals("/account/login")) {
           when (model.account) {
-            EMAIL_LOGIN_ERROR -> return MockResponse().setResponseCode(200)
-                .setBody(gson.toJson(LOGIN_API_ERROR_RESPONSE))
-
-            EMAIL_LOGIN_NOT_REGISTERED_NOT_VERIFIED -> return MockResponse().setResponseCode(200)
-                .setBody(gson.toJson(LoginResponse(null, isRegistered = false, isVerified = false)))
-
-            EMAIL_LOGIN_NOT_VERIFIED -> return MockResponse().setResponseCode(200)
-                .setBody(gson.toJson(LoginResponse(null, isRegistered = true, isVerified = false)))
-
-            EMAIL_DOMAIN_RULES_ERROR -> return MockResponse().setResponseCode(200)
-                .setBody(gson.toJson(LoginResponse(null, isRegistered = true, isVerified = true)))
-
             EMAIL_WITH_NO_PRV_CREATE_RULE -> return MockResponse().setResponseCode(200)
                 .setBody(gson.toJson(LoginResponse(null, isRegistered = true, isVerified = true)))
           }
@@ -135,9 +92,6 @@ class SignInActivityEnterpriseTest : BaseSignActivityTest() {
 
         if (request?.path.equals("/account/get")) {
           when (model.account) {
-            EMAIL_DOMAIN_RULES_ERROR -> return MockResponse().setResponseCode(200)
-                .setBody(gson.toJson(DOMAIN_RULES_ERROR_RESPONSE))
-
             EMAIL_WITH_NO_PRV_CREATE_RULE -> return MockResponse().setResponseCode(200)
                 .setBody(gson.toJson(DomainRulesResponse(null, DomainRules(listOf
                 ("NO_PRV_CREATE", "NO_PRV_BACKUP")))))
