b9e69a5560022fccd41fd5726746387db26dacca
DenBond7 <DenBond7@gmail.com>
2020-02-25 15:38:25 +0200

    Temporary disabled tests on CI

--- script/ci-tests-without-mailserver.sh
+++ script/ci-tests-without-mailserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./gradlew :FlowCrypt:testDevTestDebugUnitTest :FlowCrypt:connectedDevTestDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.annotation=com.flowcrypt.email.DoesNotNeedMailserver
+./gradlew :FlowCrypt:testDevTestDebugUnitTest
 
