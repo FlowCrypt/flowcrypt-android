34802770142833cef2ac1003aa2d9e0076ae4a57
DenBond7 <DenBond7@gmail.com>
2020-05-07 15:39:41 +0300

    Fixed updating ActionMode.| #875

--- FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/EmailListFragment.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/EmailListFragment.kt
@@ -115,6 +115,7 @@ class EmailListFragment : BaseSyncFragment(), SwipeRefreshLayout.OnRefreshListen
     }
 
     adapter.submitList(it)
+    actionMode?.invalidate()
   }
 
   private val boundaryCallback = object : PagedList.BoundaryCallback<MessageEntity>() {
