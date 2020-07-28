ae46a2de593a6205c827397584139a13bb830b89
DenBond7 <DenBond7@gmail.com>
2020-07-28 09:06:28 +0300

    Added Yahoo settings to EmailProviderSettingsHelper.| #918

--- FlowCrypt/src/main/java/com/flowcrypt/email/api/email/EmailProviderSettingsHelper.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/api/email/EmailProviderSettingsHelper.kt
@@ -23,6 +23,11 @@ object EmailProviderSettingsHelper {
   private const val IMAP_SERVER_OUTLOOK = "outlook.office365.com"
   private const val SMTP_SERVER_OUTLOOK = "smtp.office365.com"
 
+  /*********************** Yahoo **********************/
+  private const val PROVIDER_YAHOO = "yahoo.com"
+  private const val IMAP_SERVER_YAHOO = "imap.mail.yahoo.com"
+  private const val SMTP_SERVER_YAHOO = "smtp.mail.yahoo.com"
+
   /*********************** Account for internal testing **********************/
   private const val PROVIDER_TESTS = "denbond7.com"
   private const val IMAP_SERVER_TESTS = "imap.denbond7.com"
@@ -38,6 +43,7 @@ object EmailProviderSettingsHelper {
 
     return when {
       PROVIDER_OUTLOOK.equals(EmailUtil.getDomain(email), true) -> getOutlookSettings(email, password)
+      PROVIDER_YAHOO.equals(EmailUtil.getDomain(email), true) -> getYahooSettings(email, password)
       PROVIDER_TESTS.equals(EmailUtil.getDomain(email), true) -> getTestProviderSettings(email, password)
 
       else -> {
@@ -57,6 +63,23 @@ object EmailProviderSettingsHelper {
         smtpServer = SMTP_SERVER_OUTLOOK,
         smtpPort = JavaEmailConstants.STARTTLS_SMTP_PORT,
         smtpOpt = SecurityType.Option.STARTLS,
+        hasCustomSignInForSmtp = true,
+        smtpSigInUsername = email,
+        smtpSignInPassword = password
+    )
+  }
+
+  private fun getYahooSettings(email: String, password: String): AuthCredentials {
+    return AuthCredentials(
+        email = email,
+        username = email,
+        password = password,
+        imapServer = IMAP_SERVER_YAHOO,
+        imapPort = JavaEmailConstants.SSL_IMAP_PORT,
+        imapOpt = SecurityType.Option.SSL_TLS,
+        smtpServer = SMTP_SERVER_YAHOO,
+        smtpPort = JavaEmailConstants.SSL_SMTP_PORT,
+        smtpOpt = SecurityType.Option.SSL_TLS,
         hasCustomSignInForSmtp = true,
         smtpSigInUsername = email,
         smtpSignInPassword = password
