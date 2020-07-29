0434394f007d7be38b51d28f4e8e670e778e2eee
DenBond7 <DenBond7@gmail.com>
2020-07-29 13:25:59 +0300

    Added skipping too long pub keys during the message rendering.| Close #911

--- FlowCrypt/src/main/java/com/flowcrypt/email/api/retrofit/node/gson/MsgBlockDeserializer.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/api/retrofit/node/gson/MsgBlockDeserializer.kt
@@ -10,6 +10,7 @@ import com.flowcrypt.email.api.retrofit.response.model.node.DecryptErrorMsgBlock
 import com.flowcrypt.email.api.retrofit.response.model.node.DecryptedAttMsgBlock
 import com.flowcrypt.email.api.retrofit.response.model.node.MsgBlock
 import com.flowcrypt.email.api.retrofit.response.model.node.PublicKeyMsgBlock
+import com.flowcrypt.email.jetpack.viewmodel.PrivateKeysViewModel
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
@@ -24,10 +25,10 @@ import java.lang.reflect.Type
  * E-mail: DenBond7@gmail.com
  */
 class MsgBlockDeserializer : JsonDeserializer<MsgBlock> {
-  override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): MsgBlock {
+  override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): MsgBlock? {
     val jsonObject = json.asJsonObject
 
-    return when (context.deserialize<MsgBlock.Type>(jsonObject.get(BaseMsgBlock.TAG_TYPE), MsgBlock.Type::class.java)) {
+    val msgBlock: MsgBlock? = when (context.deserialize<MsgBlock.Type>(jsonObject.get(BaseMsgBlock.TAG_TYPE), MsgBlock.Type::class.java)) {
       MsgBlock.Type.PUBLIC_KEY -> context.deserialize(json, PublicKeyMsgBlock::class.java)
 
       MsgBlock.Type.DECRYPT_ERROR -> context.deserialize(json, DecryptErrorMsgBlock::class.java)
@@ -36,5 +37,14 @@ class MsgBlockDeserializer : JsonDeserializer<MsgBlock> {
 
       else -> context.deserialize(json, BaseMsgBlock::class.java)
     }
+
+    if (msgBlock?.type == MsgBlock.Type.PUBLIC_KEY) {
+      // check https://github.com/FlowCrypt/flowcrypt-android/issues/911 to see details
+      if (msgBlock.content?.length ?: 0 > PrivateKeysViewModel.MAX_SIZE_IN_BYTES) {
+        return null
+      }
+    }
+
+    return msgBlock
   }
 }
