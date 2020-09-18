950470f929a3c1f0988ca80b8f6738f52610ecac
DenBond7 <DenBond7@gmail.com>
2020-09-18 10:43:09 +0300

    Updated semaphore.yml. Moved some commands to ci-install-android-sdk. Recreated cache.| #934

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
 
-set -euxo pipefail
-
-mkdir ~/.android
-touch ~/.android/repositories.cfg
-
-SDK_ARCHIVE=sdk-tools-linux-4333796.zip
-
-sudo apt-get -yq install adb qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+sudo apt-get update -qq
+# We use "sudo apt-get -qq install app > /dev/null" to disable printing logs. To see them again use "-yq install"
+sudo apt-get -qq install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils > /dev/null
 sudo kvm-ok
-
-if [ -d ~/Android ]; then
-    echo "~/Android already exists, skipping installation"
-else
-    echo "~/Android does not exist, installing"
-    mkdir -p $ANDROID_SDK_ROOT
-
-    # download, unpack and remove sdk archive
-    wget https://dl.google.com/android/repository/$SDK_ARCHIVE
-    unzip -qq $SDK_ARCHIVE -d $ANDROID_SDK_ROOT
-    rm $SDK_ARCHIVE
-
-    echo "yes" | ~/Android/Sdk/tools/bin/sdkmanager --licenses > /dev/null
-    ( sleep 5; echo "y" ) | ~/Android/Sdk/tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29"
-    ~/Android/Sdk/tools/bin/sdkmanager "extras;google;m2repository"
-    ~/Android/Sdk/tools/bin/sdkmanager "platform-tools"
-    ~/Android/Sdk/tools/bin/sdkmanager "emulator"
-    ~/Android/Sdk/tools/bin/sdkmanager "system-images;android-29;google_apis;x86_64"
-fi
-
-~/Android/Sdk/tools/bin/sdkmanager --list
+# We use "| grep -v = || true" to disable printing logs
+sdkmanager "platform-tools" "platforms;android-29" "emulator" "extras;google;m2repository" "build-tools;29.0.2" "ndk;21.2.6472646" "cmake;3.10.2.4988404" | grep -v = || true
+sdkmanager "system-images;android-29;google_apis;x86_64" | grep -v = || true
