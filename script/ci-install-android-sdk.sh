e4fd770f93ca904308029976b3fd423202d97a1f
DenBond7 <DenBond7@gmail.com>
2020-09-21 12:29:14 +0300

    Modified ci-install-android-sdk.sh to hide licences text. 3-nd attempt.| #934

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -22,13 +22,13 @@ else
     rm $SDK_ARCHIVE
 
     (echo "yes" | ~/Android/Sdk/tools/bin/sdkmanager --licenses > /dev/null | grep -v = || true)
-    ( sleep 5; echo "y" ) | ~/Android/Sdk/tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "extras;google;m2repository" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "platform-tools" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "emulator" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "ndk;21.2.6472646" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "cmake;3.10.2.4988404" | grep -v = || true
-    ~/Android/Sdk/tools/bin/sdkmanager "system-images;android-29;google_apis;x86_64" | grep -v = || true
+    ( sleep 5; echo "y" ) | (~/Android/Sdk/tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "extras;google;m2repository" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "platform-tools" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "emulator" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "ndk;21.2.6472646" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "cmake;3.10.2.4988404" | grep -v = || true)
+    (~/Android/Sdk/tools/bin/sdkmanager "system-images;android-29;google_apis;x86_64" | grep -v = || true)
 fi
 
 #Uncomment this for debug
