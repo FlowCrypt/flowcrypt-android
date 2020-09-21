8f0f5a226c85d1920a19bbbf82f489c049f18c08
DenBond7 <DenBond7@gmail.com>
2020-09-21 13:25:23 +0300

    Semaphore CI. Refactored code.| #934

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -10,7 +10,7 @@ SDK_ARCHIVE=sdk-tools-linux-4333796.zip
 sudo apt-get -qq install adb qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils > /dev/null
 sudo kvm-ok
 
-if [ -d ~/Android ]; then
+if [[ -d ~/Android ]]; then
     echo "~/Android already exists, skipping installation"
 else
     echo "~/Android does not exist, installing"
@@ -21,14 +21,14 @@ else
     unzip -qq $SDK_ARCHIVE -d $ANDROID_SDK_ROOT
     rm $SDK_ARCHIVE
 
-    (echo "yes" | ~/Android/Sdk/tools/bin/sdkmanager --licenses > /dev/null | grep -v = || true)
-    ( sleep 5; echo "y" ) | (~/Android/Sdk/tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29" > /dev/null | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "extras;google;m2repository" | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "platform-tools" | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "emulator" | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "ndk;21.2.6472646" | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "cmake;3.10.2.4988404" | grep -v = || true)
-    (~/Android/Sdk/tools/bin/sdkmanager "system-images;android-29;google_apis;x86_64" | grep -v = || true)
+    (echo "yes" | sdkmanager --licenses > /dev/null | grep -v = || true)
+    ( sleep 5; echo "y" ) | (sdkmanager "build-tools;29.0.2" "platforms;android-29" > /dev/null | grep -v = || true)
+    (sdkmanager "extras;google;m2repository" | grep -v = || true)
+    (sdkmanager "platform-tools" | grep -v = || true)
+    (sdkmanager "emulator" | grep -v = || true)
+    (sdkmanager "ndk;21.2.6472646" | grep -v = || true)
+    (sdkmanager "cmake;3.10.2.4988404" | grep -v = || true)
+    (sdkmanager "system-images;android-29;google_apis;x86_64" | grep -v = || true)
 fi
 
 #Uncomment this for debug
