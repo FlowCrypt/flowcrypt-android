dbf864017db8b794096069d0b0dde0736e30bb31
DenBond7 <DenBond7@gmail.com>
2020-09-23 17:50:59 +0300

    Fixed CreateOrImportKeyActivityWithKeysTest.| #934

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BaseCreateOrImportKeyActivityTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/BaseCreateOrImportKeyActivityTest.kt
@@ -10,21 +10,19 @@ import android.app.Instrumentation
 import android.content.ComponentName
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.ActivityResultMatchers
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
+import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
 import com.flowcrypt.email.base.BaseTest
 import com.flowcrypt.email.ui.activity.CreateOrImportKeyActivity
 import com.flowcrypt.email.ui.activity.ImportPrivateKeyActivity
-import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.allOf
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -34,7 +32,7 @@ import org.junit.runner.RunWith
  *         Time: 10:11 AM
  *         E-mail: DenBond7@gmail.com
  */
-@LargeTest
+@MediumTest
 @RunWith(AndroidJUnit4::class)
 abstract class BaseCreateOrImportKeyActivityTest : BaseTest() {
 
@@ -47,18 +45,16 @@ abstract class BaseCreateOrImportKeyActivityTest : BaseTest() {
         hasExtraWithKey(BaseImportKeyActivity.KEY_EXTRA_IS_THROW_ERROR_IF_DUPLICATE_FOUND)))
         .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))
     onView(withId(R.id.buttonImportMyKey))
-        .check(matches(isDisplayed()))
-        .perform(click())
-    assertThat(activityTestRule?.activityResult, ActivityResultMatchers
-        .hasResultCode(Activity.RESULT_OK))
+        .perform(scrollTo(), click())
+
+    Assert.assertTrue(activityScenarioRule?.scenario?.result?.resultCode == Activity.RESULT_OK)
   }
 
   @Test
   fun testClickOnButtonSelectAnotherAccount() {
     onView(withId(R.id.buttonSelectAnotherAccount))
-        .check(matches(isDisplayed()))
-        .perform(click())
-    assertThat(activityTestRule?.activityResult,
-        ActivityResultMatchers.hasResultCode(CreateOrImportKeyActivity.RESULT_CODE_USE_ANOTHER_ACCOUNT))
+        .perform(scrollTo(), click())
+
+    Assert.assertTrue(activityScenarioRule?.scenario?.result?.resultCode == CreateOrImportKeyActivity.RESULT_CODE_USE_ANOTHER_ACCOUNT)
   }
 }
