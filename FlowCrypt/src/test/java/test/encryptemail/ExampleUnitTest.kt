fce72ccc0cb54c4e706484f3d0fc7ad8bb2898f7
DenBond7 <DenBond7@gmail.com>
2020-09-25 17:24:32 +0300

    Restored ExampleUnitTest.| #934

--- FlowCrypt/src/test/java/test/encryptemail/ExampleUnitTest.kt
+++ FlowCrypt/src/test/java/test/encryptemail/ExampleUnitTest.kt
@@ -5,11 +5,8 @@
 
 package test.encryptemail
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.io.File
-import java.security.KeyFactory
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.spec.X509EncodedKeySpec
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,19 +16,6 @@ import java.security.spec.X509EncodedKeySpec
 class ExampleUnitTest {
   @Test
   fun additionIsCorrect() {
-    val keyBytes: ByteArray = File(this.javaClass.classLoader?.getResource("mock_web_server_private_key.der")?.path
-        ?: "").readBytes()
-    val spec = PKCS8EncodedKeySpec(keyBytes)
-    val kf = KeyFactory.getInstance("RSA")
-    val s = kf.generatePrivate(spec)
-    print("Hello")
-
-    val keyBytess: ByteArray = File(this.javaClass.classLoader?.getResource("mock_web_server_public_key.der")?.path
-        ?: "").readBytes()
-
-    val spec1 = X509EncodedKeySpec(keyBytess)
-    val m = kf.generatePublic(spec1)
-
-    print("Hello2")
+    assertEquals(4, (2 + 2).toLong())
   }
 }
