ff313875d26fdb37978b7e2eae978af0708a45aa
DenBond7 <DenBond7@gmail.com>
2020-10-08 19:22:56 +0300

    Added ci-setup-and-run-emulator.sh. Refactored code.| #966

---
+++ script/ci-setup-and-run-emulator.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$ANDROID_SDK_ROOT/emulator/emulator -accel-check
+echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-64-api29 --package "system-images;android-29;google_apis;x86_64" --device 'pixel' --abi 'google_apis/x86_64'
+cat ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
+$ANDROID_SDK_ROOT/emulator/emulator -list-avds #debug
+$ANDROID_SDK_ROOT/emulator/emulator -avd ci-test-pixel-x86-64-api29 -no-window -no-boot-anim -no-audio &
+
+
+
