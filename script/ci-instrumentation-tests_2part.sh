ed3504649813b4e6163e488ea4225a0f87cc1f0a
DenBond7 <DenBond7@gmail.com>
2020-10-08 15:01:16 +0300

    Enabled tests.| #966

--- script/ci-instrumentation-tests_2part.sh
+++ script/ci-instrumentation-tests_2part.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew :FlowCrypt:connectedDevTestDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.annotation=com.flowcrypt.email.DebugTestAnnotation -Pandroid.testInstrumentationRunnerArguments.numShards=2 -Pandroid.testInstrumentationRunnerArguments.shardIndex=1
+./gradlew :FlowCrypt:connectedDevTestDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.annotation=com.flowcrypt.email.ReadyForCIAnnotation -Pandroid.testInstrumentationRunnerArguments.numShards=2 -Pandroid.testInstrumentationRunnerArguments.shardIndex=1
