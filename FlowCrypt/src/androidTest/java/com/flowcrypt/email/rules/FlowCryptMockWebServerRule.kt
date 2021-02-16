d8e4dda5bd415824fee6066cb6c64bc4cf32496f
DenBond7 <DenBond7@gmail.com>
2021-02-16 16:03:50 +0200

    Modified FlowCryptMockWebServerRule to use a predefined SSL cert. Migrated to use HTTPS instead of HTTP for the local mock web server. Refactored code.

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/rules/FlowCryptMockWebServerRule.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/rules/FlowCryptMockWebServerRule.kt
@@ -13,12 +13,7 @@ import okhttp3.tls.HeldCertificate
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 import java.net.InetAddress
-import java.security.KeyFactory
-import java.security.KeyPair
-import java.security.spec.PKCS8EncodedKeySpec
-import java.security.spec.X509EncodedKeySpec
 import javax.net.ssl.SSLSocketFactory
-
 
 /**
  * This rule describes a logic of work of a mock web server
@@ -49,38 +44,56 @@ class FlowCryptMockWebServerRule(val port: Int, val responseDispatcher: Dispatch
   }
 
   /**
-   * Generate [SSLSocketFactory] to support for self-signed (local) SSL certificates.
-   * Based on  http://developer.android.com/training/articles/security-ssl.html#UnknownCa
+   * Get [SSLSocketFactory] to support self-signed (local) SSL certificates.
    *
    * https://github.com/square/okhttp/tree/master/okhttp-tls
    * https://github.com/square/okhttp/blob/master/samples/guide/src/main/java/okhttp3/recipes/HttpsServer.java
    */
   private fun getSSLSocketFactory(): SSLSocketFactory {
-    val rootCertificate = HeldCertificate.Builder()
-        .keyPair(prepareKeyPairFromResources("ssl/ca_private_key.der", "ssl/ca_public_key.der"))
+    val serverCertificate: HeldCertificate = HeldCertificate.decode(TestGeneralUtil
+        .readResourcesAsString("ssl/server_combined.pem"))
+
+    val serverHandshakeCertificates: HandshakeCertificates = HandshakeCertificates.Builder()
+        .heldCertificate(serverCertificate)
+        .build()
+
+    return serverHandshakeCertificates.sslSocketFactory()
+  }
+
+  /**
+   * Generate [SSLSocketFactory]  using [HeldCertificate] to support self-signed (local) SSL certificates.
+   * After generation combined result can be saved to file via the following command:
+   *
+   * "server_combined.pem" = serverCertificate.certificatePem() + serverCertificate.privateKeyPkcs8Pem()
+   *
+   * Later this string can be used to create a new instance of [HeldCertificate] via the
+   * following command:
+   *
+   * val serverCertificate: HeldCertificate = HeldCertificate.decode(getString("server_combined.pem"))
+   *
+   * https://github.com/square/okhttp/tree/master/okhttp-tls
+   * https://github.com/square/okhttp/blob/master/samples/guide/src/main/java/okhttp3/recipes/HttpsServer.java
+   */
+  private fun genSSLSocketFactory(): SSLSocketFactory {
+    val rootCertificate: HeldCertificate = HeldCertificate.Builder()
+        .certificateAuthority(1)
+        .build()
+
+    val intermediateCertificate: HeldCertificate = HeldCertificate.Builder()
+        .certificateAuthority(0)
+        .signedBy(rootCertificate)
         .build()
 
     val localhost: String = InetAddress.getByName("localhost").canonicalHostName
-    val localhostCertificate = HeldCertificate.Builder()
+    val serverCertificate: HeldCertificate = HeldCertificate.Builder()
         .addSubjectAlternativeName(localhost)
-        .signedBy(rootCertificate)
-        .rsa2048()
-        .keyPair(prepareKeyPairFromResources("ssl/mock_web_server_private_key.der", "ssl/mock_web_server_public_key.der"))
+        .signedBy(intermediateCertificate)
         .build()
-    val serverCertificates = HandshakeCertificates.Builder()
-        .heldCertificate(localhostCertificate)
-        .build()
-    return serverCertificates.sslSocketFactory()
-  }
 
-  private fun prepareKeyPairFromResources(prvKeyResId: String, pubKeyResId: String): KeyPair {
-    val keyFactory = KeyFactory.getInstance("RSA")
-    val prvKeyRaw: ByteArray = TestGeneralUtil.readObjectFromResourcesAsByteArray(prvKeyResId)
-    val pkcS8EncodedKeySpec = PKCS8EncodedKeySpec(prvKeyRaw)
-    val privateKey = keyFactory.generatePrivate(pkcS8EncodedKeySpec)
-    val pubKeyRaw: ByteArray = TestGeneralUtil.readObjectFromResourcesAsByteArray(pubKeyResId)
-    val x509EncodedKeySpec = X509EncodedKeySpec(pubKeyRaw)
-    val publicKey = keyFactory.generatePublic(x509EncodedKeySpec)
-    return KeyPair(publicKey, privateKey)
+    val serverHandshakeCertificates: HandshakeCertificates = HandshakeCertificates.Builder()
+        .heldCertificate(serverCertificate)
+        .build()
+
+    return serverHandshakeCertificates.sslSocketFactory()
   }
 }
