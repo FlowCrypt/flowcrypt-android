4e896cbce28941b088a79270b953ff609d56c458
DenBond7 <DenBond7@gmail.com>
2021-02-26 09:56:24 +0200

    Fixed "IllegalArgumentException: Multiple entries with same key: clearPackageData=true and clearPackageData=true"

--- script/ci-instrumentation-tests-with-mailserver.sh
+++ script/ci-instrumentation-tests-with-mailserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew --console=verbose :FlowCrypt:connectedDevTestDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.filter=com.flowcrypt.email.junit.filters.DependsOnMailServerFilter -Pandroid.testInstrumentationRunnerArguments.clearPackageData=true
+./gradlew --console=verbose :FlowCrypt:connectedDevTestDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.filter=com.flowcrypt.email.junit.filters.DependsOnMailServerFilter
