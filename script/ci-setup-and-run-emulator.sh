490164252dbba522c0a98516177ad70266218989
DenBond7 <DenBond7@gmail.com>
2021-02-26 14:11:15 +0200

    Modified ci-setup-and-run-emulator.sh(changed skin, increased ramSize to 3Gb)

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 "$ANDROID_SDK_ROOT/emulator/emulator" -accel-check
-echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-64-api29 --package "system-images;android-29;google_apis;x86_64" --device 'pixel' --abi 'google_apis/x86_64'
+echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-64-api29 --package "system-images;android-29;google_apis;x86_64" --device 'pixel_xl' --abi 'google_apis/x86_64'
 cat ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
 echo "vm.heapSize=256"  >> ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
-echo "hw.ramSize=2048"  >> ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
+echo "hw.ramSize=3064"  >> ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
 cat ~/.android/avd/ci-test-pixel-x86-64-api29.avd/config.ini
 "$ANDROID_SDK_ROOT/emulator/emulator" -list-avds #debug
 "$ANDROID_SDK_ROOT/emulator/emulator" -avd ci-test-pixel-x86-64-api29 -no-window -no-boot-anim -no-audio &
