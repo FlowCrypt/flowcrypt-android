13fb6d29af12685a18a67a8a058d876e11b1c7dc
DenBond7 <DenBond7@gmail.com>
2021-03-15 08:50:19 +0200

    Changed ci-wait-for-emulator.sh.| #1078

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-# Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
-
-set +e
-
-bootanim=""
-failcounter=0
-timeout_in_sec=90
-
-until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
-  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline" || "$bootanim" =~ "running" ]]; then
-    echo "Waiting for emulator to start"
-    if [[ $failcounter -gt timeout_in_sec ]]; then
-      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
-      exit 1
-    fi
-  fi
-  let "failcounter += 1"
-  sleep 1
-done
+adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+adb shell wm dismiss-keyguard
+sleep 1
+adb shell settings put global window_animation_scale 0
+adb shell settings put global transition_animation_scale 0
+adb shell settings put global animator_duration_scale 0
 
 echo "Emulator is ready"
