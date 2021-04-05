6b6a4157d5e7cda7009263ebc2806166446a8099
DenBond7 <DenBond7@gmail.com>
2021-04-05 13:33:54 +0300

    Updated installation of Android SDK.| #1115

--- script/ci-install-android-sdk.sh
+++ script/ci-install-android-sdk.sh
@@ -10,7 +10,7 @@ else
      touch ~/.android/repositories.cfg
 fi
 
-SDK_ARCHIVE=sdk-tools-linux-4333796.zip
+SDK_ARCHIVE=commandlinetools-linux-6858069_latest.zip
 
 sudo apt-get -qq install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils > /dev/null
 sudo kvm-ok
@@ -23,20 +23,22 @@ else
 
     # download, unpack and remove sdk archive
     wget https://dl.google.com/android/repository/$SDK_ARCHIVE
-    unzip -qq $SDK_ARCHIVE -d $ANDROID_SDK_ROOT
+    unzip -qq $SDK_ARCHIVE -d $ANDROID_SDK_ROOT/temp
     rm $SDK_ARCHIVE
 
-    (echo "yes" | ${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --licenses > /dev/null | grep -v = || true)
-    ( sleep 5; echo "y" ) | (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29" > /dev/null | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "extras;google;m2repository" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "cmdline-tools;latest" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "platform-tools" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "emulator" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "ndk;22.0.7026061" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "cmake;3.10.2.4988404" | grep -v = || true)
-    (${ANDROID_SDK_ROOT}/tools/bin/sdkmanager "system-images;android-30;google_apis;x86" | grep -v = || true)
+    (echo "yes" | ${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager --licenses > /dev/null | grep -v = || true)
+    ( sleep 5; echo "y" ) | (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "build-tools;29.0.2" "platforms;android-29" > /dev/null | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "extras;google;m2repository" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "cmdline-tools;latest" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "platform-tools" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "emulator" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "ndk;22.0.7026061" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "cmake;3.10.2.4988404" | grep -v = || true)
+    (${ANDROID_SDK_ROOT}/temp/cmdline-tools/bin/sdkmanager "system-images;android-30;google_apis;x86" | grep -v = || true)
+
+    rm $ANDROID_SDK_ROOT/temp
 fi
 
 #Uncomment this for debug
-#~/Android/Sdk/tools/bin/${ANDROID_SDK_ROOT}/tools/bin/sdkmanager --list
+#${ANDROID_SDK_ROOT}/cmdline-tools/latest/bin/sdkmanager --list
 
