44d7e188519f2d370746db61c21baac79bfc94d8
DenBond7 <DenBond7@gmail.com>
2021-04-06 14:16:30 +0300

    Renamed CI emulator's name.| #140

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 "$ANDROID_SDK_ROOT/emulator/emulator" -accel-check
-echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-api30 --package "system-images;android-30;google_apis;x86_64" --device 'pixel_xl' --abi 'google_apis/x86'
-cat ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
-echo "vm.heapSize=256"  >> ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
-echo "hw.ramSize=3064"  >> ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
-cat ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
+echo -ne '\n' | avdmanager -v create avd --name ci-emulator --package "system-images;android-30;google_apis;x86_64" --device 'pixel_xl' --abi 'google_apis/x86'
+cat ~/.android/avd/ci-emulator.avd/config.ini
+echo "vm.heapSize=256"  >> ~/.android/avd/ci-emulator.avd/config.ini
+echo "hw.ramSize=3064"  >> ~/.android/avd/ci-emulator.avd/config.ini
+cat ~/.android/avd/ci-emulator.avd/config.ini
 "$ANDROID_SDK_ROOT/emulator/emulator" -list-avds #debug
-"$ANDROID_SDK_ROOT/emulator/emulator" -avd ci-test-pixel-x86-api30 -no-window -no-boot-anim -no-audio &
+"$ANDROID_SDK_ROOT/emulator/emulator" -avd ci-emulator -no-window -no-boot-anim -no-audio &
