95b02ae268df48bb68406b212ad9a484027529ba
DenBond7 <DenBond7@gmail.com>
2021-04-06 12:59:03 +0300

    Temporary switched to use x86_64.| #140

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 "$ANDROID_SDK_ROOT/emulator/emulator" -accel-check
-echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-api30 --package "system-images;android-30;google_apis;x86" --device 'pixel_xl' --abi 'google_apis/x86'
+echo -ne '\n' | avdmanager -v create avd --name ci-test-pixel-x86-api30 --package "system-images;android-30;google_apis;x86_64" --device 'pixel_xl' --abi 'google_apis/x86'
 cat ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
 echo "vm.heapSize=256"  >> ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
 echo "hw.ramSize=3064"  >> ~/.android/avd/ci-test-pixel-x86-api30.avd/config.ini
