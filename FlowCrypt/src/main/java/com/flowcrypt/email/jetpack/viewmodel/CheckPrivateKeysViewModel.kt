39d52f4df782bca3958fa950e6341d2c706ba19d
DenBond7 <DenBond7@gmail.com>
2021-04-15 16:39:26 +0300

    Fixed coroutine issue in CheckPrivateKeysViewModel.| #1167

--- FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/viewmodel/CheckPrivateKeysViewModel.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/viewmodel/CheckPrivateKeysViewModel.kt
@@ -15,7 +15,9 @@ import com.flowcrypt.email.api.retrofit.response.model.node.NodeKeyDetails
 import com.flowcrypt.email.security.pgp.PgpKey
 import com.flowcrypt.email.util.GeneralUtil
 import com.flowcrypt.email.util.exception.WrongPassPhraseException
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @author Denis Bondarenko
@@ -27,50 +29,54 @@ class CheckPrivateKeysViewModel(application: Application) : BaseAndroidViewModel
   val checkPrvKeysLiveData: MutableLiveData<Result<List<CheckResult>>> = MutableLiveData()
 
   fun checkKeys(keys: List<NodeKeyDetails>, passphrase: String) {
-    checkPrvKeysLiveData.value = Result.loading()
-
-    if (passphrase.isEmpty()) {
-      checkPrvKeysLiveData.value = Result.error(emptyList())
-      return
-    }
-
-    val context: Context = getApplication()
-
     viewModelScope.launch {
-      val resultList = mutableListOf<CheckResult>()
-      for (keyDetails in keys) {
-        val copy = keyDetails.copy()
-        var e: Exception? = null
-        if (copy.isPrivate) {
-          val prvKey = copy.privateKey
-          if (prvKey.isNullOrEmpty()) {
-            e = IllegalArgumentException("Empty source")
-          } else {
-            if (copy.isFullyDecrypted == true) {
-              copy.passphrase = passphrase
-            } else {
-              try {
-                PgpKey.decryptKey(prvKey, passphrase)
-                copy.passphrase = passphrase
-              } catch (ex: Exception) {
-                //to prevent leak sensitive info we skip printing stack trace for release builds
-                if (GeneralUtil.isDebugBuild()) {
-                  ex.printStackTrace()
-                }
-                e = WrongPassPhraseException(context.getString(R.string.password_is_incorrect), ex)
-              }
-            }
-          }
-        } else {
-          e = IllegalArgumentException(context.getString(R.string.not_private_key))
-        }
-
-        resultList.add(CheckResult(copy, passphrase, e))
+      checkPrvKeysLiveData.value = Result.loading()
+      if (passphrase.isEmpty()) {
+        checkPrvKeysLiveData.value = Result.error(emptyList())
+        return@launch
       }
-
-      checkPrvKeysLiveData.value = Result.success(resultList)
+      checkPrvKeysLiveData.value = Result.success(checkKeysInternal(keys, passphrase))
     }
   }
 
-  data class CheckResult(val nodeKeyDetails: NodeKeyDetails, val passphrase: String, val e: Exception? = null)
+  private suspend fun checkKeysInternal(keys: List<NodeKeyDetails>,
+                                        passphrase: String): List<CheckResult> =
+      withContext(Dispatchers.IO) {
+        val context: Context = getApplication()
+        val resultList = mutableListOf<CheckResult>()
+        for (keyDetails in keys) {
+          val copy = keyDetails.copy()
+          var e: Exception? = null
+          if (copy.isPrivate) {
+            val prvKey = copy.privateKey
+            if (prvKey.isNullOrEmpty()) {
+              e = IllegalArgumentException("Empty source")
+            } else {
+              if (copy.isFullyDecrypted == true) {
+                copy.passphrase = passphrase
+              } else {
+                try {
+                  PgpKey.decryptKey(prvKey, passphrase)
+                  copy.passphrase = passphrase
+                } catch (ex: Exception) {
+                  //to prevent leak sensitive info we skip printing stack trace for release builds
+                  if (GeneralUtil.isDebugBuild()) {
+                    ex.printStackTrace()
+                  }
+                  e = WrongPassPhraseException(
+                      message = context.getString(R.string.password_is_incorrect), cause = ex)
+                }
+              }
+            }
+          } else {
+            e = IllegalArgumentException(context.getString(R.string.not_private_key))
+          }
+
+          resultList.add(CheckResult(copy, passphrase, e))
+        }
+        return@withContext resultList
+      }
+
+  data class CheckResult(val nodeKeyDetails: NodeKeyDetails,
+                         val passphrase: String, val e: Exception? = null)
 }
