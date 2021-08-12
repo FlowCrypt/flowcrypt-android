1664798b0d3cc7ff47acdc96e472085741034470
DenBond7 <DenBond7@gmail.com>
2021-08-12 17:27:05 +0300

    Modified ci-wait-for-emulator.sh to route all traffic for localhost to localhost:1212. | #1201

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -7,4 +7,12 @@ adb shell settings put global window_animation_scale 0
 adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
 
+###################################################################################################
+# to test WKD we need to route all traffic for localhost to localhost:1212
+adb root
+adb shell "echo 1 > /proc/sys/net/ipv4/ip_forward"
+adb shell "iptables -t nat -A PREROUTING -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
+adb shell "iptables -t nat -A OUTPUT -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
+###################################################################################################
+
 echo "Emulator is ready"
