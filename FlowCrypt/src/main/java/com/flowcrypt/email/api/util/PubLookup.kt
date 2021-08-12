7ba8e4412def7b33035975cf657b02b315700d43
DenBond7 <DenBond7@gmail.com>
2021-08-12 17:43:00 +0300

    Modified PubLookup to get first matching key by 'usableForEncryption'. | #1201

--- FlowCrypt/src/main/java/com/flowcrypt/email/api/util/PubLookup.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/api/util/PubLookup.kt
@@ -11,6 +11,8 @@ import com.flowcrypt.email.extensions.org.bouncycastle.openpgp.toPgpKeyDetails
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.ResponseBody.Companion.toResponseBody
+import org.pgpainless.algorithm.EncryptionPurpose
+import org.pgpainless.key.info.KeyRingInfo
 import retrofit2.Response
 import java.net.HttpURLConnection
 
@@ -21,11 +23,22 @@ import java.net.HttpURLConnection
  *         E-mail: DenBond7@gmail.com
  */
 object PubLookup {
+  /**
+   * Fetch pub keys using the user email.
+   *
+   * @return pub key. For now, we just peak at the first matching key. It should be improved in
+   * the future. See more details here https://github.com/FlowCrypt/flowcrypt-android/issues/480
+   */
   suspend fun lookupEmail(context: Context, email: String): Response<String> =
     withContext(Dispatchers.IO) {
       val pgpPublicKeyRingCollection = WkdClient.lookupEmail(context, email)
-      val firstMatchedKey = pgpPublicKeyRingCollection?.firstOrNull()
-      return@withContext firstMatchedKey?.toPgpKeyDetails()?.publicKey?.let { armoredPubKey ->
+
+      val firstMatchingKey = pgpPublicKeyRingCollection?.firstOrNull {
+        KeyRingInfo(it)
+          .getEncryptionSubkeys(EncryptionPurpose.STORAGE_AND_COMMUNICATIONS)
+          .isNotEmpty()
+      }
+      return@withContext firstMatchingKey?.toPgpKeyDetails()?.publicKey?.let { armoredPubKey ->
         Response.success(armoredPubKey)
       } ?: Response.error(HttpURLConnection.HTTP_NOT_FOUND, "Not found".toResponseBody())
     }
