904d3fec5b7c747ce8cf28b55c9a46ea01ad69f1
Den <DenBond7@gmail.com>
2021-10-18 20:28:28 +0300

    Added final checking for NO_PRV_BACKUP on the backup screen. Added a test for that.| #1512 (#1515)

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/activity/base/AddAccountToDatabaseRuleInterface.kt
@@ -0,0 +1,19 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.activity.base
+
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 10/18/21
+ *         Time: 5:27 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+interface AddAccountToDatabaseRuleInterface {
+  val addAccountToDatabaseRule
+    get() = AddAccountToDatabaseRule()
+}
