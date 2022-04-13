d8438c443ccb71e934aa4caa9690b5bf1d166711
DenBond7 <DenBond7@gmail.com>
2022-04-13 18:16:12 +0300

    Added some configuration to print tests.| #1193

--- script/ci-instrumentation-tests-without-mailserver.sh
+++ script/ci-instrumentation-tests-without-mailserver.sh
@@ -20,6 +20,14 @@ if [[ ${varShardIndex} -ge ${varNumShards} ]]
  then
   echo "shardIndex should be lower than numShards"
  else
+   #print test names
+   #adb shell am instrument -w \
+   # -e filter com.flowcrypt.email.junit.filters.DoesNotNeedMailServerFilter \
+   # -e numShards 3 \
+   # -e shardIndex 1 \
+   # -e log true \
+   # com.flowcrypt.email.debug.test/androidx.test.runner.AndroidJUnitRunner
+
   ./gradlew --console=plain :FlowCrypt:connectedDevTestDebugAndroidTest \
     -Pandroid.testInstrumentationRunnerArguments.filter=com.flowcrypt.email.junit.filters.DoesNotNeedMailServerFilter \
     -Pandroid.testInstrumentationRunnerArguments.numShards="${varNumShards}" \
