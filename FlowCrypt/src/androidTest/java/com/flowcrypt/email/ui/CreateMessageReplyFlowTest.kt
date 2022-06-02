3a32ab7a0a28f80fc1ef2a2e872be6514299df3b
DenBond7 <DenBond7@gmail.com>
2022-06-02 15:39:20 +0300

    CreateMessageActivityReplyTest -> CreateMessageReplyFlowTest.| #1787

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/CreateMessageReplyFlowTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/CreateMessageReplyFlowTest.kt
@@ -3,15 +3,15 @@
  * Contributors: DenBond7
  */
 
-package com.flowcrypt.email.ui.activity
+package com.flowcrypt.email.ui
 
 import android.content.Intent
-import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
@@ -22,7 +22,7 @@ import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
 import com.flowcrypt.email.rules.ClearAppSettingsRule
 import com.flowcrypt.email.rules.RetryRule
 import com.flowcrypt.email.rules.ScreenshotTestRule
-import com.flowcrypt.email.rules.lazyActivityScenarioRule
+import com.flowcrypt.email.ui.activity.CreateMessageActivity
 import com.flowcrypt.email.ui.activity.fragment.CreateMessageFragmentArgs
 import com.hootsuite.nachos.tokenizer.SpanChipTokenizer
 import org.junit.Rule
@@ -39,11 +39,25 @@ import org.junit.runner.RunWith
  */
 @MediumTest
 @RunWith(AndroidJUnit4::class)
-class CreateMessageActivityReplyTest : BaseTest() {
-  override val activeActivityRule =
-    lazyActivityScenarioRule<CreateMessageActivity>(launchActivity = false)
-  override val activityScenario: ActivityScenario<*>?
-    get() = activeActivityRule.scenario
+class CreateMessageReplyFlowTest : BaseTest() {
+  private val msgInfo = getMsgInfo(
+    "messages/info/standard_msg_reply_to_header.json",
+    "messages/mime/standard_msg_reply_to_header.txt"
+  )
+
+  override val activeActivityRule = activityScenarioRule<CreateMessageActivity>(
+    Intent(
+      getTargetContext(),
+      CreateMessageActivity::class.java
+    ).apply {
+      putExtras(
+        CreateMessageFragmentArgs(
+          incomingMessageInfo = msgInfo,
+          encryptedByDefault = false,
+          messageType = MessageType.REPLY
+        ).toBundle()
+      )
+    })
 
   @get:Rule
   var ruleChain: TestRule = RuleChain
@@ -56,24 +70,6 @@ class CreateMessageActivityReplyTest : BaseTest() {
 
   @Test
   fun testReplyToHeader() {
-    val msgInfo = getMsgInfo(
-      "messages/info/standard_msg_reply_to_header.json",
-      "messages/mime/standard_msg_reply_to_header.txt"
-    )
-    activeActivityRule.launch(
-      Intent(getTargetContext(), CreateMessageActivity::class.java).apply {
-        putExtras(
-          CreateMessageFragmentArgs(
-            incomingMessageInfo = msgInfo,
-            encryptedByDefault = false,
-            messageType = MessageType.REPLY
-          ).toBundle()
-        )
-      }
-    )
-
-    registerAllIdlingResources()
-
     onView(withId(R.id.editTextRecipientTo))
       .check(matches(isDisplayed()))
       .check(matches(withText(prepareChipText(msgInfo?.getReplyTo()?.first()?.address))))
