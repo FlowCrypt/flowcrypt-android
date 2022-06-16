ca5b9b294f968c5d783f930b448ccf4c8dc75f1b
DenBond7 <DenBond7@gmail.com>
2022-06-16 21:07:43 +0300

    wip

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/ComposeScreenDisallowUpdateRevokedKeyFlowTest.kt
@@ -0,0 +1,145 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.flowcrypt.email.TestConstants
+import com.flowcrypt.email.database.entity.RecipientEntity
+import com.flowcrypt.email.database.entity.relation.RecipientWithPubKeys
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.AddRecipientsToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.rules.FlowCryptMockWebServerRule
+import com.flowcrypt.email.rules.LazyActivityScenarioRule
+import com.flowcrypt.email.rules.RetryRule
+import com.flowcrypt.email.rules.ScreenshotTestRule
+import com.flowcrypt.email.ui.activity.CreateMessageActivity
+import com.flowcrypt.email.ui.base.BaseComposeScreenTest
+import com.flowcrypt.email.util.AccountDaoManager
+import com.flowcrypt.email.util.PrivateKeysManager
+import com.flowcrypt.email.util.TestGeneralUtil
+import okhttp3.mockwebserver.Dispatcher
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.RecordedRequest
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import java.net.HttpURLConnection
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 6/16/22
+ *         Time: 5:58 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class ComposeScreenDisallowUpdateRevokedKeyFlowTest : BaseComposeScreenTest() {
+  override val activeActivityRule: LazyActivityScenarioRule<CreateMessageActivity>? = null
+  override val activityScenarioRule = activityScenarioRule<CreateMessageActivity>(intent = intent)
+  override val activityScenario: ActivityScenario<*>?
+    get() = activityScenarioRule.scenario
+
+  override val addAccountToDatabaseRule: AddAccountToDatabaseRule
+    get() = AddAccountToDatabaseRule(AccountDaoManager.getUserFromBaseSettings(ACCOUNT))
+
+  private val addPrivateKeyToDatabaseRule =
+    AddPrivateKeyToDatabaseRule("pgp/denbond7@flowcrypt.test_prv_strong_primary.asc")
+
+  val pgpKeyDetails = PrivateKeysManager.getPgpKeyDetailsFromAssets(
+    "pgp/default@flowcrypt.test_secondKey_pub_revoked.asc"
+  )
+
+  private val addRecipientsToDatabaseRule = AddRecipientsToDatabaseRule(
+    listOf(
+      RecipientWithPubKeys(
+        RecipientEntity(email = RECIPIENT_WITH_REVOKED_KEY),
+        listOf(pgpKeyDetails.toPublicKeyEntity(RECIPIENT_WITH_REVOKED_KEY).copy(id = 12))
+      )
+    )
+  )
+
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+    .outerRule(RetryRule.DEFAULT)
+    .around(ClearAppSettingsRule())
+    .around(addAccountToDatabaseRule)
+    .around(addPrivateKeyToDatabaseRule)
+    .around(addRecipientsToDatabaseRule)
+    .around(activityScenarioRule)
+    .around(ScreenshotTestRule())
+
+  @Test
+  fun test() {
+    fillInAllFields(requireNotNull(pgpKeyDetails.getPrimaryInternetAddress()?.address))
+
+    /*onView(withId(R.id.editTextRecipientTo))
+      .check(
+        matches(
+          withChipsBackgroundColor(
+            internetAddress.address,
+            UIUtil.getColor(getTargetContext(), R.color.colorPrimary)
+          )
+        )
+      )*/
+
+    Thread.sleep(20000)
+  }
+
+  companion object {
+
+    const val ACCOUNT = "denbond7@flowcrypt.test"
+    const val RECIPIENT_WITH_REVOKED_KEY = "default@flowcrypt.test"
+
+    @get:ClassRule
+    @JvmStatic
+    val mockWebServerRule = FlowCryptMockWebServerRule(TestConstants.MOCK_WEB_SERVER_PORT,
+      object : Dispatcher() {
+        override fun dispatch(request: RecordedRequest): MockResponse {
+          if (request.path?.startsWith("/pub", ignoreCase = true) == true) {
+            val lastSegment = request.requestUrl?.pathSegments?.lastOrNull()
+
+            when {
+              TestConstants.RECIPIENT_WITHOUT_PUBLIC_KEY_ON_ATTESTER.equals(
+                lastSegment, true
+              ) -> {
+                return MockResponse()
+                  .setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
+                  .setBody(TestGeneralUtil.readResourceAsString("2.txt"))
+              }
+
+              TestConstants.RECIPIENT_WITH_PUBLIC_KEY_ON_ATTESTER.equals(
+                lastSegment, true
+              ) -> {
+                return MockResponse()
+                  .setResponseCode(HttpURLConnection.HTTP_OK)
+                  .setBody(TestGeneralUtil.readResourceAsString("3.txt"))
+              }
+
+              "95FC072E853C9C333C68EDD34B9CA2FBCA5B5FE7".equals(lastSegment, true) -> {
+                return MockResponse()
+                  .setResponseCode(HttpURLConnection.HTTP_OK)
+                  .setBody(
+                    TestGeneralUtil.readFileFromAssetsAsString(
+                      "pgp/expired_fixed@flowcrypt.test_not_expired_pub.asc"
+                    )
+                  )
+              }
+            }
+          }
+
+          return MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
+        }
+      })
+  }
+}
