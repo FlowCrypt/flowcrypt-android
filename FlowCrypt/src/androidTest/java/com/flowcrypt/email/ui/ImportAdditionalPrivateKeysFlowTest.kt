37246685c8331a39c4b95e40e9977b15c2affc3d
Denys Bondarenko <denbond7@gmail.com>
2022-08-01 21:41:00 +0300

    Fixed a bug in ImportAdditionalPrivateKeysFragment.| #1434

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/ImportAdditionalPrivateKeysFlowTest.kt
@@ -0,0 +1,81 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.flowcrypt.email.R
+import com.flowcrypt.email.TestConstants
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.database.entity.KeyEntity
+import com.flowcrypt.email.junit.annotations.DependsOnMailServer
+import com.flowcrypt.email.model.KeyImportDetails
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.rules.RetryRule
+import com.flowcrypt.email.rules.ScreenshotTestRule
+import com.flowcrypt.email.ui.activity.MainActivity
+import com.flowcrypt.email.ui.activity.fragment.ImportAdditionalPrivateKeysFragmentArgs
+import com.flowcrypt.email.util.TestGeneralUtil
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 8/1/22
+ *         Time: 3:05 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class ImportAdditionalPrivateKeysFlowTest : BaseTest() {
+  private val addAccountToDatabaseRule = AddAccountToDatabaseRule()
+  private val addPrivateKeyToDatabaseRule = AddPrivateKeyToDatabaseRule(
+    accountEntity = addAccountToDatabaseRule.account,
+    keyPath = "pgp/default@flowcrypt.test_secondKey_prv_default.asc",
+    passphrase = TestConstants.DEFAULT_PASSWORD,
+    sourceType = KeyImportDetails.SourceType.EMAIL,
+    passphraseType = KeyEntity.PassphraseType.RAM
+  )
+  override val activityScenarioRule = activityScenarioRule<MainActivity>(
+    TestGeneralUtil.genIntentForNavigationComponent(
+      R.id.importAdditionalPrivateKeysFragment,
+      ImportAdditionalPrivateKeysFragmentArgs(addAccountToDatabaseRule.account).toBundle()
+    )
+  )
+
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+    .outerRule(RetryRule.DEFAULT)
+    .around(ClearAppSettingsRule())
+    .around(addAccountToDatabaseRule)
+    .around(addPrivateKeyToDatabaseRule)
+    .around(activityScenarioRule)
+    .around(ScreenshotTestRule())
+
+  @Test
+  @DependsOnMailServer
+  fun testButtonImportBackup() {
+    onView(withId(R.id.buttonImportBackup))
+      .check(matches(isDisplayed()))
+      .perform(click())
+
+    //check that click on a button works well
+    onView(withId(R.id.editTextKeyPassword))
+      .check(matches(isDisplayed()))
+      .perform(click())
+  }
+}
