6bc7b0f690a453498b3ca5025e32db7bbf1a28cf
Denys Bondarenko <denbond7@gmail.com>
2022-08-01 21:35:55 +0300

    Dropped jcenter and jetifier.| #1036

--- FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/dialog/EditScreenshotDialogFragment.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/ui/activity/fragment/dialog/EditScreenshotDialogFragment.kt
@@ -13,6 +13,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.navArgs
@@ -24,6 +25,7 @@ import com.flowcrypt.email.util.GeneralUtil
 import com.flowcrypt.email.util.UIUtil
 import ja.burhanrashid52.photoeditor.OnSaveBitmap
 import ja.burhanrashid52.photoeditor.PhotoEditor
+import ja.burhanrashid52.photoeditor.shape.ShapeBuilder
 
 /**
  * It's a dialog which helps edit the given screenshot
@@ -36,8 +38,8 @@ import ja.burhanrashid52.photoeditor.PhotoEditor
 class EditScreenshotDialogFragment : BaseDialogFragment() {
   private var binding: FragmentScreenshotEditorBinding? = null
   private val args by navArgs<EditScreenshotDialogFragmentArgs>()
-  private lateinit var photoEditor: PhotoEditor
-  private lateinit var bitmap: Bitmap
+  private var photoEditor: PhotoEditor? = null
+  private var bitmap: Bitmap? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -62,10 +64,10 @@ class EditScreenshotDialogFragment : BaseDialogFragment() {
   }
 
   private fun initViews() {
-    binding?.imageButtonUndo?.setOnClickListener { photoEditor.undo() }
+    binding?.imageButtonUndo?.setOnClickListener { photoEditor?.undo() }
     binding?.imageButtonCancel?.setOnClickListener { navController?.navigateUp() }
     binding?.imageButtonSave?.setOnClickListener {
-      photoEditor.saveAsBitmap(object : OnSaveBitmap {
+      photoEditor?.saveAsBitmap(object : OnSaveBitmap {
         override fun onBitmapReady(saveBitmap: Bitmap?) {
           navController?.navigateUp()
           setFragmentResult(
@@ -83,23 +85,51 @@ class EditScreenshotDialogFragment : BaseDialogFragment() {
     binding?.radioGroupColors?.setOnCheckedChangeListener { _, checkedId ->
       when (checkedId) {
         R.id.radioButtonFullColor -> {
-          photoEditor.brushColor = ContextCompat.getColor(requireContext(), android.R.color.black)
-          photoEditor.setOpacity(100)
+          photoEditor?.setShape(
+            ShapeBuilder()
+              .withShapeColor(
+                ColorUtils.setAlphaComponent(
+                  ContextCompat.getColor(
+                    requireContext(),
+                    android.R.color.black
+                  ), 255
+                )
+              )
+          )
         }
 
         R.id.radioButtonLightColor -> {
-          photoEditor.brushColor = ContextCompat.getColor(requireContext(), R.color.yellow)
-          photoEditor.setOpacity(50)
+          photoEditor?.setShape(
+            ShapeBuilder()
+              .withShapeColor(
+                ColorUtils.setAlphaComponent(
+                  ContextCompat.getColor(
+                    requireContext(),
+                    R.color.yellow
+                  ), 127
+                )
+              )
+          )
         }
       }
     }
 
-    binding?.photoEditorView?.source?.setImageBitmap(bitmap)
-
-    photoEditor = PhotoEditor.Builder(context, binding?.photoEditorView).build()
-    photoEditor.setBrushDrawingMode(true)
-    photoEditor.brushColor = ContextCompat.getColor(requireContext(), android.R.color.black)
-    photoEditor.setOpacity(100)
+    binding?.photoEditorView?.let {
+      it.source.setImageBitmap(bitmap)
+      photoEditor = PhotoEditor.Builder(requireContext(), it).build()
+      photoEditor?.setBrushDrawingMode(true)
+      photoEditor?.setShape(
+        ShapeBuilder()
+          .withShapeColor(
+            ColorUtils.setAlphaComponent(
+              ContextCompat.getColor(
+                requireContext(),
+                android.R.color.black
+              ), 255
+            )
+          )
+      )
+    }
   }
 
   companion object {
