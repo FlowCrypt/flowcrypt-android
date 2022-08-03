c512f1ef8b1617fb8e63d827f1611afda7942dc0
DenBond7 <DenBond7@gmail.com>
2022-08-03 18:26:30 +0300

    Added UserRecoverableAuthExceptionFragmentInIsolationTest.| #1932

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/fragment/isolation/incontainer/UserRecoverableAuthExceptionFragmentInIsolationTest.kt
@@ -0,0 +1,64 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui.fragment.isolation.incontainer
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.rules.RetryRule
+import com.flowcrypt.email.rules.ScreenshotTestRule
+import com.flowcrypt.email.ui.activity.fragment.UserRecoverableAuthExceptionFragment
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 8/3/22
+ *         Time: 6:20 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class UserRecoverableAuthExceptionFragmentInIsolationTest : BaseTest() {
+  val addAccountToDatabaseRule = AddAccountToDatabaseRule()
+
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+    .outerRule(RetryRule.DEFAULT)
+    .around(ClearAppSettingsRule())
+    .around(addAccountToDatabaseRule)
+    .around(AddPrivateKeyToDatabaseRule())
+    .around(ScreenshotTestRule())
+
+  @Before
+  fun launchFragmentInContainerWithPredefinedArgs() {
+    launchFragmentInContainer<UserRecoverableAuthExceptionFragment>()
+  }
+
+  @Test
+  fun testBaseInfo() {
+    onView(
+      withText(
+        getResString(
+          R.string.reconnect_your_account,
+          getResString(R.string.app_name), addAccountToDatabaseRule.account.email
+        )
+      )
+    ).check(matches(isDisplayed()))
+  }
+}
