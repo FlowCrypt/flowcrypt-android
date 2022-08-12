757258908e1bd95fe20061f08f291e93ae721c2d
DenBond7 <DenBond7@gmail.com>
2022-08-12 12:52:53 +0300

    Modified PrivateKeysListFragmentFlowTest.| #1947

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/PrivateKeysListFragmentFlowTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/PrivateKeysListFragmentFlowTest.kt
@@ -12,24 +12,19 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
-import com.flowcrypt.email.TestConstants
 import com.flowcrypt.email.base.BaseTest
-import com.flowcrypt.email.matchers.CustomMatchers.Companion.withRecyclerViewItemCount
-import com.flowcrypt.email.model.KeyImportDetails
 import com.flowcrypt.email.rules.AddAccountToDatabaseRule
 import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
 import com.flowcrypt.email.rules.ClearAppSettingsRule
 import com.flowcrypt.email.rules.RetryRule
 import com.flowcrypt.email.rules.ScreenshotTestRule
 import com.flowcrypt.email.ui.activity.MainActivity
-import com.flowcrypt.email.util.PrivateKeysManager
 import com.flowcrypt.email.util.TestGeneralUtil
-import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -45,7 +40,6 @@ import org.junit.runner.RunWith
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 class PrivateKeysListFragmentFlowTest : BaseTest() {
-
   override val useIntents: Boolean = true
   override val activityScenarioRule = activityScenarioRule<MainActivity>(
     TestGeneralUtil.genIntentForNavigationComponent(
@@ -65,34 +59,13 @@ class PrivateKeysListFragmentFlowTest : BaseTest() {
     .around(activityScenarioRule)
     .around(ScreenshotTestRule())
 
-  @Before
-  fun waitData() {
-    //todo-denbond7 need to wait while activity lunches a fragment and loads data via ROOM.
-    // Need to improve this code after espresso updates
-    Thread.sleep(3000)
-  }
-
   @Test
-  @Ignore("Temporary disabled due to architecture changes")
-  fun testAddNewKeys() {
-    /*intending(hasComponent(ComponentName(getTargetContext(), ImportPrivateKeyActivity::class.java)))
-      .respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, null))*/
-
-    val details =
-      PrivateKeysManager.getPgpKeyDetailsFromAssets("pgp/default@flowcrypt.test_secondKey_prv_default.asc")
-    PrivateKeysManager.saveKeyToDatabase(
-      accountEntity = addAccountToDatabaseRule.account,
-      pgpKeyDetails = details,
-      passphrase = TestConstants.DEFAULT_PASSWORD,
-      sourceType = KeyImportDetails.SourceType.EMAIL
-    )
-
+  fun testNavigationToImportKeyScreen() {
     onView(withId(R.id.floatActionButtonAddKey))
       .check(matches(isDisplayed()))
       .perform(click())
-    onView(withId(R.id.recyclerViewKeys))
+    onView(withText(R.string.import_private_key))
       .check(matches(isDisplayed()))
-      .check(matches(withRecyclerViewItemCount(2)))
   }
 
   @Test
