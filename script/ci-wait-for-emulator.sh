63e137a43f2bf80a9a9a4cba366324ce0dd7afcb
DenBond7 <DenBond7@gmail.com>
2022-10-25 15:52:29 +0300

    Updated ci-wait-for-emulator.sh.| #2013

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -10,6 +10,8 @@ adb shell settings put global animator_duration_scale 0
 ###################################################################################################
 # to test WKD we need to route all traffic for localhost to localhost:1212
 adb root
+# Need wait for the root environment
+sleep 20
 adb shell "echo 1 > /proc/sys/net/ipv4/ip_forward"
 adb shell "iptables -t nat -A PREROUTING -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
 adb shell "iptables -t nat -A OUTPUT -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
