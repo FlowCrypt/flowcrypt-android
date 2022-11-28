8dbd010a73a9e2f3148202d14d3845a6763406d7
DenBond7 <DenBond7@gmail.com>
2022-11-28 13:02:12 +0200

    Added DraftsGmailAPITestCorrectCreatingAndUpdatingFlowTest.| #2050

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/DraftsGmailAPITestCorrectCreatingAndUpdatingFlowTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/DraftsGmailAPITestCorrectCreatingAndUpdatingFlowTest.kt
@@ -7,17 +7,21 @@ package com.flowcrypt.email.ui
 
 import android.util.Base64
 import android.view.Gravity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -32,6 +36,7 @@ import com.flowcrypt.email.api.retrofit.response.model.OrgRules
 import com.flowcrypt.email.base.BaseTest
 import com.flowcrypt.email.database.entity.AccountEntity
 import com.flowcrypt.email.jetpack.viewmodel.DraftViewModel
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withRecyclerViewItemCount
 import com.flowcrypt.email.rules.AddAccountToDatabaseRule
 import com.flowcrypt.email.rules.AddLabelsToDatabaseRule
 import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
@@ -75,7 +80,7 @@ import kotlin.random.Random
 
 @MediumTest
 @RunWith(AndroidJUnit4::class)
-class DraftsGmailAPIFlowTest : BaseTest() {
+class DraftsGmailAPITestCorrectCreatingAndUpdatingFlowTest : BaseTest() {
   override val activityScenarioRule = activityScenarioRule<MainActivity>()
 
   private val accountEntity = AccountDaoManager.getDefaultAccountDao().copy(
@@ -118,7 +123,7 @@ class DraftsGmailAPIFlowTest : BaseTest() {
     Passphrase.fromPassword(TestConstants.DEFAULT_STRONG_PASSWORD)
   )
 
-  private val drafts = mutableListOf<Draft>()
+  private val draftsCache = mutableListOf<Draft>()
 
   private val mockWebServerRule =
     FlowCryptMockWebServerRule(TestConstants.MOCK_WEB_SERVER_PORT, object : Dispatcher() {
@@ -165,61 +170,152 @@ class DraftsGmailAPIFlowTest : BaseTest() {
             )
           }
 
-          request.method == "GET" && request.path == "/gmail/v1/users/me/drafts?fields=drafts/id,drafts/message/id&maxResults=500" -> {
+          request.method == "GET" && request.path == "/gmail/v1/users/me/drafts?maxResults=45" -> {
             return MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(
               ListDraftsResponse().apply {
                 factory = GsonFactory.getDefaultInstance()
-                drafts = drafts
+                drafts = draftsCache.map { draft ->
+                  Draft().apply {
+                    id = draft.id
+                    message = Message().apply {
+                      id = draft.message.id
+                      threadId = draft.message.threadId
+                    }
+                  }
+                }
               }.toString()
             )
           }
 
-          request.method == "POST" && request.path == "/gmail/v1/users/me/drafts" -> {
-            val gzipInputStream = GZIPInputStream(request.body.inputStream())
-            val draft = JsonObjectParser(GsonFactory.getDefaultInstance()).parseAndClose(
-              InputStreamReader(gzipInputStream), Draft::class.java
-            )
-            val rawMimeMessageAsByteArray = Base64.decode(
-              draft.message.raw, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
-            )
-            val mimeMessage = MimeMessage(
-              Session.getInstance(Properties()), rawMimeMessageAsByteArray.inputStream()
-            )
-
-            return if (MESSAGE_SUBJECT_FIRST == mimeMessage.subject) {
-              val newDraft = Draft().apply {
+          request.method == "GET" && request.path == "/gmail/v1/users/me/drafts?fields=drafts/id,drafts/message/id&maxResults=500" -> {
+            return MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(
+              ListDraftsResponse().apply {
                 factory = GsonFactory.getDefaultInstance()
-                id = DRAFT_ID_FIRST
-                message = Message().apply {
-                  id = MESSAGE_ID_FIRST
-                  threadId = THREAD_ID_FIRST
-                  labelIds = listOf(LABEL_DRAFT)
-                  raw = draft.message.raw
+                drafts = draftsCache.map { draft ->
+                  Draft().apply {
+                    id = draft.id
+                    message = Message().apply {
+                      id = draft.message.id
+                    }
+                  }
                 }
-              }
-              drafts.add(newDraft)
+              }.toString()
+            )
+          }
 
-              MockResponse().setResponseCode(HttpURLConnection.HTTP_OK).setBody(draft.toString())
+          request.method == "PUT" && request.path == "/gmail/v1/users/me/drafts/$DRAFT_ID_FIRST" -> {
+            val (draft, mimeMessage) = getDraftAndMimeMessageFromRequest(request)
+            val existingDraftInCache = draftsCache.firstOrNull { it.id == DRAFT_ID_FIRST }
+
+            return if (existingDraftInCache != null && mimeMessage.subject == MESSAGE_SUBJECT_FIRST_EDITED) {
+              val existingMessage = existingDraftInCache.message
+              existingDraftInCache.message = Message().apply {
+                id = existingMessage.id
+                threadId = existingMessage.threadId
+                labelIds = existingMessage.labelIds
+                raw = draft.message.raw
+              }
+
+              MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(existingDraftInCache.toString())
             } else {
-              MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+              MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
+            }
+          }
+
+          request.method == "POST" && request.path == "/gmail/v1/users/me/drafts" -> {
+            val (draft, mimeMessage) = getDraftAndMimeMessageFromRequest(request)
+
+            return when (mimeMessage.subject) {
+              MESSAGE_SUBJECT_FIRST -> {
+                val newDraft = prepareDraft(
+                  draftId = DRAFT_ID_FIRST,
+                  messageId = MESSAGE_ID_FIRST,
+                  messageThreadId = THREAD_ID_FIRST,
+                  rawMsg = draft.message.raw
+                )
+                draftsCache.add(newDraft)
+
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                  .setBody(newDraft.toString())
+              }
+
+              MESSAGE_SUBJECT_SECOND -> {
+                val newDraft = prepareDraft(
+                  draftId = DRAFT_ID_SECOND,
+                  messageId = MESSAGE_ID_SECOND,
+                  messageThreadId = THREAD_ID_SECOND,
+                  rawMsg = draft.message.raw
+                )
+                draftsCache.add(newDraft)
+
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+                  .setBody(newDraft.toString())
+              }
+
+              else -> {
+                MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST)
+              }
             }
           }
 
           request.path == "/gmail/v1/users/me/messages/${MESSAGE_ID_FIRST}?fields=id,threadId,historyId&format=full" -> {
-            return MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
-              .setBody(Message().apply {
-                factory = GsonFactory.getDefaultInstance()
-                id = MESSAGE_ID_FIRST
-                threadId = THREAD_ID_FIRST
-                labelIds = listOf(LABEL_DRAFT)
-                historyId = BigInteger.valueOf(Random.nextLong())
-              }.toString())
+            return genMsgDetailsMockResponse(MESSAGE_ID_FIRST, THREAD_ID_FIRST)
+          }
+
+          request.path == "/gmail/v1/users/me/messages/${MESSAGE_ID_SECOND}?fields=id,threadId,historyId&format=full" -> {
+            return genMsgDetailsMockResponse(MESSAGE_ID_SECOND, THREAD_ID_SECOND)
           }
 
           else -> return MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
         }
       }
     })
+
+  private fun getDraftAndMimeMessageFromRequest(request: RecordedRequest): Pair<Draft, MimeMessage> {
+    val gzipInputStream = GZIPInputStream(request.body.inputStream())
+    val draft = JsonObjectParser(GsonFactory.getDefaultInstance()).parseAndClose(
+      InputStreamReader(gzipInputStream), Draft::class.java
+    )
+    val rawMimeMessageAsByteArray = Base64.decode(
+      draft.message.raw, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
+    )
+    val mimeMessage = MimeMessage(
+      Session.getInstance(Properties()), rawMimeMessageAsByteArray.inputStream()
+    )
+    return Pair(draft, mimeMessage)
+  }
+
+  private fun genMsgDetailsMockResponse(
+    messageId: String,
+    messageThreadId: String,
+  ) =
+    MockResponse().setResponseCode(HttpURLConnection.HTTP_OK)
+      .setBody(Message().apply {
+        factory = GsonFactory.getDefaultInstance()
+        id = messageId
+        threadId = messageThreadId
+        labelIds = listOf(JavaEmailConstants.FOLDER_DRAFT)
+        historyId = BigInteger.valueOf(Random.nextLong())
+      }.toString())
+
+  private fun prepareDraft(
+    draftId: String,
+    messageId: String,
+    messageThreadId: String,
+    rawMsg: String
+  ): Draft {
+    return Draft().apply {
+      factory = GsonFactory.getDefaultInstance()
+      id = draftId
+      message = Message().apply {
+        id = messageId
+        threadId = messageThreadId
+        labelIds = listOf(JavaEmailConstants.FOLDER_DRAFT)
+        raw = rawMsg
+      }
+    }
+  }
 
   @get:Rule
   var ruleChain: TestRule =
@@ -234,9 +330,66 @@ class DraftsGmailAPIFlowTest : BaseTest() {
 
   @Test
   fun testCorrectCreatingAndUpdating() {
+    draftsCache.clear()
     moveToDraftFolder()
 
-    Thread.sleep(10000)
+    onView(withId(R.id.recyclerViewMsgs))
+      .check(matches(withRecyclerViewItemCount(0)))
+
+    openComposeScreenAndTypeSubject(MESSAGE_SUBJECT_FIRST)
+    Thread.sleep(DraftViewModel.DELAY_TIMEOUT * 2)
+    Espresso.pressBack()
+
+    //check that the first draft was created
+    assertEquals(1, draftsCache.size)
+    onView(withId(R.id.recyclerViewMsgs))
+      .check(matches(withRecyclerViewItemCount(1)))
+    val mimeMessageFirst = getMimeMessageFromCache(0)
+    assertEquals(MESSAGE_SUBJECT_FIRST, mimeMessageFirst.subject)
+
+    openComposeScreenAndTypeSubject(MESSAGE_SUBJECT_SECOND)
+    Thread.sleep(DraftViewModel.DELAY_TIMEOUT * 2)
+    Espresso.pressBack()
+
+    //check that the second draft was created
+    assertEquals(2, draftsCache.size)
+    onView(withId(R.id.recyclerViewMsgs))
+      .check(matches(withRecyclerViewItemCount(2)))
+    val mimeMessageSecond = getMimeMessageFromCache(1)
+    assertEquals(MESSAGE_SUBJECT_SECOND, mimeMessageSecond.subject)
+
+    //open the first draft and modify it
+    onView(withId(R.id.recyclerViewMsgs))
+      .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+    onView(withId(R.id.imageButtonEditDraft))
+      .check(matches(isDisplayed()))
+      .perform(click())
+    onView(withId(R.id.editTextEmailSubject))
+      .check(matches(isDisplayed()))
+      .perform(
+        scrollTo(),
+        click(),
+        replaceText(MESSAGE_SUBJECT_FIRST_EDITED),
+        closeSoftKeyboard()
+      )
+
+    Thread.sleep(DraftViewModel.DELAY_TIMEOUT * 2)
+    Espresso.pressBack()//back to the message details screen
+    Espresso.pressBack()//back to the messages list screen
+
+    //check if 1st draft is updated correctly and 2nd draft remains same
+    assertEquals(2, draftsCache.size)
+    onView(withId(R.id.recyclerViewMsgs))
+      .check(matches(withRecyclerViewItemCount(2)))
+    val mimeMessageFirstEdited = getMimeMessageFromCache(0)
+    assertEquals(MESSAGE_SUBJECT_FIRST_EDITED, mimeMessageFirstEdited.subject)
+    onView(withText(MESSAGE_SUBJECT_FIRST_EDITED))
+      .check(matches(isDisplayed()))
+    val mimeMessageSecondAfterEditingFirst = getMimeMessageFromCache(1)
+    assertEquals(MESSAGE_SUBJECT_SECOND, mimeMessageSecondAfterEditingFirst.subject)
+  }
+
+  private fun openComposeScreenAndTypeSubject(subject: String) {
     //open the compose screen
     onView(withId(R.id.floatActionButtonCompose))
       .check(matches(isDisplayed()))
@@ -248,24 +401,18 @@ class DraftsGmailAPIFlowTest : BaseTest() {
       .perform(
         scrollTo(),
         click(),
-        typeText(MESSAGE_SUBJECT_FIRST),
+        typeText(subject),
         closeSoftKeyboard()
       )
+  }
 
-    Thread.sleep(DraftViewModel.DELAY_TIMEOUT * 2)
-
-    Espresso.pressBack()
-
-    //check that the first draft was created
-    assertEquals(1, drafts.size)
-    val rawMimeMessageAsByteArray = Base64.decode(
-      drafts[0].message.raw, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
+  private fun getMimeMessageFromCache(msgPosition: Int): MimeMessage {
+    val rawMimeMessageAsByteArrayOfSecondMsg = Base64.decode(
+      draftsCache[msgPosition].message.raw, Base64.URL_SAFE or Base64.NO_PADDING or Base64.NO_WRAP
     )
-    val mimeMessage = MimeMessage(
-      Session.getInstance(Properties()), rawMimeMessageAsByteArray.inputStream()
+    return MimeMessage(
+      Session.getInstance(Properties()), rawMimeMessageAsByteArrayOfSecondMsg.inputStream()
     )
-    assertEquals(MESSAGE_SUBJECT_FIRST, mimeMessage.subject)
-    Thread.sleep(1000)
   }
 
   private fun moveToDraftFolder() {
@@ -280,10 +427,15 @@ class DraftsGmailAPIFlowTest : BaseTest() {
   }
 
   companion object {
-    const val DRAFT_ID_FIRST = "r5555555555555555555"
-    const val MESSAGE_ID_FIRST = "5555555555555555"
+    const val DRAFT_ID_FIRST = "r5555555555555555551"
+    const val MESSAGE_ID_FIRST = "5555555555555551"
     const val THREAD_ID_FIRST = "1111111111111111"
-    const val LABEL_DRAFT = "Draft"
-    const val MESSAGE_SUBJECT_FIRST = "Some subject"
+    const val MESSAGE_SUBJECT_FIRST = "first"
+    const val MESSAGE_SUBJECT_FIRST_EDITED = "first edited"
+
+    const val DRAFT_ID_SECOND = "r5555555555555555552"
+    const val MESSAGE_ID_SECOND = "5555555555555552"
+    const val THREAD_ID_SECOND = "11111111111111112"
+    const val MESSAGE_SUBJECT_SECOND = "second"
   }
 }
