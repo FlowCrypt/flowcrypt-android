4721fc930a8d044e73ecd1697080d5807f1713f3
DenBond7 <DenBond7@gmail.com>
2023-01-10 19:05:46 +0200

    Added code to modify an emulator settings.| #2136

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -8,10 +8,14 @@ adb shell settings put global transition_animation_scale 0
 adb shell settings put global animator_duration_scale 0
 
 ###################################################################################################
-# to test WKD we need to route all traffic for localhost to localhost:1212
 adb root
 # Need wait for the root environment
 sleep 20
+# on SemaphoreCI ping fes.localhost on an emulator returns 'unknown host fes.localhost'.
+# Need to change /etc/hosts
+adb remount # Use this option to have a writable system image during an emulation session
+adb shell "echo \"127.0.0.1  fes.localhost\" >> /etc/hosts"
+# to test WKD we need to route all traffic for localhost to localhost:1212
 adb shell "echo 1 > /proc/sys/net/ipv4/ip_forward"
 adb shell "iptables -t nat -A PREROUTING -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
 adb shell "iptables -t nat -A OUTPUT -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
