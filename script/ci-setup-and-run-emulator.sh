865cbf7b920d10008de1f142b231f3627e9a14c2
DenBond7 <DenBond7@gmail.com>
2023-01-11 10:20:43 +0200

    Added code to modify an emulator settings. Step 2.| #2136

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -7,4 +7,4 @@ cat ~/.android/avd/ci-emulator.avd/config.ini
 # echo "hw.ramSize=3064"  >> ~/.android/avd/ci-emulator.avd/config.ini
 # cat ~/.android/avd/ci-emulator.avd/config.ini
 "$ANDROID_SDK_ROOT/emulator/emulator" -list-avds #debug
-"$ANDROID_SDK_ROOT/emulator/emulator" -avd ci-emulator -no-window -no-boot-anim -no-audio -gpu auto -verbose &
+"$ANDROID_SDK_ROOT/emulator/emulator" -avd ci-emulator -no-window -no-boot-anim -no-audio -gpu auto -writable-system -verbose &
