e13b95ec6ef23bc1c493a1ade2546a22fd63f64b
DenBond7 <DenBond7@gmail.com>
2023-01-11 11:53:46 +0200

    Added code to modify an emulator settings. Step .| #2136

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -o xtrace
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 adb shell wm dismiss-keyguard
 sleep 1
@@ -11,6 +11,8 @@ adb shell settings put global animator_duration_scale 0
 # on SemaphoreCI ping fes.localhost on an emulator returns 'unknown host fes.localhost'.
 # Need to change /etc/hosts
 adb root
+# Need wait for the root environment
+sleep 20
 adb remount # Use this option to have a writable system image during an emulation session
 adb reboot
 # Need to wait the device reboot.
@@ -29,3 +31,4 @@ adb shell "iptables -t nat -A OUTPUT -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT
 ###################################################################################################
 
 echo "Emulator is ready"
+set +o xtrace
