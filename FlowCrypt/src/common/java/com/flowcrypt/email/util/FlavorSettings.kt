3661a5314748365c2ed644b8a6e0f501bcae3e70
DenBond7 <DenBond7@gmail.com>
2023-01-26 17:40:07 +0200

    Modified build.gradle. Added 'common' productFlavors.| #2162

---
+++ FlowCrypt/src/common/java/com/flowcrypt/email/util/FlavorSettings.kt
@@ -0,0 +1,18 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.util
+
+import android.content.Context
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 10/13/20
+ *         Time: 12:41 PM
+ *         E-mail: DenBond7@gmail.com
+ */
+object FlavorSettings : EnvironmentSettings {
+  override fun configure(context: Context) {}
+}
