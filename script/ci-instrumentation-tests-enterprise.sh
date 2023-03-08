911eaf50f5706885fa1f0ba081ad4207089cc2f1
DenBond7 <DenBond7@gmail.com>
2023-03-08 18:19:35 +0200

    Update Semaphore settings to run enterprise tests.| #2162

---
+++ script/ci-instrumentation-tests-enterprise.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./gradlew --console=plain :FlowCrypt:connectedEnterpriseUiTestsAndroidTest \
+  -Pandroid.testInstrumentationRunnerArguments.filter=com.flowcrypt.email.junit.filters.EnterpriseTestsFilter
