505b9b9f8d76da8cb5c222296e9151fdfddbd592
DenBond7 <DenBond7@gmail.com>
2023-03-09 12:58:49 +0200

    Marked some tests as FlakyTest.| #2162

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/fragment/isolation/incontainer/AttesterSettingsFragmentInIsolationTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/fragment/isolation/incontainer/AttesterSettingsFragmentInIsolationTest.kt
@@ -15,6 +15,7 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
 import com.flowcrypt.email.TestConstants
@@ -124,6 +125,7 @@ class AttesterSettingsFragmentInIsolationTest : BaseTest() {
   }
 
   @Test
+  @FlakyTest
   fun testAccountWithNoKeysOnAttester() {
     FlowCryptRoomDatabase.getDatabase(getTargetContext()).accountDao()
       .addAccount(userWithoutPubKeyOnAttester)
