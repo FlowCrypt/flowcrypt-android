17e26464556197c15b90cdf10f4eeeb9f561bf23
DenBond7 <DenBond7@gmail.com>
2023-03-24 13:17:36 +0200

    Improved PasswordProtectedAttachmentInfoDataSource to use a stream directly instead of read all data to a buffer.| #2202

--- FlowCrypt/src/main/java/com/flowcrypt/email/api/email/javamail/PasswordProtectedAttachmentInfoDataSource.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/api/email/javamail/PasswordProtectedAttachmentInfoDataSource.kt
@@ -11,12 +11,15 @@ import com.flowcrypt.email.security.pgp.PgpDecryptAndOrVerify
 import org.apache.commons.io.FilenameUtils
 import org.bouncycastle.openpgp.PGPSecretKeyRingCollection
 import org.pgpainless.key.protection.SecretKeyRingProtector
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
+import java.io.BufferedInputStream
 import java.io.InputStream
 
 /**
  * It's a special version of [AttachmentInfoDataSource] which decrypts a source before using.
+ * Due to https://github.com/pgpainless/pgpainless/pull/360 we have to use a custom implementation
+ * for streams here. For the first call of [jakarta.activation.DataSource.getInputStream]
+ * we will not care about errors for [InputStream.close].
+ *
  * Also, it drops a file extension(as this an incoming attachment here always will have '.pgp').
  *
  * @author Denys Bondarenko
@@ -27,18 +30,29 @@ class PasswordProtectedAttachmentInfoDataSource(
   private val secretKeys: PGPSecretKeyRingCollection,
   private val protector: SecretKeyRingProtector
 ) : AttachmentInfoDataSource(context, att) {
+  private var isFirstUsage = false
   override fun getInputStream(): InputStream? {
     val inputStream = super.getInputStream() ?: return null
-    val buffer = ByteArrayOutputStream()
     val decryptionStream = PgpDecryptAndOrVerify.genDecryptionStream(
       srcInputStream = inputStream,
       secretKeys = secretKeys,
       protector = protector
     )
-    decryptionStream.copyTo(buffer)
-    //ref https://github.com/pgpainless/pgpainless/pull/360
-    //todo-denbond7 should be improved to use a stream directly
-    return ByteArrayInputStream(buffer.toByteArray())
+
+    return if (isFirstUsage) {
+      decryptionStream
+    } else {
+      isFirstUsage = true
+      object : BufferedInputStream(decryptionStream) {
+        override fun close() {
+          try {
+            super.close()
+          } catch (e: Exception) {
+            //we don't care about exception here. Can be skipped.
+          }
+        }
+      }
+    }
   }
 
   override fun getName(): String {
