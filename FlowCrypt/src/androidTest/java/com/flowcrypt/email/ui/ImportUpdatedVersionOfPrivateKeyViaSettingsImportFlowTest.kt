ce8652d3144d37646789100e9fca0c5cf10ac193
Den <DenBond7@gmail.com>
2023-05-15 17:56:46 +0300

    Ability to update an existing private key with a newer key (#2331)
    * Added ability to update an existing private key with a newer key.| #1287
    
    * Added ImportUpdatedVersionOfPrivateKeyViaSettingsImportFlowTest.| #1287
    
    * Refactored code.| #1287

---
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/ImportUpdatedVersionOfPrivateKeyViaSettingsImportFlowTest.kt
@@ -0,0 +1,151 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+package com.flowcrypt.email.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasSibling
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withChild
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.flowcrypt.email.R
+import com.flowcrypt.email.TestConstants
+import com.flowcrypt.email.base.BaseTest
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.hasItem
+import com.flowcrypt.email.matchers.CustomMatchers.Companion.withRecyclerViewItemCount
+import com.flowcrypt.email.model.KeyImportDetails
+import com.flowcrypt.email.rules.AddAccountToDatabaseRule
+import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
+import com.flowcrypt.email.rules.ClearAppSettingsRule
+import com.flowcrypt.email.rules.GrantPermissionRuleChooser
+import com.flowcrypt.email.rules.RetryRule
+import com.flowcrypt.email.rules.ScreenshotTestRule
+import com.flowcrypt.email.security.KeysStorageImpl
+import com.flowcrypt.email.ui.activity.MainActivity
+import com.flowcrypt.email.util.TestGeneralUtil
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.not
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+
+/**
+ * @author Denys Bondarenko
+ */
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class ImportUpdatedVersionOfPrivateKeyViaSettingsImportFlowTest : BaseTest() {
+  override val useIntents: Boolean = true
+  override val activityScenarioRule = activityScenarioRule<MainActivity>(
+    TestGeneralUtil.genIntentForNavigationComponent(
+      destinationId = R.id.privateKeysListFragment
+    )
+  )
+
+  private val addAccountToDatabaseRule = AddAccountToDatabaseRule()
+  private val addPrivateKeyToDatabaseRuleDefault = AddPrivateKeyToDatabaseRule()
+  private val addPrivateKeyToDatabaseRuleExpired = AddPrivateKeyToDatabaseRule(
+    accountEntity = addAccountToDatabaseRule.account,
+    keyPath = "pgp/expired@flowcrypt.test_prv_default.asc",
+    passphrase = TestConstants.DEFAULT_PASSWORD,
+    sourceType = KeyImportDetails.SourceType.EMAIL
+  )
+
+  private val expiredExtendedPrvKey = TestGeneralUtil.readFileFromAssetsAsString(
+    "pgp/expired@flowcrypt.test_prv_default_extended.asc"
+  )
+
+  @get:Rule
+  var ruleChain: TestRule = RuleChain
+    .outerRule(RetryRule.DEFAULT)
+    .around(ClearAppSettingsRule())
+    .around(GrantPermissionRuleChooser.grant(android.Manifest.permission.POST_NOTIFICATIONS))
+    .around(addAccountToDatabaseRule)
+    .around(addPrivateKeyToDatabaseRuleDefault)
+    .around(addPrivateKeyToDatabaseRuleExpired)
+    .around(activityScenarioRule)
+    .around(ScreenshotTestRule())
+
+  @Test
+  fun testNavigationToImportKeyScreen() {
+
+    onView(withId(R.id.recyclerViewKeys))
+      .check(matches(withRecyclerViewItemCount(2)))
+
+    onView(withId(R.id.recyclerViewKeys))
+      .check(
+        matches(prepareMatcherForExpiredKey())
+      )
+
+    assertTrue(
+      KeysStorageImpl.getInstance(getTargetContext()).getPgpKeyDetailsList().first {
+        it.fingerprint == addPrivateKeyToDatabaseRuleExpired.pgpKeyDetails.fingerprint
+      }.isExpired
+    )
+
+    onView(withId(R.id.floatActionButtonAddKey))
+      .check(matches(isDisplayed()))
+      .perform(click())
+
+    onView(withText(R.string.import_private_key))
+      .check(matches(isDisplayed()))
+
+    addTextToClipboard("private key", expiredExtendedPrvKey)
+
+    onView(withId(R.id.buttonLoadFromClipboard))
+      .check(matches(isDisplayed()))
+      .perform(click())
+
+    onView(withId(R.id.editTextKeyPassword))
+      .perform(
+        scrollTo(),
+        typeText(TestConstants.DEFAULT_PASSWORD),
+        closeSoftKeyboard()
+      )
+    onView(withId(R.id.buttonPositiveAction))
+      .perform(scrollTo(), click())
+
+    Thread.sleep(1000)
+
+    onView(withId(R.id.recyclerViewKeys))
+      .check(matches(not(prepareMatcherForExpiredKey())))
+
+    assertFalse(
+      KeysStorageImpl.getInstance(getTargetContext()).getPgpKeyDetailsList().first {
+        it.fingerprint == addPrivateKeyToDatabaseRuleExpired.pgpKeyDetails.fingerprint
+      }.isExpired
+    )
+  }
+
+  private fun prepareMatcherForExpiredKey() = hasItem(
+    withChild(
+      allOf(
+        hasSibling(
+          withText(
+            addPrivateKeyToDatabaseRuleExpired.pgpKeyDetails.getUserIdsAsSingleString()
+          )
+        ),
+        hasSibling(
+          withText(
+            addPrivateKeyToDatabaseRuleExpired.pgpKeyDetails.getStatusText(getTargetContext())
+          )
+        )
+      )
+    )
+  )
+}
--- FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/viewmodel/PrivateKeysViewModel.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/viewmodel/PrivateKeysViewModel.kt
@@ -192,71 +192,85 @@ class PrivateKeysViewModel(application: Application) : AccountViewModel(applicat
       val context: Context = getApplication()
       savePrivateKeysLiveData.value = Result.loading()
       try {
-        for (keyDetails in keys) {
-          val fingerprint = keyDetails.fingerprint
-          if (!keyDetails.isFullyEncrypted) {
+        for (pgpKeyDetails in keys) {
+          val fingerprint = pgpKeyDetails.fingerprint
+          if (!pgpKeyDetails.isFullyEncrypted) {
             throw IllegalStateException(
               context.getString(R.string.found_not_fully_encrypted_key, fingerprint)
             )
           }
 
-          if (roomDatabase.keysDao().getKeyByAccountAndFingerprintSuspend(
-              accountEntity.email.lowercase(), fingerprint
-            ) == null
-          ) {
-            if (addAccountIfNotExist) {
-              val existedAccount = roomDatabase.accountDao()
-                .getAccountSuspend(accountEntity.email.lowercase())
-              if (existedAccount == null) {
-                roomDatabase.accountDao().addAccountSuspend(accountEntity)
-              }
+          val existingKeyEntity = roomDatabase.keysDao().getKeyByAccountAndFingerprintSuspend(
+            accountEntity.email.lowercase(), fingerprint
+          )
+
+          if (existingKeyEntity != null) {
+            val decryptedPrivateKey =
+              KeyStoreCryptoManager.decryptSuspend(String(existingKeyEntity.privateKey))
+            val existingPgpKeyDetails =
+              PgpKey.parseKeys(decryptedPrivateKey).pgpKeyDetailsList.firstOrNull()
+
+            if (existingPgpKeyDetails?.isNewerThan(pgpKeyDetails) == true) {
+              continue
+            }
+          }
+
+          if (addAccountIfNotExist) {
+            val existedAccount = roomDatabase.accountDao()
+              .getAccountSuspend(accountEntity.email.lowercase())
+            if (existedAccount == null) {
+              roomDatabase.accountDao().addAccountSuspend(accountEntity)
+            }
+          }
+
+          val encryptedPassphrase =
+            if (pgpKeyDetails.passphraseType == KeyEntity.PassphraseType.DATABASE) {
+              KeyStoreCryptoManager.encryptSuspend(
+                String(requireNotNull(pgpKeyDetails.tempPassphrase))
+              )
+            } else null
+
+          val encryptedPrvKey =
+            KeyStoreCryptoManager.encryptSuspend(pgpKeyDetails.privateKey).toByteArray()
+
+          val keyEntity = (existingKeyEntity ?: pgpKeyDetails.toKeyEntity(accountEntity)).copy(
+            source = requireNotNull(pgpKeyDetails.importSourceType?.toPrivateKeySourceTypeString()),
+            privateKey = encryptedPrvKey,
+            storedPassphrase = encryptedPassphrase
+          )
+          val isAddedOrUpdated = if (existingKeyEntity != null) {
+            roomDatabase.keysDao().updateSuspend(keyEntity) > 0
+          } else {
+            roomDatabase.keysDao().insertSuspend(keyEntity) > 0
+          }
+
+          if (isAddedOrUpdated) {
+            if (pgpKeyDetails.passphraseType == KeyEntity.PassphraseType.RAM) {
+              keysStorage.putPassphraseToCache(
+                fingerprint = fingerprint,
+                passphrase = Passphrase(pgpKeyDetails.tempPassphrase),
+                validUntil = keysStorage.calculateLifeTimeForPassphrase(),
+                passphraseType = KeyEntity.PassphraseType.RAM
+              )
             }
 
-            val encryptedPassphrase =
-              if (keyDetails.passphraseType == KeyEntity.PassphraseType.DATABASE) {
-                KeyStoreCryptoManager.encryptSuspend(
-                  String(requireNotNull(keyDetails.tempPassphrase))
-                )
-              } else null
+            //update pub keys
+            val recipientDao = roomDatabase.recipientDao()
+            val pubKeysDao = roomDatabase.pubKeyDao()
+            for (mimeAddress in pgpKeyDetails.mimeAddresses) {
+              val address = mimeAddress.address.lowercase()
+              val name = mimeAddress.personal
 
-            val encryptedPrvKey =
-              KeyStoreCryptoManager.encryptSuspend(keyDetails.privateKey).toByteArray()
-
-            val keyEntity = keyDetails.toKeyEntity(accountEntity).copy(
-              source = requireNotNull(keyDetails.importSourceType?.toPrivateKeySourceTypeString()),
-              privateKey = encryptedPrvKey,
-              storedPassphrase = encryptedPassphrase
-            )
-            val isAdded = roomDatabase.keysDao().insertSuspend(keyEntity) > 0
-
-            if (isAdded) {
-              if (keyDetails.passphraseType == KeyEntity.PassphraseType.RAM) {
-                keysStorage.putPassphraseToCache(
-                  fingerprint = fingerprint,
-                  passphrase = Passphrase(keyDetails.tempPassphrase),
-                  validUntil = keysStorage.calculateLifeTimeForPassphrase(),
-                  passphraseType = KeyEntity.PassphraseType.RAM
-                )
+              val existedRecipientWithPubKeys =
+                recipientDao.getRecipientWithPubKeysByEmailSuspend(address)
+              if (existedRecipientWithPubKeys == null) {
+                recipientDao.insertSuspend(RecipientEntity(email = address, name = name))
               }
 
-              //update pub keys
-              val recipientDao = roomDatabase.recipientDao()
-              val pubKeysDao = roomDatabase.pubKeyDao()
-              for (mimeAddress in keyDetails.mimeAddresses) {
-                val address = mimeAddress.address.lowercase()
-                val name = mimeAddress.personal
-
-                val existedRecipientWithPubKeys =
-                  recipientDao.getRecipientWithPubKeysByEmailSuspend(address)
-                if (existedRecipientWithPubKeys == null) {
-                  recipientDao.insertSuspend(RecipientEntity(email = address, name = name))
-                }
-
-                val existedPubKeyEntity =
-                  pubKeysDao.getPublicKeyByRecipientAndFingerprint(address, keyDetails.fingerprint)
-                if (existedPubKeyEntity == null) {
-                  pubKeysDao.insertSuspend(keyDetails.toPublicKeyEntity(address))
-                }
+              val existedPubKeyEntity =
+                pubKeysDao.getPublicKeyByRecipientAndFingerprint(address, pgpKeyDetails.fingerprint)
+              if (existedPubKeyEntity == null) {
+                pubKeysDao.insertSuspend(pgpKeyDetails.toPublicKeyEntity(address))
               }
             }
           }
