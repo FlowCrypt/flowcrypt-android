0485f015937cf2dd04985f8daedebf6f4752dfd0
DenBond7 <DenBond7@gmail.com>
2023-08-02 15:36:26 +0300

    Converted '.gradle' to '.gradle.kts'.| #2409

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+ * Contributors: DenBond7
+ */
+
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+include(":FlowCrypt")
