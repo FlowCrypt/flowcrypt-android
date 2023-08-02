82edb2a560fbef92b0e6970cd22deb2f6e9daac5
DenBond7 <DenBond7@gmail.com>
2023-08-02 18:44:06 +0300

    Converted '.gradle' to '.gradle.kts'. Not completed.| #2409

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    kotlin("kapt") version "1.9.0" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.6.0" apply false
-    id("com.starter.easylauncher") version "6.2.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.0" apply false
+  id("com.android.application") version "8.1.0" apply false
+  id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+  kotlin("kapt") version "1.9.0" apply false
+  id("androidx.navigation.safeargs.kotlin") version "2.6.0" apply false
+  id("com.starter.easylauncher") version "6.2.0" apply false
+  id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.0" apply false
 }
 
 apply {
-    from("./ext.gradle.kts")
+  from("./ext.gradle.kts")
 }
