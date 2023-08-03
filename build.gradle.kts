db32aa9b0d79f307a5b9004738410135a2684e65
DenBond7 <DenBond7@gmail.com>
2023-08-03 09:35:19 +0300

    Renamed some files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    apply from: './ext.gradle'
+    apply from : './ext.gradle.kts'
 
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url "https://plugins.gradle.org/m2/"
-        }
+  repositories {
+    google()
+    mavenCentral()
+    maven {
+      url "https://plugins.gradle.org/m2/"
     }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:8.1.0'
+  }
+  dependencies {
+    classpath 'com.android.tools.build:gradle:8.1.0'
         classpath 'androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0'
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
