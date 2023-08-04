e2f8dc7814dfffb0052324edc461e3972c769011
DenBond7 <DenBond7@gmail.com>
2023-08-04 12:59:55 +0300

    Removed unused code.| #2409

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@
 
 plugins {
   id("com.android.application") version "8.1.0" apply false
-  id("com.android.library") version "8.1.0" apply false
   id("org.jetbrains.kotlin.android") version "1.8.10" apply false
   id("androidx.navigation.safeargs.kotlin") version "2.6.0" apply false
   id("com.starter.easylauncher") version "6.2.0" apply false
