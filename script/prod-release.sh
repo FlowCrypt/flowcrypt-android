3fc540778c1a01a37260d030db3e57f2eb7339cd
Den <DenBond7@gmail.com>
2023-08-07 12:44:24 +0300

    Issue 2409 migrate to kotlin dsl (#2410)
    * Removed 'detekt' as unused
    
    * Updated gradle to 8.1
    
    * Converted '.gradle' to '.gradle.kts'.| #2409
    
    * Converted '.gradle' to '.gradle.kts'. Not completed.| #2409
    
    * Revert "Converted '.gradle' to '.gradle.kts'. Not completed.| #2409"
    
    This reverts commit 82edb2a560fbef92b0e6970cd22deb2f6e9daac5.
    
    * Revert "Converted '.gradle' to '.gradle.kts'.| #2409"
    
    This reverts commit 0485f015937cf2dd04985f8daedebf6f4752dfd0.
    
    * Renamed some files
    
    * Renamed some files
    
    * Added some changes to keep history
    
    * Added some changes to keep history
    
    * Fixed build.gradle.kts.| #2409
    
    * Fixed build.gradle.kts.| #2409
    
    * Fixed easylauncher usage.| #2409
    
    * Fixed some tasks.| #2409
    
    * Refactored code
    
    * Fixed Glide usage.| #2409
    
    * Removed unused code.| #2409
    
    * Fixed build.gradle.kts.| #2409
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Rename ext.gradle to ext.gradle.kts
    
    * wip
    
    * wip

--- script/prod-release.sh
+++ script/prod-release.sh
@@ -7,4 +7,5 @@ echo ""
 read -s -p "Android Signing Key Password: " KEY_PWD
 echo ""
 
-./gradlew --console=verbose clean lintConsumerRelease prepareConsumerReleaseApk prepareEnterpriseReleaseApk -PruntimeSign -PstorePassword="$STORE_PWD" -PkeyPassword="$KEY_PWD"
+./gradlew --console=verbose clean lintConsumerRelease assembleConsumerRelease assembleEnterpriseRelease -PruntimeSign -PstorePassword="$STORE_PWD" -PkeyPassword="$KEY_PWD" --rerun-tasks
+./gradlew --console=verbose renameReleaseBuilds copyReleaseApks
