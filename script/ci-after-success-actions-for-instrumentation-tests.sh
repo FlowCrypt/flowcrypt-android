d80b53263114b872bdbf376ea04e6a2d1c183ba8
DenBond7 <DenBond7@gmail.com>
2024-01-23 10:51:11 +0200

    wip

--- script/ci-after-success-actions-for-instrumentation-tests.sh
+++ script/ci-after-success-actions-for-instrumentation-tests.sh
@@ -7,12 +7,12 @@ if [[ "$SEMAPHORE_JOB_NAME" =~ ^Instrumentation.* ]]; then
 
     # store logcat log
     echo "Store logcat log"
-    artifact push job 1w ~/logcat_log.txt
+    artifact push job ~/logcat_log.txt
 
     # store screenshots
     echo "Store screenshots"
     adb pull "/sdcard/Pictures"
     adb shell ls /sdcard/Pictures
-    artifact push job 1w Pictures
+    artifact push job Pictures
 fi
 
