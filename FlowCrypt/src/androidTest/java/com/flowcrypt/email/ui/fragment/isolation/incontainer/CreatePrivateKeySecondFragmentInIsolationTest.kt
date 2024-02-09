c23e84a4ce2f5b72fd1a681ce87ffb1ac0fb2108
DenBond7 <DenBond7@gmail.com>
2024-02-09 12:42:41 +0200

    wip

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/fragment/isolation/incontainer/CreatePrivateKeySecondFragmentInIsolationTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/fragment/isolation/incontainer/CreatePrivateKeySecondFragmentInIsolationTest.kt
@@ -6,16 +6,15 @@
 package com.flowcrypt.email.ui.fragment.isolation.incontainer
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
+import com.flowcrypt.email.base.BaseTest
 import com.flowcrypt.email.junit.annotations.FlowCryptTestSettings
 import com.flowcrypt.email.rules.ClearAppSettingsRule
 import com.flowcrypt.email.rules.GrantPermissionRuleChooser
@@ -23,9 +22,8 @@ import com.flowcrypt.email.rules.RetryRule
 import com.flowcrypt.email.rules.ScreenshotTestRule
 import com.flowcrypt.email.ui.activity.fragment.CreatePrivateKeySecondFragment
 import com.flowcrypt.email.ui.activity.fragment.CreatePrivateKeySecondFragmentArgs
-import com.flowcrypt.email.ui.base.BaseCheckPassphraseOnFirstScreenTest
+import com.flowcrypt.email.ui.base.AddAccountToDatabaseRuleInterface
 import com.flowcrypt.email.util.AccountDaoManager
-import org.hamcrest.Matchers
 import org.hamcrest.Matchers.emptyString
 import org.hamcrest.Matchers.`is`
 import org.junit.Before
@@ -41,10 +39,8 @@ import org.junit.runner.RunWith
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 @FlowCryptTestSettings(useCommonIdling = false)
-class CreatePrivateKeySecondFragmentInIsolationTest : BaseCheckPassphraseOnFirstScreenTest() {
-  override val firstScreenContinueButtonResId: Int = R.id.buttonSetPassPhrase
-  override val firstScreenEditTextResId: Int = R.id.editTextKeyPassword
-  override val firstScreenPasswordQualityInfoResId: Int = R.id.textViewPasswordQualityInfo
+class CreatePrivateKeySecondFragmentInIsolationTest : BaseTest(),
+  AddAccountToDatabaseRuleInterface {
 
   @get:Rule
   var ruleChain: TestRule = RuleChain
@@ -82,6 +78,6 @@ class CreatePrivateKeySecondFragmentInIsolationTest : BaseCheckPassphraseOnFirst
   }
 
   companion object {
-    val PASSPHRASE = "qwerty1234".chars().toArray()
+    val PASSPHRASE: IntArray = "qwerty1234".chars().toArray()
   }
 }
