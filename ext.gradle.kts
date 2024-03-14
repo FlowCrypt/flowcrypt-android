9c781ccc7f0f757477e7b14555be232b6d211bda
Denys Bondarenko <denbond7@gmail.com>
2024-03-14 09:01:02 +0200

    version 1.5.4: Improved message rendering for PGP/Mime(Inline)

--- ext.gradle.kts
+++ ext.gradle.kts
@@ -3,8 +3,8 @@
  * Contributors: DenBond7
  */
 
-extra["appVersionCode"] = 152
-extra["appVersionName"] = "1.5.2"
+extra["appVersionCode"] = 154
+extra["appVersionName"] = "1.5.4"
 extra["compileSdkVersion"] = 34
 extra["targetSdkVersion"] = 34
 extra["minSdkVersion"] = 26
