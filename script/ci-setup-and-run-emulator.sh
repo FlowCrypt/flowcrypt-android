2be10bd7bb344f0de85199eab3e7da3fea8bd51c
Denys Bondarenko <denbond7@gmail.com>
2024-03-15 20:38:07 +0200

    wip

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -2,7 +2,7 @@
 
 "$ANDROID_HOME/emulator/emulator" -accel-check
 avdmanager list devices #debug
-echo -ne '\n' | avdmanager -v create avd --name ci-emulator --package "system-images;android-32;google_apis;x86_64" --device 'pixel_5' --abi 'google_apis/x86_64'
+echo -ne '\n' | avdmanager -v create avd --name ci-emulator --package "system-images;android-34;google_apis;x86_64" --device 'pixel_5' --abi 'google_apis/x86_64'
 cat ~/.android/avd/ci-emulator.avd/config.ini
 # echo "hw.ramSize=3064"  >> ~/.android/avd/ci-emulator.avd/config.ini
 # cat ~/.android/avd/ci-emulator.avd/config.ini
