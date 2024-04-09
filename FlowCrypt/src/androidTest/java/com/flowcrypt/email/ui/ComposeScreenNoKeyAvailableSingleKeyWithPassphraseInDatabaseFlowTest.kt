d26b20c0dcddea93dc94c1f1ef7bd13f07313c17
DenBond7 <DenBond7@gmail.com>
2024-04-09 10:46:29 +0300

    Refactored code

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/ComposeScreenNoKeyAvailableSingleKeyWithPassphraseInDatabaseFlowTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/ComposeScreenNoKeyAvailableSingleKeyWithPassphraseInDatabaseFlowTest.kt
@@ -11,7 +11,6 @@ import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasTextColor
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -19,7 +18,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
 import com.flowcrypt.email.TestConstants
-import com.flowcrypt.email.extensions.kotlin.asInternetAddress
 import com.flowcrypt.email.junit.annotations.FlowCryptTestSettings
 import com.flowcrypt.email.rules.AddPrivateKeyToDatabaseRule
 import com.flowcrypt.email.rules.ClearAppSettingsRule
@@ -28,7 +26,6 @@ import com.flowcrypt.email.rules.RetryRule
 import com.flowcrypt.email.rules.ScreenshotTestRule
 import com.flowcrypt.email.ui.base.BaseComposeScreenNoKeyAvailableTest
 import com.flowcrypt.email.util.PrivateKeysManager
-import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -61,52 +58,26 @@ class ComposeScreenNoKeyAvailableSingleKeyWithPassphraseInDatabaseFlowTest : Bas
 
   @Test
   fun testImportKey() {
-    activeActivityRule?.launch(intent)
-    registerAllIdlingResources()
-    fillInAllFields(
-      to = setOf(
-        requireNotNull(TestConstants.RECIPIENT_WITH_PUBLIC_KEY_ON_ATTESTER.asInternetAddress())
-      )
-    )
+    doBaseActions {
+      addTextToClipboard("private key", requireNotNull(pgpKeyDetails.privateKey))
+      onView(withText(R.string.import_private_key))
+        .check(matches(isDisplayed()))
+        .perform(click())
+      Thread.sleep(1000)
+      onView(withText(R.string.load_from_clipboard))
+        .check(matches(isDisplayed()))
+        .perform(click())
 
-    //check that editTextFrom has gray text color. It means a sender doesn't have a private key
-    onView(withId(R.id.editTextFrom))
-      .check(matches(isDisplayed()))
-      .check(matches(hasTextColor(R.color.gray)))
+      onView(withId(R.id.editTextKeyPassword))
+        .perform(
+          replaceText(TestConstants.DEFAULT_STRONG_PASSWORD),
+          closeSoftKeyboard()
+        )
+      onView(withId(R.id.buttonPositiveAction))
+        .perform(scrollTo(), click())
 
-    onView(withId(R.id.menuActionSend))
-      .check(matches(isDisplayed()))
-      .perform(click())
-
-    isDialogWithTextDisplayed(
-      decorView,
-      getResString(R.string.no_key_available, addAccountToDatabaseRule.account.email)
-    )
-
-    addTextToClipboard("private key", requireNotNull(pgpKeyDetails.privateKey))
-    onView(withText(R.string.import_private_key))
-      .check(matches(isDisplayed()))
-      .perform(click())
-
-    Thread.sleep(1000)
-    onView(withText(R.string.load_from_clipboard))
-      .check(matches(isDisplayed()))
-      .perform(click())
-
-    onView(withId(R.id.editTextKeyPassword))
-      .perform(
-        replaceText(TestConstants.DEFAULT_STRONG_PASSWORD),
-        closeSoftKeyboard()
-      )
-    onView(withId(R.id.buttonPositiveAction))
-      .perform(scrollTo(), click())
-
-    waitForObjectWithText(addAccountToDatabaseRule.account.email, 5000)
-
-    //check that editTextFrom doesn't have gray text color. It means a sender has a private key.
-    onView(withId(R.id.editTextFrom))
-      .check(matches(isDisplayed()))
-      .check(matches(not(hasTextColor(R.color.gray))))
+      waitForObjectWithText(addAccountToDatabaseRule.account.email, 5000)
+    }
   }
 
   @Test
