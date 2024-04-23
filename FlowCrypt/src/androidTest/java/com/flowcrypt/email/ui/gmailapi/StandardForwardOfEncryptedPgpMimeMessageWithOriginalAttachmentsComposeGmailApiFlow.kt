1bc552a34277cd6b51629a25dbdd240503812590
Den <DenBond7@gmail.com>
2024-04-23 12:33:35 +0300

    code refactoring (#2690)
    * wip
    
    * wip

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/gmailapi/StandardForwardOfEncryptedPgpMimeMessageWithOriginalAttachmentsComposeGmailApiFlow.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/gmailapi/StandardForwardOfEncryptedPgpMimeMessageWithOriginalAttachmentsComposeGmailApiFlow.kt
@@ -10,14 +10,10 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.hasSibling
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withChild
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withParent
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -43,9 +39,7 @@ import jakarta.mail.internet.MimeMultipart
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
-import org.hamcrest.CoreMatchers.allOf
 import org.junit.Assert.assertEquals
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -102,29 +96,7 @@ class StandardForwardOfEncryptedPgpMimeMessageWithOriginalAttachmentsComposeGmai
     Thread.sleep(2000)
 
     //click on forward
-    onView(
-      allOf(
-        withId(R.id.layoutFwdButton),
-        withParent(
-          withParent(
-            withParent(
-              hasSibling(
-                allOf(
-                  withId(R.id.layoutHeader),
-                  withChild(
-                    allOf(
-                      withId(R.id.textViewSubject),
-                      withText(SUBJECT_EXISTING_PGP_MIME)
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
-      )
-    ).check(matches(isDisplayed()))
-      .perform(scrollTo(), click())
+    openReplyScreen(R.id.layoutFwdButton, SUBJECT_EXISTING_PGP_MIME)
 
     //switch to standard mode
     openActionBarOverflowOrOptionsMenu(getTargetContext())
