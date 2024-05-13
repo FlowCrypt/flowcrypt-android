269d3380404cb516a82dece59859de581b81d8d5
DenBond7 <DenBond7@gmail.com>
2024-05-13 10:42:28 +0300

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,11 @@
 
 plugins {
   id("com.android.application") version "8.4.0" apply false
-  id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.24" apply false
   id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
   id("com.starter.easylauncher") version "6.3.0" apply false
   id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.24" apply false
-  id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
+  id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
   id("org.ajoberstar.grgit") version "5.2.2" apply false
 }
 
