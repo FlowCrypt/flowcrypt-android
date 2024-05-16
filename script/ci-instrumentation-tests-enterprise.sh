bc76e80a4b7de498288e1599f6d977d5169c3c59
DenBond7 <DenBond7@gmail.com>
2024-05-16 10:04:09 +0300

    Disabled debugging for some scripts.| #2642

--- script/ci-instrumentation-tests-enterprise.sh
+++ script/ci-instrumentation-tests-enterprise.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./gradlew --info --console=plain :FlowCrypt:connectedEnterpriseUiTestsAndroidTest \
+./gradlew --console=plain :FlowCrypt:connectedEnterpriseUiTestsAndroidTest \
   -Pandroid.testInstrumentationRunnerArguments.filter=com.flowcrypt.email.junit.filters.EnterpriseTestsFilter
