939bd20849ee96066b311c0d525c0c48f810d1b4
DenBond7 <DenBond7@gmail.com>
2024-09-05 12:45:58 +0300

    wip

--- FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/workmanager/sync/UpdateMsgsSeenStateWorker.kt
+++ FlowCrypt/src/main/java/com/flowcrypt/email/jetpack/workmanager/sync/UpdateMsgsSeenStateWorker.kt
@@ -1,6 +1,6 @@
 /*
  * © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
- * Contributors: DenBond7
+ * Contributors: denbond7
  */
 
 package com.flowcrypt.email.jetpack.workmanager.sync
@@ -13,13 +13,14 @@ import com.flowcrypt.email.api.email.gmail.GmailApiHelper
 import com.flowcrypt.email.database.FlowCryptRoomDatabase
 import com.flowcrypt.email.database.MessageState
 import com.flowcrypt.email.database.entity.AccountEntity
-import org.eclipse.angus.mail.imap.IMAPFolder
+import com.flowcrypt.email.database.entity.MessageEntity
 import jakarta.mail.Flags
 import jakarta.mail.Folder
 import jakarta.mail.Message
 import jakarta.mail.Store
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.eclipse.angus.mail.imap.IMAPFolder
 
 /**
  * This task mark candidates as read/unread.
@@ -43,9 +44,10 @@ class UpdateMsgsSeenStateWorker(context: Context, params: WorkerParameters) :
     store: Store,
     state: MessageState
   ) = withContext(Dispatchers.IO) {
-    changeMsgsReadStateInternal(account, state) { fullFolderName, uidList ->
+    changeMsgsReadStateInternal(account, state) { fullFolderName, entities ->
       store.getFolder(fullFolderName).use { folder ->
         val imapFolder = (folder as IMAPFolder).apply { open(Folder.READ_WRITE) }
+        val uidList = entities.map { it.uid }
         val msgs: List<Message> = imapFolder.getMessagesByUID(uidList.toLongArray()).filterNotNull()
         if (msgs.isNotEmpty()) {
           imapFolder.setFlags(
@@ -60,22 +62,40 @@ class UpdateMsgsSeenStateWorker(context: Context, params: WorkerParameters) :
 
   private suspend fun changeMsgsReadState(account: AccountEntity, state: MessageState) =
     withContext(Dispatchers.IO) {
-      changeMsgsReadStateInternal(account, state) { _, uidList ->
+      changeMsgsReadStateInternal(account, state) { _, entities ->
         executeGMailAPICall(applicationContext) {
           if (state == MessageState.PENDING_MARK_READ) {
-            GmailApiHelper.changeLabels(
-              context = applicationContext,
-              accountEntity = account,
-              ids = uidList.map { java.lang.Long.toHexString(it).lowercase() },
-              removeLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
-            )
+            if (account.useConversationMode) {
+              GmailApiHelper.changeLabelsForThreads(
+                context = applicationContext,
+                accountEntity = account,
+                threadIdList = entities.mapNotNull { it.threadId }.toSet(),
+                removeLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
+              )
+            } else {
+              GmailApiHelper.changeLabels(
+                context = applicationContext,
+                accountEntity = account,
+                ids = entities.map { java.lang.Long.toHexString(it.uid).lowercase() },
+                removeLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
+              )
+            }
           } else {
-            GmailApiHelper.changeLabels(
-              context = applicationContext,
-              accountEntity = account,
-              ids = uidList.map { java.lang.Long.toHexString(it).lowercase() },
-              addLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
-            )
+            if (account.useConversationMode) {
+              GmailApiHelper.changeLabelsForThreads(
+                context = applicationContext,
+                accountEntity = account,
+                threadIdList = entities.mapNotNull { it.threadId }.toSet(),
+                addLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
+              )
+            } else {
+              GmailApiHelper.changeLabels(
+                context = applicationContext,
+                accountEntity = account,
+                ids = entities.map { java.lang.Long.toHexString(it.uid).lowercase() },
+                addLabelIds = listOf(GmailApiHelper.LABEL_UNREAD)
+              )
+            }
           }
         }
       }
@@ -84,7 +104,7 @@ class UpdateMsgsSeenStateWorker(context: Context, params: WorkerParameters) :
   private suspend fun changeMsgsReadStateInternal(
     account: AccountEntity,
     state: MessageState,
-    action: suspend (folderName: String, list: List<Long>) -> Unit
+    action: suspend (folderName: String, entities: List<MessageEntity>) -> Unit
   ) = withContext(Dispatchers.IO) {
     val roomDatabase = FlowCryptRoomDatabase.getDatabase(applicationContext)
     val candidatesForMark =
@@ -100,8 +120,8 @@ class UpdateMsgsSeenStateWorker(context: Context, params: WorkerParameters) :
           continue
         }
 
+        action.invoke(folderName, filteredMsgs)
         val uidList = filteredMsgs.map { it.uid }
-        action.invoke(folderName, uidList)
         val entities = roomDatabase.msgDao().getMsgsByUIDs(account.email, folderName, uidList)
           .filter { it.msgState == state }
           .map { it.copy(state = MessageState.NONE.value) }
