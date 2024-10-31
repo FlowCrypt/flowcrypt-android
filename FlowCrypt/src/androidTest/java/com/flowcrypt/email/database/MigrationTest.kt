734bbd7412bf25dff169171e0e386f2e7e638f1f
DenBond7 <DenBond7@gmail.com>
2024-10-31 14:12:49 +0200

    wip

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/database/MigrationTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/database/MigrationTest.kt
@@ -61,7 +61,6 @@ class MigrationTest {
 
   @Test
   @Throws(IOException::class)
-
   fun testAllMigrations() {
     // Create earliest version of the database.
     migrationTestHelper.createDatabase(FlowCryptRoomDatabase.DB_NAME, INIT_DATABASE_VERSION).apply {
