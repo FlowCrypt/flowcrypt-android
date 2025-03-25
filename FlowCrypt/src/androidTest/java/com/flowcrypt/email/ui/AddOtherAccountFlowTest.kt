59dd9ff6a6497c46fef5c29848d9ef7eef39aedb
denbond7 <denbond7@gmail.com>
2025-03-25 16:20:32 +0200

    Refactored code

--- FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/AddOtherAccountFlowTest.kt
+++ FlowCrypt/src/androidTest/java/com/flowcrypt/email/ui/AddOtherAccountFlowTest.kt
@@ -8,7 +8,7 @@ package com.flowcrypt.email.ui
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.filters.MediumTest
 import com.flowcrypt.email.R
-import com.flowcrypt.email.junit.suites.CustomSuite
+import com.flowcrypt.email.junit.runner.RepeatableAndroidJUnit4ClassRunner
 import com.flowcrypt.email.rules.ClearAppSettingsRule
 import com.flowcrypt.email.rules.GrantPermissionRuleChooser
 import com.flowcrypt.email.rules.RetryRule
@@ -17,6 +17,7 @@ import com.flowcrypt.email.ui.activity.MainActivity
 import com.flowcrypt.email.ui.base.AddOtherAccountBaseTest
 import com.flowcrypt.email.util.AuthCredentialsManager
 import com.flowcrypt.email.util.TestGeneralUtil
+import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
@@ -27,7 +28,8 @@ import org.junit.runner.RunWith
  * @author Denys Bondarenko
  */
 @MediumTest
-@RunWith(CustomSuite::class)
+@RunWith(RepeatableAndroidJUnit4ClassRunner::class)
+@RepeatableAndroidJUnit4ClassRunner.RepeatTest(3)
 class AddOtherAccountFlowTest : AddOtherAccountBaseTest() {
 
   override val activeActivityRule = activityScenarioRule<MainActivity>(
@@ -52,20 +54,10 @@ class AddOtherAccountFlowTest : AddOtherAccountBaseTest() {
   }
 
   @Test
+  @Ignore("skipp")
   fun two() {
     enableAdvancedMode()
     val credentials = AuthCredentialsManager.getAuthCredentials("user_with_not_existed_server.json")
     fillAllFields(credentials)
-  }
-
-  companion object {
-    @JvmStatic
-    // name argument is optional, it will show up on the test results
-    @CustomSuite.Parameters(name = "Input: {0}")
-    // parameters are provided as arrays, allowing more than one parameter
-    fun params() = listOf(
-      arrayOf(1),
-      arrayOf(2),
-    )
   }
 }
