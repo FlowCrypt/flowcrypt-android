fb7976c5267742b8b9bc2ef8e4e5536cada7dccd
denbond7 <denbond7@gmail.com>
2025-04-18 14:02:51 +0300

    wip

--- script/ci-wait-for-emulator.sh
+++ script/ci-wait-for-emulator.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# © 2016-present FlowCrypt a.s. Limitations apply. Contact human@flowcrypt.com
+# Contributors: denbond7
+#
+
 set -o xtrace
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 adb shell wm dismiss-keyguard
@@ -17,6 +22,11 @@ adb shell "echo 1 > /proc/sys/net/ipv4/ip_forward"
 adb shell "iptables -t nat -A PREROUTING -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
 adb shell "iptables -t nat -A OUTPUT -s 127.0.0.1 -p tcp --dport 443 -j REDIRECT --to 1212"
 ###################################################################################################
+
+# https://developer.android.com/tools/adb#forwardports
+# forwards requests on a specific host port to a different port on a device.
+# It can be helpful for debugging a mock web server
+adb forward tcp:1212 tcp:1212
 
 echo "Emulator is ready"
 set +o xtrace
