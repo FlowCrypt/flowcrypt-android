31e34b518bfdcb5f55d0c70e918b2b5e44ae8c7d
denbond7 <denbond7@gmail.com>
2025-09-14 16:16:28 +0300

    Fixed compilation issue for roboelectric.

--- FlowCrypt/build.gradle.kts
+++ FlowCrypt/build.gradle.kts
@@ -184,8 +184,8 @@ android {
 
   compileOptions {
     isCoreLibraryDesugaringEnabled = true
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
   }
 
   buildFeatures {
@@ -323,9 +323,16 @@ easylauncher {
   }
 }
 
+java {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
+}
+
+
 kotlin {
   compilerOptions {
-    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
   }
 }
