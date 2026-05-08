8e828d4a4ba01025507ff87993e45c1f78937ef6
Denys Bondarenko <denbond7@gmail.com>
2026-05-08 22:25:55 +0300

    Fixed lint warnings

--- FlowCrypt/build.gradle.kts
+++ FlowCrypt/build.gradle.kts
@@ -6,8 +6,6 @@
 
 import com.android.build.api.artifact.SingleArtifact
 import com.android.build.api.variant.ResValue
-import org.gradle.api.GradleException
-import java.io.File
 import com.android.ddmlib.DdmPreferences
 import java.io.FileInputStream
 import java.text.SimpleDateFormat
@@ -94,7 +92,6 @@ android {
 
   buildTypes {
     getByName("release") {
-      isShrinkResources = false
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
