d62a1aa2a5ee51f56f1af2b0f584ec85ba40fa87
Denys Bondarenko <denbond7@gmail.com>
2026-05-21 12:59:58 +0300

    i(dns): align HttpsTestWebServer dnsmasq with CI DNS setup and add startup DNS checks

--- docker/HttpsTestWebServer/entrypoint.sh
+++ docker/HttpsTestWebServer/entrypoint.sh
@@ -8,4 +8,18 @@
 set -e
 
 dnsmasq --no-daemon --conf-file=/etc/dnsmasq.d/flowcrypt-test.conf --log-facility=- &
+
+echo "Configuring resolver to use dnsmasq..."
+printf "nameserver 127.0.0.1\n" > /etc/resolv.conf
+
+echo "Checking dnsmasq directly..."
+dig @127.0.0.1 fel.localhost +short
+dig @127.0.0.1 fel.flowcrypt.test +short
+dig @127.0.0.1 www.google.com +short
+
+echo "Checking resolver..."
+ping -c 1 fel.localhost
+ping -c 1 fel.flowcrypt.test
+ping -c 1 www.google.com
+
 exec nginx -g 'daemon off;'
