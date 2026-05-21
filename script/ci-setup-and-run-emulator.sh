51a607b0b773306f35b511c460df1b30518c1970
denbond7 <denbond7@gmail.com>
2026-05-21 18:51:46 +0300

    switched to use -gpu auto for emulators on CI

--- script/ci-setup-and-run-emulator.sh
+++ script/ci-setup-and-run-emulator.sh
@@ -8,7 +8,7 @@
 set -euo pipefail
 
 AVD_RAM_SIZE=2048 ./script/create-avd.sh
-EMULATOR_GPU_MODE=swiftshader_indirect \
+EMULATOR_GPU_MODE=auto \
 EMULATOR_READ_ONLY=1 \
 EMULATOR_WIPE_DATA=1 \
 ./script/run-emulator.sh
