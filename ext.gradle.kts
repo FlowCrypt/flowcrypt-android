d0d5329a0746f8ec8e9ac8f991b853ca171c37bf
Denys Bondarenko <denbond7@gmail.com>
2026-08-14 09:44:38 +0300

    Version 1.6.5: Internal improvements and security fixes

--- ext.gradle.kts
+++ ext.gradle.kts
@@ -3,8 +3,8 @@
  * Contributors: denbond7
  */
 
-extra["appVersionCode"] = 164
-extra["appVersionName"] = "1.6.4"
+extra["appVersionCode"] = 165
+extra["appVersionName"] = "1.6.5"
 extra["compileSdkVersion"] = 37
 extra["targetSdkVersion"] = 37
 extra["minSdkVersion"] = 29
